cf94d5e7539d3a15c62eff51a74731a4faaeb42f
German Valdes <germanvs93@hotmail.com>
2018-12-13 14:23:17 +1300

    EX-221: Created color bar component

---
+++ src/app/shared/color-bar/color-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-color-bar',
+  templateUrl: './color-bar.component.html',
+  styleUrls: ['./color-bar.component.scss']
+})
+export class ColorBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
