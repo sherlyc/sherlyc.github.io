629b49f7ae6b3fd014525a9132fec7335009f51c
Chao Yang <chao.yang@fairfaxmedia.co.nz>
2018-12-13 18:09:39 +1300

    EX-220: add icon and header component

--- src/app/shared/header/header.component.spec.ts
+++ src/app/shared/header/header.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { StuffCustomMaterialModule } from '../../shared/stuff-custom-material/stuff-custom-material.module';
 import { HeaderComponent } from './header.component';
+import { IconComponent } from '../icon/icon.component';
+import { ColorBarComponent } from '../color-bar/color-bar.component';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -11,7 +13,7 @@ describe('HeaderComponent', () => {
       imports: [
         StuffCustomMaterialModule
       ],
-      declarations: [ HeaderComponent ]
+      declarations: [ HeaderComponent, IconComponent, ColorBarComponent ]
     })
     .compileComponents();
   }));
@@ -19,6 +21,9 @@ describe('HeaderComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
+    // inputs
+    component.mainNavLink = {text: 'national', href: '/national'};
+    component.subNavLink = {text: 'politics', href: '/national/politics'};
     fixture.detectChanges();
   });
 
@@ -28,6 +33,7 @@ describe('HeaderComponent', () => {
 
   it('should render app name in header', async(() => {
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.app-header__title').textContent).toContain('Stuff');
+    expect(compiled.querySelector('.main-navigation__link').textContent).toContain(component.mainNavLink.text);
+    expect(compiled.querySelector('.sub-nav__heading').textContent).toContain(component.subNavLink.text);
   }));
 });
