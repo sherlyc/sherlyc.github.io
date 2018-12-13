2540bc75a474d82d3c05538cf0e18d000d168a83
Chao Yang <chaoyang.nz@gmail.com>
2018-12-13 18:09:39 +1300

    EX-220: add icon and header component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ColorBarComponent } from './color-bar/color-bar.component';
+import {IconComponent} from './icon/icon.component';
+import {HeaderComponent} from './header/header.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [
-    ColorBarComponent
+    ColorBarComponent,
+    IconComponent,
+    HeaderComponent
   ],
   exports: [
-    ColorBarComponent
+    ColorBarComponent,
+    IconComponent,
+    HeaderComponent
   ]
 })
 export class SharedModule { }
