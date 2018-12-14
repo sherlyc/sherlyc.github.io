299efa99c480739c14fd2a4cac4c20dc11e4a0d7
German Valdes <germanvs93@hotmail.com>
2018-12-14 14:53:05 +1300

    EX-221: Header component test removed unused import

--- src/app/shared/header/header.component.spec.ts
+++ src/app/shared/header/header.component.spec.ts
@@ -1,8 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { StuffCustomMaterialModule } from '../../shared/stuff-custom-material/stuff-custom-material.module';
 import { HeaderComponent } from './header.component';
-import { LogoComponent } from '../logo/logo.component';
-import { ColorBarComponent } from '../color-bar/color-bar.component';
 import {SharedModule} from '../shared.module';
 
 describe('HeaderComponent', () => {
