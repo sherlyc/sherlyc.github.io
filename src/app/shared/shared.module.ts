0a82d213acb902348442ab807537d317701f1986
German Valdes <germanvs93@hotmail.com>
2018-12-14 14:59:33 +1300

    EX-221: Footer component added to SharedModule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ColorBarComponent } from './color-bar/color-bar.component';
+import { LogoComponent } from './logo/logo.component';
+import { HeaderComponent } from './header/header.component';
+import {FooterComponent} from "./footer/footer.component";
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [
-    ColorBarComponent
+    ColorBarComponent,
+    LogoComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   exports: [
-    ColorBarComponent
+    ColorBarComponent,
+    LogoComponent,
+    HeaderComponent,
+    FooterComponent
   ]
 })
 export class SharedModule { }
