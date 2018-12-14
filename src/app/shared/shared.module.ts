5c8659aa0c10da5a72c751b7d37547205e78c892
German Valdes <germanvs93@hotmail.com>
2018-12-14 16:07:47 +1300

    EX-221: Fixed lint rules after check

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ColorBarComponent } from './color-bar/color-bar.component';
 import { LogoComponent } from './logo/logo.component';
 import { HeaderComponent } from './header/header.component';
-import {FooterComponent} from "./footer/footer.component";
+import {FooterComponent} from './footer/footer.component';
 
 @NgModule({
   imports: [
