b07e7180357658d6d2a87d30ac6d79bd06715e85
German Valdes <germanvs93@hotmail.com>
2018-12-17 11:17:06 +1300

    EX-221: Added unit tests to footer component

--- src/app/shared/footer/footer.component.spec.ts
+++ src/app/shared/footer/footer.component.spec.ts
@@ -27,5 +27,44 @@ describe('FooterComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  it('should render footer links correctly', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    fixture.detectChanges();
 
+    expect(compiled.querySelectorAll('.footer__link').length).toBe(component.footerLinks.length);
+
+    const footerLinksNodes = Array.from(compiled.querySelectorAll('.footer__link a'));
+    const footerLinksText = footerLinksNodes.map((el: any) => el.textContent.trim());
+    const footerLinksHrefs = footerLinksNodes.map((el: any) => el.getAttribute('href'));
+
+    component.footerLinks.forEach(link => {
+      expect(footerLinksText).toContain(link.title);
+      expect(footerLinksHrefs).toContain(link.url);
+    });
+  });
+
+
+  it('should render footer icons correctly', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    fixture.detectChanges();
+
+    expect(compiled.querySelectorAll('.footer__secondary-social--icon').length)
+      .toBe(component.footerIcons.length);
+
+    const footerIconNodes = Array.from(compiled.querySelectorAll('.footer__secondary-social--icon a'));
+    const footerIconsUrls = footerIconNodes.map((el: any) => el.getAttribute('href'));
+
+    component.footerIcons.forEach(icon => {
+      expect(footerIconsUrls).toContain(icon.href);
+    });
+  });
+
+  it('should show copyright with current year correctly', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    fixture.detectChanges();
+
+    const currentYear = new Date().getFullYear();
+    expect(compiled.querySelector('.footer__secondary-copyright p').textContent.trim())
+      .toContain(currentYear);
+  });
 });
