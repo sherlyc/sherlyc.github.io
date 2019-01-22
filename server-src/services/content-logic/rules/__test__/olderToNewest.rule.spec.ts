57388ccc77f9313a1809000e69248755c5810ba5
Chao Yang <chao.yang@fairfaxmedia.co.nz>
2019-01-23 10:42:12 +1300

    EX-330: fix module imports after move files and introduce moment.js to solve iso8601 date parsing

--- server-src/services/content-logic/rules/__test__/olderToNewest.rule.spec.ts
+++ server-src/services/content-logic/rules/__test__/olderToNewest.rule.spec.ts
@@ -1,6 +1,6 @@
-import applyRules from '../content-logic/contentLogic';
-import * as articleList from './fixtures/standard.json';
-import * as olderToNewest from './fixtures/olderToNewest.json';
+import applyRules from '../../../content-logic/contentLogic';
+import * as articleList from '../../../__test__/fixtures/standard.json';
+import * as olderToNewest from '../../../__test__/fixtures/olderToNewest.json';
 
 describe('Older to newest rule', () => {
   it('should sort a provided standard article list from older to newest', () => {
