220714feda7891f028ce448899f0655be8c5ccf6
Chao Yang <chao.yang@fairfaxmedia.co.nz>
2019-01-23 10:42:12 +1300

    EX-330: fix module imports after move files and introduce moment.js to solve iso8601 date parsing

--- server-src/services/content-logic/__test__/contentLogic.spec.ts
+++ server-src/services/content-logic/__test__/contentLogic.spec.ts
@@ -1,11 +1,11 @@
-import { IContentRule } from '../../interfaces/IContentRule';
-import { IRawArticle } from '../../interfaces/IRawArticle';
-import * as articleList from './fixtures/standard.json';
-import * as olderToNewest from './fixtures/olderToNewest.json';
-import applyRules from '../content-logic/contentLogic';
+import { IContentRule } from '../../../interfaces/IContentRule';
+import { IRawArticle } from '../../../interfaces/IRawArticle';
+import * as articleList from '../../__test__/fixtures/standard.json';
+import * as olderToNewest from '../../__test__/fixtures/olderToNewest.json';
+import applyRules from '../../content-logic/contentLogic';
 
 const mockRule: IContentRule = jest.fn((articles: IRawArticle[]) => articles);
-jest.mock('../content-logic/rulesLoader', () => ({
+jest.mock('../../content-logic/rulesLoader', () => ({
   __esModule: true,
   default: (): Map<string, IContentRule> => new Map([['dummy', mockRule]])
 }));
@@ -14,6 +14,6 @@ describe('Content Logic', () => {
   it('should apply all the rules provided a list of article', () => {
     const expectedArticleList: IRawArticle[] = olderToNewest;
     expect(applyRules(articleList)).toEqual(expectedArticleList);
-    expect((mockRule as any).mock.calls.length).toEqual(1);
+    expect((mockRule as jest.Mock<IContentRule>).mock.calls.length).toEqual(1);
   });
 });
--- server-src/services/content-logic/rules/olderToNewest.rule.ts
+++ server-src/services/content-logic/rules/olderToNewest.rule.ts
@@ -1,12 +1,9 @@
+import * as moment from 'moment';
 import { IRawArticle } from '../../../interfaces/IRawArticle';
 
 export default (articleList: IRawArticle[]): IRawArticle[] => {
-  return articleList.sort((article1: IRawArticle, article2: IRawArticle) => {
-    if (article1.displayTime < article2.displayTime) {
-      return -1;
-    } else if (article1.displayTime > article2.displayTime) {
-      return 1;
-    }
-    return 0;
-  }) as IRawArticle[];
+  return articleList.sort(
+    (article1: IRawArticle, article2: IRawArticle) =>
+      moment(article1.displayTime).unix() - moment(article2.displayTime).unix()
+  );
 };
