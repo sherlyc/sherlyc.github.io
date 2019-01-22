254a70b61e61016fbccec8c5d70d9a9e6898aa7c
Chao Yang <chao.yang@fairfaxmedia.co.nz>
2019-01-22 15:51:50 +1300

    EX-330: refined contentLogic tests

--- server-src/services/__test__/contentLogic.spec.ts
+++ server-src/services/__test__/contentLogic.spec.ts
@@ -4,19 +4,16 @@ import * as articleList from './fixtures/standard.json';
 import * as olderToNewest from './fixtures/olderToNewest.json';
 import applyRules from '../content-logic/contentLogic';
 
+const mockRule: IContentRule = jest.fn((articles: IRawArticle[]) => articles);
 jest.mock('../content-logic/rulesLoader', () => ({
   __esModule: true,
-  default: (): Map<string, IContentRule> => {
-    const rules: Map<string, IContentRule> = new Map();
-    const dummyRule: IContentRule = (articles: IRawArticle[]) => articles;
-    rules.set('dummy', dummyRule);
-    return rules;
-  }
+  default: (): Map<string, IContentRule> => new Map([['dummy', mockRule]])
 }));
 
 describe('Content Logic', () => {
   it('should apply all the rules provided a list of article', () => {
     const expectedArticleList: IRawArticle[] = olderToNewest;
     expect(applyRules(articleList)).toEqual(expectedArticleList);
+    expect((mockRule as any).mock.calls.length).toEqual(1);
   });
 });
