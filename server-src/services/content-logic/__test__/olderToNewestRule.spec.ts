c94930e86b528dd1b0e0dbae66eff2d1bedb9f84
German Valdes <german.valdes@stuff.co.nz>
2019-01-24 12:11:54 +1300

    Ex-330: removed spec test to keep a more BDD style in our tests

--- server-src/services/content-logic/__test__/olderToNewestRule.spec.ts
+++ server-src/services/content-logic/__test__/olderToNewestRule.spec.ts
@@ -1,17 +1,10 @@
 import * as articleList from '../../__test__/fixtures/standard.json';
 import * as olderToNewestExpected from '../../__test__/fixtures/olderToNewest.json';
 import olderToNewest from '../olderToNewestRule';
-import { IRawArticle } from '../../../interfaces/IRawArticle';
 
 describe('Older to newest rule', () => {
   it('should sort a provided standard article list from older to newest', () => {
     const result = olderToNewest(Object.values(articleList));
     expect(result).toEqual(olderToNewestExpected);
-  });
-
-  it('should not mutate the original list', () => {
-    const list: IRawArticle[] = [];
-    const result = olderToNewest(list);
-    expect(result).not.toBe(list);
   });
 });
