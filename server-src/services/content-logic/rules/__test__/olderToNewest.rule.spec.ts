fcdc872c3c9c0d52b70ff3965a30000e22f0cf4e
German Valdes <german.valdes@stuff.co.nz>
2019-01-23 14:20:34 +1300

    Ex-330: updated oldestToNewest test

--- server-src/services/content-logic/rules/__test__/olderToNewest.rule.spec.ts
+++ server-src/services/content-logic/rules/__test__/olderToNewest.rule.spec.ts
@@ -1,12 +1,10 @@
-import applyRules from '../../../content-logic/contentLogic';
 import * as articleList from '../../../__test__/fixtures/standard.json';
-import * as olderToNewest from '../../../__test__/fixtures/olderToNewest.json';
+import * as olderToNewestExpected from '../../../__test__/fixtures/olderToNewest.json';
+import olderToNewest from '../olderToNewest.rule';
 
 describe('Older to newest rule', () => {
   it('should sort a provided standard article list from older to newest', () => {
-    const expectedOldestToNewest = olderToNewest;
-    const result = applyRules(articleList);
-
-    expect(result).toEqual(expectedOldestToNewest);
+    const result = olderToNewest(Object.values(articleList));
+    expect(result).toEqual(olderToNewestExpected);
   });
 });
