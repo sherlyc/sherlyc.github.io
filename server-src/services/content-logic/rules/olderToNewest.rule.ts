5e3138774668b5df58ac043ad5a567e03074f871
Lucas Rainett <lucas.rainett@stuff.co.nz>
2019-01-24 09:57:40 +1300

    avoid mutating the article list during transformation

--- server-src/services/content-logic/rules/olderToNewest.rule.ts
+++ server-src/services/content-logic/rules/olderToNewest.rule.ts
@@ -1,8 +1,10 @@
 import { IRawArticle } from '../../../interfaces/IRawArticle';
 
 export default (articleList: IRawArticle[]): IRawArticle[] => {
-  return articleList.sort(
-    (article1: IRawArticle, article2: IRawArticle) =>
-      article1.lastPublishedTime - article2.lastPublishedTime
-  );
+  return articleList
+    .slice()
+    .sort(
+      (article1: IRawArticle, article2: IRawArticle) =>
+        article1.lastPublishedTime - article2.lastPublishedTime
+    );
 };
