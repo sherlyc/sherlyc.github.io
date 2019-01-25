c6a869541e1b5938ad2ac8ab7efe500b172b7c15
Chao Yang <chao.yang@fairfaxmedia.co.nz>
2019-01-25 15:35:43 +1300

    EX-330: refined orchestrator test

--- server-src/services/__test__/orchestrator.spec.ts
+++ server-src/services/__test__/orchestrator.spec.ts
@@ -1,14 +1,24 @@
 import orchestrate from '../orchestrator';
 import * as homepage from './fixtures/homepage.json';
-import * as standard from './fixtures/standard.json';
 import getRawArticleList from '../content-source/jsonFeed';
+import contentLogic from '../content-logic/contentLogic';
+import generate from '../layout/layoutGenerator';
 
 jest.mock('../content-source/jsonFeed');
+jest.mock('../content-logic/contentLogic');
+jest.mock('../layout/layoutGenerator');
 
 describe.only('Orchestrator', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('should provide homepage content blocks', async () => {
-    (getRawArticleList as jest.Mock).mockResolvedValue(standard);
+    (generate as jest.Mock).mockReturnValue(homepage);
     await expect(orchestrate()).resolves.toEqual(homepage);
+    expect(getRawArticleList as jest.Mock).toHaveBeenCalledTimes(1);
+    expect(contentLogic as jest.Mock).toBeCalledTimes(1);
+    expect(generate as jest.Mock).toBeCalledTimes(1);
   });
 
   it('should provide an error content block when jsonfeed retriever fails', async () => {
@@ -16,5 +26,7 @@ describe.only('Orchestrator', () => {
     await expect(orchestrate()).resolves.toEqual([
       { type: 'ErrorBlock', message: 'Http error' }
     ]);
+    expect(contentLogic as jest.Mock).not.toHaveBeenCalled();
+    expect(generate as jest.Mock).not.toHaveBeenCalled();
   });
 });
