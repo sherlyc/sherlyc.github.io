529924faaa4acabe515034eb637c46ab487329a4
German Valdes <german.valdes@stuff.co.nz>
2019-02-05 12:53:27 +1300

    EX-333: Added return type to contentRetrieverService getContent() function

--- src/app/content-retriever.service.ts
+++ src/app/content-retriever.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
+import { IContentBlock } from '../../server-src/services/__types__/IContentBlock';
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +10,9 @@ import { catchError } from 'rxjs/operators';
 export class ContentRetrieverService {
   constructor(private http: HttpClient) {}
 
-  getContent(): Observable<any> {
+  getContent(): Observable<IContentBlock[]> {
     return this.http
-      .get('http://localhost:4000/api/')
+      .get<IContentBlock[]>('http://localhost:4000/api/')
       .pipe(catchError(this.handleError));
   }
 
