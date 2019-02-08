cc76aca8f047b7e5db0da105801d186adc9aa793
Chao Yang <chaoyang.nz@gmail.com>
2019-02-08 22:18:27 +1300

    EX-333: fix content retriever failure in ssr

--- src/app/services/content-retriever.service.ts
+++ src/app/services/content-retriever.service.ts
@@ -1,20 +1,35 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 import { IContentBlock } from '../../../common/__types__/IContentBlock';
 import { environment } from '../../environments/environment';
 import { IErrorBlock } from '../../../common/__types__/IErrorBlock';
+import { isPlatformBrowser } from '@angular/common';
+
+declare const process: any;
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContentRetrieverService {
-  constructor(private http: HttpClient) {}
+  isBrowser: boolean;
+  constructor(
+    private http: HttpClient,
+    @Inject(PLATFORM_ID) platformId: Object
+  ) {
+    this.isBrowser = isPlatformBrowser(platformId);
+  }
 
   getContent(): Observable<IContentBlock[]> {
+    let url = environment.backendUrl;
+    if (!this.isBrowser) {
+      const port = process.env.PORT || 4000;
+      url = `http://localhost:${port}${url}`;
+    }
+
     return this.http
-      .get<IContentBlock[]>(environment.backendUrl)
+      .get<IContentBlock[]>(url)
       .pipe(catchError(this.handleError));
   }
 
