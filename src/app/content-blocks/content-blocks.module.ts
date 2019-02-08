746fe8e9fd42469d44b1be4c943203d115f08317
Chao Yang <chao.yang@fairfaxmedia.co.nz>
2019-02-08 17:12:21 +1300

    EX-333: refine

--- src/app/content-blocks/content-blocks.module.ts
+++ src/app/content-blocks/content-blocks.module.ts
@@ -3,18 +3,14 @@ import { CommonModule } from '@angular/common';
 import { TimeAgoPipe } from 'time-ago-pipe';
 import { ContentBlockComponent } from './content-block/content-block.component';
 import { BasicArticleUnitComponent } from './basic-article-unit/basic-article-unit.component';
-import registry from './content-blocks-registry';
 import { ErrorBlockComponent } from './error-block/error-block.component';
 
+const contentBlockComponents = [BasicArticleUnitComponent, ErrorBlockComponent];
+
 @NgModule({
-  declarations: [
-    TimeAgoPipe,
-    ContentBlockComponent,
-    BasicArticleUnitComponent,
-    ErrorBlockComponent
-  ],
+  declarations: [TimeAgoPipe, ContentBlockComponent, ...contentBlockComponents],
   imports: [CommonModule],
   exports: [ContentBlockComponent],
-  entryComponents: [Object.values(registry)]
+  entryComponents: contentBlockComponents
 })
 export class ContentBlocksModule {}
