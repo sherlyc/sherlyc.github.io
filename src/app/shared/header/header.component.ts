93fad918f9e03a0da02bd9e53e6705f42fe08db2
Lucas Rainett <lucas.rainett@stuff.co.nz>
2019-02-12 15:04:46 +1300

    add flexgrid and restructure layout

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,23 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
-  @Input()
-  mainNavLink!: INavLink;
-
-  @Input()
-  subNavLink!: INavLink;
-
+export class HeaderComponent {
   constructor() {}
-
-  ngOnInit() {}
-}
-
-interface INavLink {
-  text: string;
-  href: string;
 }
