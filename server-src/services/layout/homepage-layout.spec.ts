fbccdaac5b2258d9848896ea6afd34853ed2fda9
Lucas Rainett <lucas.rainett@stuff.co.nz>
2019-02-18 14:29:22 +1300

    fix homepage layout test to only test homepage function, add sections

--- server-src/services/layout/homepage-layout.spec.ts
+++ server-src/services/layout/homepage-layout.spec.ts
@@ -1,9 +1,9 @@
-import generate from './layout-generator';
+import homepageLayout from './homepage-layout';
 import * as articleList from './__fixtures__/standard.json';
-import * as homepage from './__fixtures__/homepage-layout.json';
+import * as homepage from './__fixtures__/homepage.json';
 
 describe('Homepage Layout', () => {
   it('should generate a homepage layout', () => {
-    expect(generate(articleList)).toEqual(homepage);
+    expect(homepageLayout(articleList)).toEqual(homepage);
   });
 });
