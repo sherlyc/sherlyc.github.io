cddd6c724d85d4855fe52dc6fa367f9cd927f5be
Lucas Rainett <lucas.rainett@stuff.co.nz>
2019-02-20 11:59:48 +1300

    unnecessary change

--- src/app/services/config/__types__/IEnvironmentDefinition.ts
+++ src/app/services/config/__types__/IEnvironmentDefinition.ts
@@ -1,7 +1,5 @@
 import { ILoggerOptions } from '../../../../../common/__types__/ILoggerOptions';
-
 export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
-
 export interface IEnvironmentDefinition {
   retryTimeout: number;
   requestTimeout: number;
