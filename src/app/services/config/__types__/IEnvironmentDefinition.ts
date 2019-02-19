1a761a38f6cb2cc810fda13a28fed81782c0f623
Chao Yang <chao.yang@fairfaxmedia.co.nz>
2019-02-19 19:47:21 +1300

    fix server side request without authentication

--- src/app/services/config/__types__/IEnvironmentDefinition.ts
+++ src/app/services/config/__types__/IEnvironmentDefinition.ts
@@ -1,9 +1,17 @@
 import { ILoggerOptions } from '../../../../../common/__types__/ILoggerOptions';
-
+export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 export interface IEnvironmentDefinition {
   retryTimeout: number;
   requestTimeout: number;
   spadeAPI: string;
   loggerOptions: ILoggerOptions;
   aadSdkUrl: string;
+  serverOverrides?: Omit<
+    IEnvironmentDefinition,
+    'serverOverrides' | 'browserOverrides'
+  >;
+  browserOverrides?: Omit<
+    IEnvironmentDefinition,
+    'serverOverrides' | 'browserOverrides'
+  >;
 }
--- src/app/services/config/config.service.ts
+++ src/app/services/config/config.service.ts
@@ -1,7 +1,8 @@
 import * as config from './config.json';
-import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { IEnvironmentDefinition } from './__types__/IEnvironmentDefinition';
 import { RuntimeService } from '../runtime/runtime.service';
+import { defaults } from 'lodash';
 
 @Injectable({
   providedIn: 'root'
@@ -11,10 +12,21 @@ export class ConfigService {
 
   getConfig(): IEnvironmentDefinition {
     const environments: { [key: string]: IEnvironmentDefinition } = config;
-    return (
-      environments[
-        this.runtime.getEnvironmentVariable('SPADE_ENV', 'production')
-      ] || environments['production']
+    const environmentName = this.runtime.getEnvironmentVariable(
+      'SPADE_ENV',
+      'production'
     );
+    let environment =
+      environments[environmentName] || environments['production'];
+    if (this.runtime.isServer() && environment.serverOverrides) {
+      environment = defaults(environment.serverOverrides, environment);
+    }
+    if (this.runtime.isBrowser() && environment.browserOverrides) {
+      environment = defaults(environment.browserOverrides, environment);
+    }
+    delete environment.browserOverrides;
+    delete environment.serverOverrides;
+
+    return environment;
   }
 }
