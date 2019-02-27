b31ce4b302bb425a9567ae6d500942fb3a268442
Chao Yang <chao.yang@fairfaxmedia.co.nz>
2019-02-27 17:13:30 +1300

    no need to test

--- src/app/content-blocks/content-block-list/content-block-list.component.spec.ts
+++ src/app/content-blocks/content-block-list/content-block-list.component.spec.ts
@@ -1,10 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ContentBlockListComponent } from './content-block-list.component';
-import { IContentBlock } from '../../../../common/__types__/IContentBlock';
 import { ContentBlockDirective } from '../../directives/content-block/content-block.directive';
 import { LoggerService } from '../../services/logger/logger.service';
 import { LoggerServiceMock } from '../../services/logger/logger.service.mock';
-import { By } from '@angular/platform-browser';
 
 describe('Content block list', () => {
   let component: ContentBlockListComponent;
@@ -25,16 +23,7 @@ describe('Content block list', () => {
     component = fixture.componentInstance;
   });
 
-  it('should display a list of content block', () => {
-    fixture.detectChanges();
-    component.contentBlocks = ([
-      { type: 'FakeContentBlock' },
-      { type: 'FakeContentBlock' },
-      { type: 'FakeContentBlock' }
-    ] as any) as IContentBlock[];
-
-    fixture.detectChanges();
-
-    expect(1).toBe(1);
+  it('should create', () => {
+    expect(component).toBeTruthy();
   });
 });
