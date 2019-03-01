2f589d629c5bb243ae1ed33b2f9e77d222cb6e17
Lucas Rainett <lucas.rainett@stuff.co.nz>
2019-03-01 15:07:17 +1300

    more ad units

--- server-src/services/handlers/basic-article-section.ts
+++ server-src/services/handlers/basic-article-section.ts
@@ -24,7 +24,8 @@ export default async function(
           sectionId,
           totalArticles
         })
-      }
+      },
+      { type: ContentBlockType.BasicAdUnit }
     ];
   } catch (e) {
     return [];
