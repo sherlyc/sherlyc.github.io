5362ae1ca47c2c8004ed1f79d5062e31c3185b3e
Lucas Rainett <lucas.rainett@stuff.co.nz>
2019-03-08 16:44:16 +1300

    add intersection observer polyfill

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -73,3 +73,4 @@ import 'zone.js/dist/zone'; // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+import 'intersection-observer';
