c4291709bc53f006cab9f3bd43653a1e83e2e18f
German Valdes <german.valdes@stuff.co.nz>
2019-03-12 15:55:05 +1300

    extracted copyright component and added to burger button menu

--- src/app/content-blocks/content-blocks.module.ts
+++ src/app/content-blocks/content-blocks.module.ts
@@ -5,13 +5,15 @@ import { MomentModule } from 'ngx-moment';
 import { ContentBlockDirective } from '../directives/content-block/content-block.directive';
 import { HideHeaderDirective } from '../directives/hide-header/hide-header.directive';
 import { ContentBlockListComponent } from './content-block-list/content-block-list.component';
+import { CopyrightComponent } from './copyright/copyright.component';
 
 @NgModule({
   declarations: [
     ...contentBlockComponents,
     HideHeaderDirective,
     ContentBlockDirective,
-    ContentBlockListComponent
+    ContentBlockListComponent,
+    CopyrightComponent
   ],
   imports: [CommonModule, MomentModule],
   exports: [ContentBlockDirective, ContentBlockListComponent],
