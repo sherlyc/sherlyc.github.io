ac85cf54fa8ad9514b874c296d88da2e7033982c
German Valdes <german.valdes@stuff.co.nz>
2019-03-18 12:17:26 +1300

    Updated json retriever to use URL so it can throw an Exception when the url is malformed

--- server-src/services/adapters/jsonfeed-retriever.ts
+++ server-src/services/adapters/jsonfeed-retriever.ts
@@ -4,12 +4,14 @@ import retry from '../utils/retry';
 import http from '../utils/http';
 import { Section } from '../section';
 
-async function apiCall(url: URL): Promise<IJsonFeedArticleList> {
+async function apiCall(
+  section: Section,
+  total: number
+): Promise<IJsonFeedArticleList> {
+  const url: URL = new URL(`${config.jsonFeedAPI}/${section}?limit=${total}`);
   const response = await http.get<IJsonFeedArticleList>(url.href);
   return response.data;
 }
 
-export default (section: Section, total: number) => {
-  const url: URL = new URL(`${config.jsonFeedAPI}/${section}?limit=${total}`);
-  return retry(() => apiCall(url));
-};
+export default (section: Section, total: number) =>
+  retry(() => apiCall(section, total));
