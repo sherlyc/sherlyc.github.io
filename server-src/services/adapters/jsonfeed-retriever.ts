ab0c9aeb98b0c3fdc44e2fb9df95d78aa348a1ac
German Valdes <german.valdes@stuff.co.nz>
2019-03-18 11:18:44 +1300

    Added logging to json retriever and introduced URL object to validate url is valid

--- server-src/services/adapters/jsonfeed-retriever.ts
+++ server-src/services/adapters/jsonfeed-retriever.ts
@@ -3,17 +3,23 @@ import config from '../utils/config';
 import retry from '../utils/retry';
 import http from '../utils/http';
 import { Section } from '../section';
+import * as pRetry from 'p-retry';
+import logger from '../utils/logger';
 
-async function apiCall(
-  section: Section,
-  total: number
-): Promise<IJsonFeedArticleList> {
-  const response = await http.get<IJsonFeedArticleList>(
-    `${config.jsonFeedAPI}/${section}?limit=${total}`
-  );
+async function apiCall(url: URL): Promise<IJsonFeedArticleList> {
+  const response = await http.get<IJsonFeedArticleList>(url.href);
   return response.data;
 }
 
 export default (section: Section, total: number) => {
-  return retry(() => apiCall(section, total));
+  const url: URL = new URL(`${config.jsonFeedAPI}/${section}?limit=${total}`);
+  return retry(() => apiCall(url), {
+    onFailedAttempt: (error: pRetry.FailedAttemptError) => {
+      logger.warn(
+        `Attempt ${error.attemptNumber} failing when calling ${
+          url.href
+        }. There are ${error.retriesLeft} retries left.`
+      );
+    }
+  });
 };
