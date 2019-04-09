69f078ac72733bb96c1d0ac463487e5ce9f93d44
Lucas Rainett <lucas.rainett@stuff.co.nz>
2019-04-09 17:37:43 +1200

    small improvement in the api test

---
+++ server-src/test-api/api.spec.ts
@@ -0,0 +1,32 @@
+import * as supertest from 'supertest';
+import { IPage } from '../../common/__types__/IPage';
+
+describe('api test', () => {
+  it('should respond with a json payload', async () => {
+    const app = require('../app').default;
+    const response: supertest.Response = await supertest(app)
+      .get('/api/content')
+      .set('Accept', 'application/json');
+
+    expect(response.ok).toBeTruthy();
+    expect(response.header['content-type']).toMatch(/application\/json/);
+
+    const page: IPage = response.body;
+    expect(page.title).toBe('Stuff');
+    expect(page.content).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ type: 'Header' }),
+        expect.objectContaining({
+          type: 'Container',
+          items: expect.arrayContaining([
+            expect.objectContaining({ type: 'BasicAdUnit' }),
+            expect.objectContaining({ type: 'BasicArticleUnit' }),
+            expect.objectContaining({ type: 'ColumnContainer' }),
+            expect.objectContaining({ type: 'BasicArticleSection' })
+          ])
+        }),
+        expect.objectContaining({ type: 'Footer' })
+      ])
+    );
+  });
+});
