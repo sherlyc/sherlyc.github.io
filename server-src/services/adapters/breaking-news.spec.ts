c3955d706ade180cbcd0e06b010d6bcd0cb04929
Sherly Chan <sherly.chan@fairfaxmedia.co.nz>
2019-04-17 17:33:10 +1200

    EX-455-ensure linting is happy

--- server-src/services/adapters/breaking-news.spec.ts
+++ server-src/services/adapters/breaking-news.spec.ts
@@ -33,7 +33,7 @@ describe('Breaking news service', () => {
     expect(await getBreakingNews()).toEqual({
       id: '123',
       text:
-        "$1 billion budget blowout in Auckland's City Rail Link, officials admit... read more",
+        '$1 billion budget blowout in Aucklands City Rail Link, officials admit... read more',
       link:
         'https://www.stuff.co.nz/business/111688527/vendors-claim-real-estate-agent-acted-inappropriately-in-helping-buyer-flip-house',
       enabled: true
--- server-src/services/adapters/breaking-news.ts
+++ server-src/services/adapters/breaking-news.ts
@@ -8,7 +8,7 @@ async function apiCall(): Promise<IRawBreakingNews> {
     return {
       id: '123',
       text:
-        "$1 billion budget blowout in Auckland's City Rail Link, officials admit... read more",
+        '$1 billion budget blowout in Aucklands City Rail Link, officials admit... read more',
       link:
         'https://www.stuff.co.nz/business/111688527/vendors-claim-real-estate-agent-acted-inappropriately-in-helping-buyer-flip-house',
       enabled: true
