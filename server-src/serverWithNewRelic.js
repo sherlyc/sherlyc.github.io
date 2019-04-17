dc7d6c2bae3c7a51d6fceae0a484bfc920753b7e
Viet Tran <viet.tran@fairfaxmedia.co.nz>
2019-04-17 20:55:35 +0000

    Merged in feature/EX-415-newRelic (pull request #157)
    Feature/EX-415 newRelic. New Relic application monitoring
    
    * Add new relic to package json
    
    * Update pom xml
    
    * Copy serverfile with new relic to dist
    
    * Update pom
    
    * EX-415: Add new relic env to dockerfile
    
    * EX-415: Explicitly define new relic log level
    
    * Update new relic env
    
    * Remove newrelic from package json
    
    * EX-415: Remove env variable for npm ci
    
    * Move env from dockerfile to k8s deployment
    
    * Change single quote to double quote
    
    * Change to double quote
    
    * Fix deployment yaml
    
    Approved-by: german valdes <german.valdes@fairfaxmedia.co.nz>
    Approved-by: Chao Yang <chao.yang@fairfaxmedia.co.nz>
    Approved-by: Sherly Chan <sherly.chan@fairfaxmedia.co.nz>
    Approved-by: Lucas Rainett <lucas.rainett@fairfaxmedia.co.nz>
    Approved-by: Nick Kou <nick.kou@fairfaxmedia.co.nz>

---
+++ server-src/serverWithNewRelic.js
@@ -0,0 +1,8 @@
+/**
+ * This is a workaround for new relic with webpack.
+ * Please follow the discussion here https://discuss.newrelic.com/t/please-officially-support-webpack/58585
+ */
+if (process.env.NEW_RELIC_LICENSE_KEY) {
+  require('newrelic');
+}
+require('./main');
