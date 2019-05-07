54a286a906306b03ca6509dc1cdc38cd43cd6f96
Karen Xie <karen.xie@fairfaxmedia.co.nz>
2019-05-08 11:27:28 +1200

    EX-466: add data layer setup call into app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,10 +2,14 @@ import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { AdService } from './services/ad/ad.service';
-import { mockService } from './services/mocks/MockService';
+import { mockService, ServiceMock } from './services/mocks/MockService';
 import { DtmService } from './services/dtm/dtm.service';
+import { DataLayerService } from './services/data-layer/data-layer.service';
 
 describe('AppComponent', () => {
+  let dataLayerService: ServiceMock<DataLayerService>;
+  let dtmService: ServiceMock<DtmService>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule],
@@ -16,12 +20,20 @@ describe('AppComponent', () => {
           useClass: mockService(AdService)
         },
         {
+          provide: DataLayerService,
+          useClass: mockService(DataLayerService)
+        },
+        {
           provide: DtmService,
           useClass: mockService(DtmService)
         }
       ]
     }).compileComponents();
+
+    dataLayerService = TestBed.get(DataLayerService);
+    dtmService = TestBed.get(DtmService);
   });
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
@@ -34,4 +46,20 @@ describe('AppComponent', () => {
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('router-outlet')).toBeTruthy();
   }));
+
+  it('should setup data layer service before dtm service', () => {
+    const serviceCallOrder: string[] = [];
+    dataLayerService.setup.mockImplementation(() => {
+      serviceCallOrder.push('1');
+    });
+    dtmService.setup.mockImplementation(() => {
+      serviceCallOrder.push('2');
+      return null;
+    });
+
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+
+    expect(serviceCallOrder).toEqual(['1', '2']);
+  });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { AdService } from './services/ad/ad.service';
 import { EventsService } from './services/events/events.service';
 import { DtmService } from './services/dtm/dtm.service';
+import { DataLayerService } from './services/data-layer/data-layer.service';
 
 @Component({
   selector: 'app-root',
@@ -12,10 +13,12 @@ export class AppComponent {
   constructor(
     private adService: AdService,
     private eventsService: EventsService,
+    private dataLayerService: DataLayerService,
     private dtm: DtmService
   ) {
     this.adService.setupAds();
     this.eventsService.setup();
+    this.dataLayerService.setup();
     this.dtm.setup();
   }
 }
