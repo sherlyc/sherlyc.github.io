e10c1be2c210680a336296099784aac4505448c4
Karen Xie <karen.xie@stuff.co.nz>
2019-05-10 13:47:13 +1200

    EX-380: Analytics for twitter footer link

---
+++ src/app/content-blocks/footer/footer.component.spec.ts
@@ -0,0 +1,36 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FooterComponent } from './footer.component';
+import { By } from '@angular/platform-browser';
+import { CopyrightComponent } from '../../shared/components/copyright/copyright.component';
+import { AnalyticsService } from 'src/app/services/data-layer/analytics.service';
+import { mockService, ServiceMock } from 'src/app/services/mocks/MockService';
+
+describe('Header', () => {
+  let fixture: ComponentFixture<FooterComponent>;
+  let analyticsService: ServiceMock<AnalyticsService>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [FooterComponent, CopyrightComponent],
+      providers: [
+        {
+          provide: AnalyticsService,
+          useClass: mockService(AnalyticsService)
+        }
+      ]
+    }).compileComponents();
+    fixture = TestBed.createComponent(FooterComponent);
+    analyticsService = TestBed.get(AnalyticsService);
+  });
+
+  it('push analytics event when twitter link is clicked', () => {
+    fixture.debugElement
+      .query(By.css('a[aria-label="Twitter"]'))
+      .nativeElement.click();
+
+    expect(analyticsService.pushEvent).toHaveBeenCalledWith({
+      event: 'menu.footer',
+      'menu.link': 'Twitter'
+    });
+  });
+});
