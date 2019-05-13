8cca5ceaa43d274180f479f216c02457c8d5503e
Viet Anh Tran <viet.tran@fairfaxmedia.co.nz>
2019-05-13 13:27:42 +1200

    EX-412: Replace switch case for jsonfeed with a registry

--- server-src/services/adapters/jsonfeed-retriever.spec.ts
+++ server-src/services/adapters/jsonfeed-retriever.spec.ts
@@ -45,20 +45,20 @@ describe('JsonFeed Retriever', () => {
     it('should respond with mid strip data', async () => {
       (http(params).get as jest.Mock).mockResolvedValue({ data: midStripData });
 
-      expect(await retrieveMidStrip(2, params)).toEqual(midStripData);
+      expect(await retrieveMidStrip(params, 2)).toEqual(midStripData);
     });
 
     it('should throw error when jsonfeed request for mid strip fails', async () => {
       const error = new Error('AJAX error');
       (http(params).get as jest.Mock).mockRejectedValue(error);
 
-      await expect(retrieveMidStrip(2, params)).rejects.toEqual(error);
+      await expect(retrieveMidStrip(params, 2)).rejects.toEqual(error);
     });
 
     it('should retrieve specified number of articles', async () => {
       (http(params).get as jest.Mock).mockResolvedValue({ data: midStripData });
 
-      const midStripJsonFeed = await retrieveMidStrip(1, params);
+      const midStripJsonFeed = await retrieveMidStrip(params, 1);
 
       const articles = midStripJsonFeed.assets;
       expect(articles.length).toEqual(1);
@@ -67,7 +67,7 @@ describe('JsonFeed Retriever', () => {
     it('should retrieve all articles if specifed total is more than number of articles received', async () => {
       (http(params).get as jest.Mock).mockResolvedValue({ data: midStripData });
 
-      const midStripJsonFeed = await retrieveMidStrip(5, params);
+      const midStripJsonFeed = await retrieveMidStrip(params, 5);
 
       const articles = midStripJsonFeed.assets;
       expect(articles.length).toEqual(2);
