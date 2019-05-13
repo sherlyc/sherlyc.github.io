26b34bd5a0e08bd0b92d5a9313f2caf33d944893
German Valdes <german.valdes@stuff.co.nz>
2019-05-13 18:14:35 +1200

    Merge remote-tracking branch 'origin/master' into analytics-refactored Remaining conflicts in analytics.service.spec.ts resolved

--- src/app/services/analytics/analytics.service.spec.ts
+++ src/app/services/analytics/analytics.service.spec.ts
@@ -1,59 +1,45 @@
 import { TestBed } from '@angular/core/testing';
 import { AnalyticsService } from './analytics.service';
-import { RuntimeService } from '../runtime/runtime.service';
 import { mockService, ServiceMock } from '../mocks/MockService';
-import { IDigitalData } from './__types__/IDigitalData';
 import { AnalyticsEventsType } from './__types__/AnalyticsEventsType';
-
-declare let window: {
-  digitalData: IDigitalData;
-};
+import { WindowService } from '../window/window.service';
+import { LoggerService } from '../logger/logger.service';
 
 describe('AnalyticsService', () => {
-  let runtimeService: ServiceMock<RuntimeService>;
+  let windowService: ServiceMock<WindowService>;
   let analyticsService: AnalyticsService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
         {
-          provide: RuntimeService,
-          useClass: mockService(RuntimeService)
+          provide: WindowService,
+          useClass: mockService(WindowService)
+        },
+        {
+          provide: LoggerService,
+          useClass: mockService(LoggerService)
         }
       ]
     });
-    runtimeService = TestBed.get(RuntimeService);
     analyticsService = TestBed.get(AnalyticsService);
-  });
-
-  afterEach(() => {
-    window.digitalData = {} as IDigitalData;
+    windowService = TestBed.get(WindowService);
+    windowService.getWindow.mockReturnValue({});
   });
 
   it('should be created', () => {
     expect(analyticsService).toBeTruthy();
   });
 
-  it('should assign digitalData object to the window if running in browser', () => {
-    runtimeService.isBrowser.mockReturnValue(true);
-
+  it('should assign digitalData object to the window ', () => {
     analyticsService.setup();
 
-    expect(window.digitalData).toBeTruthy();
-  });
-
-  it('should not assign digitalData object to the window if not running in browser', () => {
-    runtimeService.isBrowser.mockReturnValue(false);
-
-    analyticsService.setup();
-
-    expect(window.digitalData).toEqual({});
+    expect(windowService.getWindow().digitalData).toBeTruthy();
   });
 
   it('should push corresponding analytics when weather bar is opened', () => {
-    runtimeService.isBrowser.mockReturnValue(true);
     analyticsService.setup();
-    window.digitalData.events.push = jest.fn();
+    windowService.getWindow().digitalData.events.push = jest.fn();
 
     const event = {
       event: 'weather.location.bar',
@@ -61,16 +47,17 @@ describe('AnalyticsService', () => {
     };
     analyticsService.pushEvent(AnalyticsEventsType.WEATHER_BAR_OPENED);
 
-    expect(window.digitalData.events.push).toHaveBeenCalledWith({
+    expect(
+      windowService.getWindow().digitalData.events.push
+    ).toHaveBeenCalledWith({
       type: 'analytics',
       ...event
     });
   });
 
   it('should push corresponding analytics when weather bar is closed', () => {
-    runtimeService.isBrowser.mockReturnValue(true);
     analyticsService.setup();
-    window.digitalData.events.push = jest.fn();
+    windowService.getWindow().digitalData.events.push = jest.fn();
 
     const event = {
       event: 'weather.location.bar',
@@ -78,32 +65,34 @@ describe('AnalyticsService', () => {
     };
     analyticsService.pushEvent(AnalyticsEventsType.WEATHER_BAR_CLOSED);
 
-    expect(window.digitalData.events.push).toHaveBeenCalledWith({
+    expect(
+      windowService.getWindow().digitalData.events.push
+    ).toHaveBeenCalledWith({
       type: 'analytics',
       ...event
     });
   });
 
   it('should push corresponding analytics when weather bar is closed with exit button', () => {
-    runtimeService.isBrowser.mockReturnValue(true);
     analyticsService.setup();
-    window.digitalData.events.push = jest.fn();
+    windowService.getWindow().digitalData.events.push = jest.fn();
 
     const event = {
       event: 'weather.location.exit'
     };
     analyticsService.pushEvent(AnalyticsEventsType.WEATHER_EXIT_BUTTON);
 
-    expect(window.digitalData.events.push).toHaveBeenCalledWith({
+    expect(
+      windowService.getWindow().digitalData.events.push
+    ).toHaveBeenCalledWith({
       type: 'analytics',
       ...event
     });
   });
 
   it('should push corresponding analytics when weather bar is closed with exit button', () => {
-    runtimeService.isBrowser.mockReturnValue(true);
     analyticsService.setup();
-    window.digitalData.events.push = jest.fn();
+    windowService.getWindow().digitalData.events.push = jest.fn();
 
     const event = {
       event: 'weather.location.change',
@@ -115,64 +104,68 @@ describe('AnalyticsService', () => {
       new Map().set('location', 'Auckland')
     );
 
-    expect(window.digitalData.events.push).toHaveBeenCalledWith({
+    expect(
+      windowService.getWindow().digitalData.events.push
+    ).toHaveBeenCalledWith({
       type: 'analytics',
       ...event
     });
   });
 
   it('should push corresponding analytics when the navigation menu is open', () => {
-    runtimeService.isBrowser.mockReturnValue(true);
     analyticsService.setup();
-    window.digitalData.events.push = jest.fn();
+    windowService.getWindow().digitalData.events.push = jest.fn();
 
     const event = {
       event: 'menu.nav'
     };
     analyticsService.pushEvent(AnalyticsEventsType.MENU_NAV_OPENED);
 
-    expect(window.digitalData.events.push).toHaveBeenCalledWith({
+    expect(
+      windowService.getWindow().digitalData.events.push
+    ).toHaveBeenCalledWith({
       type: 'analytics',
       ...event
     });
   });
 
   it('should push corresponding analytics when the navigation menu is closed ', () => {
-    runtimeService.isBrowser.mockReturnValue(true);
     analyticsService.setup();
-    window.digitalData.events.push = jest.fn();
+    windowService.getWindow().digitalData.events.push = jest.fn();
 
     const event = {
       event: 'close.menu.nav'
     };
     analyticsService.pushEvent(AnalyticsEventsType.MENU_NAV_CLOSED);
 
-    expect(window.digitalData.events.push).toHaveBeenCalledWith({
+    expect(
+      windowService.getWindow().digitalData.events.push
+    ).toHaveBeenCalledWith({
       type: 'analytics',
       ...event
     });
   });
 
   it('should push corresponding analytics when the stuff logo of top menu is clicked ', () => {
-    runtimeService.isBrowser.mockReturnValue(true);
     analyticsService.setup();
-    window.digitalData.events.push = jest.fn();
+    windowService.getWindow().digitalData.events.push = jest.fn();
 
     const event = {
       event: 'stuff.logo'
     };
     analyticsService.pushEvent(AnalyticsEventsType.STUFF_LOGO_CLICKED);
 
-    expect(window.digitalData.events.push).toHaveBeenCalledWith({
+    expect(
+      windowService.getWindow().digitalData.events.push
+    ).toHaveBeenCalledWith({
       type: 'analytics',
       ...event
     });
   });
 
   it('should push corresponding analytics when a link on footer menu is clicked ', () => {
-    runtimeService.isBrowser.mockReturnValue(true);
     analyticsService.setup();
-    window.digitalData.events.push = jest.fn();
+    windowService.getWindow().digitalData.events.push = jest.fn();
 
     const event = {
       event: 'menu.footer',
@@ -183,48 +176,51 @@ describe('AnalyticsService', () => {
       new Map().set('name', 'twitter')
     );
 
-    expect(window.digitalData.events.push).toHaveBeenCalledWith({
+    expect(
+      windowService.getWindow().digitalData.events.push
+    ).toHaveBeenCalledWith({
       type: 'analytics',
       ...event
     });
   });
 
   it('should push corresponding analytics when the breaking news banner is open', () => {
-    runtimeService.isBrowser.mockReturnValue(true);
     analyticsService.setup();
-    window.digitalData.events.push = jest.fn();
+    windowService.getWindow().digitalData.events.push = jest.fn();
 
     const event = {
       event: 'breaking.news.open'
     };
     analyticsService.pushEvent(AnalyticsEventsType.BREAKING_NEWS_OPEN);
 
-    expect(window.digitalData.events.push).toHaveBeenCalledWith({
+    expect(
+      windowService.getWindow().digitalData.events.push
+    ).toHaveBeenCalledWith({
       type: 'analytics',
       ...event
     });
   });
 
   it('should push corresponding analytics when the breaking news banner is closed', () => {
-    runtimeService.isBrowser.mockReturnValue(true);
     analyticsService.setup();
-    window.digitalData.events.push = jest.fn();
+    windowService.getWindow().digitalData.events.push = jest.fn();
 
     const event = {
       event: 'breaking.news.close'
     };
     analyticsService.pushEvent(AnalyticsEventsType.BREAKING_NEWS_CLOSE);
 
-    expect(window.digitalData.events.push).toHaveBeenCalledWith({
+    expect(
+      windowService.getWindow().digitalData.events.push
+    ).toHaveBeenCalledWith({
       type: 'analytics',
       ...event
     });
   });
 
   it('should push corresponding analytics when the more button is clicked', () => {
-    runtimeService.isBrowser.mockReturnValue(true);
     analyticsService.setup();
-    window.digitalData.events.push = jest.fn();
+    windowService.getWindow().digitalData.events.push = jest.fn();
 
     const event = {
       event: 'more.content.button',
@@ -235,7 +231,9 @@ describe('AnalyticsService', () => {
       new Map().set('url', '/national')
     );
 
-    expect(window.digitalData.events.push).toHaveBeenCalledWith({
+    expect(
+      windowService.getWindow().digitalData.events.push
+    ).toHaveBeenCalledWith({
       type: 'analytics',
       ...event
     });
