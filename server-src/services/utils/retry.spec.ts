fb8d47f5c5d636db2ddbe643c61c57ca6011d8b1
Lucas Rainett <lucas.rainett@stuff.co.nz>
2019-05-14 13:59:05 +1200

    shallow update libs

--- server-src/services/utils/retry.spec.ts
+++ server-src/services/utils/retry.spec.ts
@@ -5,9 +5,9 @@ describe('Retry', () => {
   const params: IParams = { apiRequestId: 'request-id-for-testing' };
   it('should succeed after retrying 3 times when the last one resolved', async () => {
     const input = jest.fn();
-    input.mockRejectedValueOnce({ reject: 1 });
-    input.mockRejectedValueOnce({ reject: 2 });
-    input.mockRejectedValueOnce({ reject: 3 });
+    input.mockRejectedValueOnce(new Error('1'));
+    input.mockRejectedValueOnce(new Error('2'));
+    input.mockRejectedValueOnce(new Error('3'));
     input.mockResolvedValue({ resolve: 4 });
 
     await expect(
@@ -19,15 +19,13 @@ describe('Retry', () => {
 
   it('should fail after retry 3 times when last one is still not resolved', async () => {
     const input = jest.fn();
-    input.mockRejectedValueOnce({ reject: 1 });
-    input.mockRejectedValueOnce({ reject: 2 });
-    input.mockRejectedValueOnce({ reject: 3 });
-    input.mockRejectedValueOnce({ reject: 4 });
+    input.mockRejectedValueOnce(new Error('1'));
+    input.mockRejectedValueOnce(new Error('2'));
+    input.mockRejectedValueOnce(new Error('3'));
+    input.mockRejectedValueOnce(new Error('4'));
 
     await expect(
       retry(input, params, { minTimeout: 100, retries: 3 })
-    ).rejects.toMatchObject({
-      reject: 4
-    });
+    ).rejects.toEqual(new Error('4'));
   });
 });
