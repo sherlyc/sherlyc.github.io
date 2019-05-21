a833e01d4402a896c6d3522a3e655f5989b8d3c9
Lucas Rainett <lucas.rainett@stuff.co.nz>
2019-05-21 17:55:17 +1200

    add Browser override Service

---
+++ src/app/services/browser-override/browser-override.service.spec.ts
@@ -0,0 +1,31 @@
+import { TestBed } from '@angular/core/testing';
+import { mockService, ServiceMock } from '../mocks/MockService';
+import { WindowService } from '../window/window.service';
+import { BrowserOverrideService } from './browser-override.service';
+
+describe('Browser Override Service', () => {
+  let windowService: ServiceMock<WindowService>;
+  let browserOverrideService: ServiceMock<BrowserOverrideService>;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: WindowService,
+          useClass: mockService(WindowService)
+        }
+      ]
+    });
+    windowService = TestBed.get(WindowService);
+    windowService.getWindow.mockReturnValue({});
+    browserOverrideService = TestBed.get(BrowserOverrideService);
+  });
+
+  it('should override native browser functions', () => {
+    const window = windowService.getWindow();
+    expect(window.alert).toBeFalsy();
+    expect(window.confirm).toBeFalsy();
+    browserOverrideService.setup();
+    expect(window.alert).toBeTruthy();
+    expect(window.confirm).toBeTruthy();
+  });
+});
