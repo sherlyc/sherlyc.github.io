eda5b93729fd98d7aa8902818e8ed1a0d5d87a1c
Karen Xie <karen.xie@stuff.co.nz>
2019-05-22 15:34:40 +1200

    Make timeout 60000 and progress commit for playing ads on video player e2e test

--- e2e/homepage.spec.ts
+++ e2e/homepage.spec.ts
@@ -2,7 +2,7 @@ import { getDriver } from './driver';
 import { By, until, WebDriver } from 'selenium-webdriver';
 import config from './spaceConfig';
 
-describe('Homepage test', () => {
+describe('Homepage', () => {
   let driver: WebDriver;
   beforeAll(async () => {
     driver = await getDriver();
@@ -31,7 +31,7 @@ describe('Homepage test', () => {
       const image = await article.findElement(By.css('img'));
       expect(image).toBeTruthy();
     }
-  }, 60000);
+  });
 
   it('should navigate to article page when articles are clicked', async () => {
     await driver.get(config.url);
@@ -43,12 +43,24 @@ describe('Homepage test', () => {
     link.click();
     await driver.wait(until.elementLocated(By.css('app-basic-article-unit')));
     expect(await driver.getCurrentUrl()).toBe(href);
-  }, 60000);
+  });
 
   it('should display basic ad units', async () => {
     await driver.get(config.url);
     await driver.wait(until.elementLocated(By.css('app-basic-ad-unit')));
     const ads = await driver.findElements(By.css('app-basic-ad-unit'));
     expect(ads.length).toBeGreaterThan(0);
-  }, 60000);
+  });
+
+  it.only('should display ad when video is clicked', async () => {
+    await driver.get(config.url);
+    await driver.wait(until.elementLocated(By.css('app-video-unit')));
+
+    // NOT WORKING - click on play button
+
+    // const playButton = await driver.findElement(By.css('.vjs-big-play-button'));
+    // playButton.click();
+
+    // Check ad is displayed
+  });
 });
