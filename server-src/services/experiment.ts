e884352b2dd386d251b3187663aca5475727d2ed
German Valdes <german.valdes@stuff.co.nz>
2019-05-24 09:53:07 +1200

    EX-396: Fixed experiment ranges

--- server-src/services/experiment.ts
+++ server-src/services/experiment.ts
@@ -1,20 +1,7 @@
-function validateLottery(name: string, lotteryNumber: number) {
-  if (!lotteryNumber || lotteryNumber > 100 || lotteryNumber < 0) {
-    throwException(name, lotteryNumber);
-  }
-}
-
-function throwException(name: string, lotteryNumber: number) {
-  throw Error(
-    `bad experiment data provided, name[${name}], lotteryNumber[${lotteryNumber}]`
-  );
-}
-
 export const experimentService = (
   name: string,
   lotteryNumber: number
 ): string => {
-  validateLottery(name, lotteryNumber);
 
   switch (name) {
     case 'Users':
@@ -26,24 +13,24 @@ export const experimentService = (
         return 'control';
       }
     case 'Parrot':
-      if (lotteryNumber < 34) {
+      if (lotteryNumber <= 32) {
         return 'redHeadline';
-      } else if (lotteryNumber > 34 && lotteryNumber < 68) {
+      } else if (lotteryNumber >= 33 && lotteryNumber <= 66) {
         return 'greenHeadline';
       } else {
         return 'control';
       }
       break;
     case 'Toucan':
-      if (lotteryNumber < 34) {
+      if (lotteryNumber < 32) {
         return 'purpleHeadline';
-      } else if (lotteryNumber > 34 && lotteryNumber < 68) {
+      } else if (lotteryNumber >= 33 && lotteryNumber <= 66) {
         return 'orangeHeadline';
       } else {
         return 'control';
       }
     default:
-      throwException(name, lotteryNumber);
+      throw Error(`Bad experiment input data provided: name [${name}], lotteryNumber [${lotteryNumber}]`);
       return '';
   }
 };
