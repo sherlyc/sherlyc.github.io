5fbdf3cb9f6af78626315c5b06c5f5633c04a3ae
German Valdes <german.valdes@stuff.co.nz>
2019-05-24 10:37:32 +1200

    EX-396: Experiment controller responds with variant

--- server-src/api/experiment-controller.spec.ts
+++ server-src/api/experiment-controller.spec.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express';
-import { experimentController } from '../api/experiment';
+import { experimentController } from './experiment-controller';
+import { getExperimentVariant } from '../services/experiment';
+jest.mock('../services/experiment');
 
 describe('Experiment controller', () => {
   const req = {
@@ -12,7 +14,7 @@ describe('Experiment controller', () => {
     res.status.mockReturnValue(res);
   });
 
-  it('should handle errors when provided with empty name and negative lottery number', () => {
+  it('should handle errors when provided with empty name and negative lottery number and return 400', () => {
     req.query.name = '';
     req.query.lotteryNumber = -1;
 
@@ -20,17 +22,27 @@ describe('Experiment controller', () => {
     assertIs400Status(res, req);
   });
 
-  it('should handle errors when provided with invalid number', () => {
+  it('should handle errors when provided with invalid number and return 400', () => {
     req.query.name = 'afjdjafia';
     req.query.lotteryNumber = -8;
 
     experimentController(req, res);
     assertIs400Status(res, req);
   });
+
+  it('should respond with variant', () => {
+    req.query.name = 'Users';
+    req.query.lotteryNumber = 27;
+
+    (getExperimentVariant as jest.Mock).mockReturnValue('A');
+
+    experimentController(req, res);
+    expect(res.send).toHaveBeenCalledWith('A');
+  });
 });
 
 function assertIs400Status(res: Response, req: Request) {
   expect(res.status).toHaveBeenCalledWith(400);
   expect(res.send).toHaveBeenCalledWith(`bad experiment data provided,
-     name[${req.query.name}], lotteryNumber[${req.query.lotteryNumber}]`);
+     name [${req.query.name}], lotteryNumber [${req.query.lotteryNumber}]`);
 }
--- server-src/api/experiment-controller.ts
+++ server-src/api/experiment-controller.ts
@@ -1,13 +1,17 @@
 import { Request, Response } from 'express';
+import { getExperimentVariant } from '../services/experiment';
 
 function validateRequest(name: string, lotteryNumber: number, res: Response) {
   if ((!name && !lotteryNumber) || lotteryNumber > 100 || lotteryNumber < 0) {
     res.status(400).send(`bad experiment data provided,
-     name[${name}], lotteryNumber[${lotteryNumber}]`);
+     name [${name}], lotteryNumber [${lotteryNumber}]`);
   }
 }
 
 export const experimentController = function(req: Request, res: Response) {
   const { name, lotteryNumber } = req.query;
   validateRequest(name, lotteryNumber, res);
+
+  const variant = getExperimentVariant(name, lotteryNumber);
+  res.send(variant);
 };
