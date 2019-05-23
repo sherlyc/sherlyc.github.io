cd36ff3e51ecf52a3a9301afcb59fdbfc3e496cc
German Valdes <german.valdes@stuff.co.nz>
2019-05-24 09:51:57 +1200

    EX-396: Added test to handle non-existent experiment

--- server-src/services/experiment.spec.ts
+++ server-src/services/experiment.spec.ts
@@ -2,28 +2,40 @@ import { experimentService } from './experiment';
 
 describe('Experiment service', () => {
   let experimentName = 'Parrot';
+  let lotteryNumber = 0;
 
-  it('should assign variant of redHeadline when lottery number is between 0 and 33', () => {
-    const lotteryNumber = 33;
+  it('should assign variant of redHeadline when lottery number is between 0 and 32', () => {
+    lotteryNumber = 32;
     const variant = 'redHeadline';
 
     expect(experimentService(experimentName, lotteryNumber)).toEqual(variant);
   });
 
-  it('should assign variant of greenHeadline when lottery number is between 34 and 67', () => {
-    const lotteryNumber = 67;
+  it('should assign variant of greenHeadline when lottery number is between 33 and 66', () => {
+    lotteryNumber = 66;
     const variant = 'greenHeadline';
 
     expect(experimentService(experimentName, lotteryNumber)).toEqual(variant);
   });
 
-  it('should assign variant Control when lottery number is between 68 and 99', () => {
-    let lotteryNumber = 68;
+  it('should assign variant Control when lottery number is between 67 and 99', () => {
+    lotteryNumber = 67;
     const variant = 'control';
 
     expect(experimentService(experimentName, lotteryNumber)).toEqual(variant);
 
     lotteryNumber = 99;
     expect(experimentService(experimentName, lotteryNumber)).toEqual(variant);
+  });
+
+  it('should throw an error when the experiment does not exists', () => {
+    experimentName = 'Eagle';
+    lotteryNumber = 23;
+
+    expect(() => {
+      experimentService(experimentName, lotteryNumber);
+    }).toThrowError(
+      `Bad experiment input data provided: name [Eagle], lotteryNumber [23]`
+    );
   });
 });
