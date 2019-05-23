39e3b64aef44ac8e5e4b515daf04313a0ce2b698
German Valdes <german.valdes@stuff.co.nz>
2019-05-23 18:01:41 +1200

    EX-396: Experiment controller extracted validation logic to function

--- server-src/api/experiment.spec.ts
+++ server-src/api/experiment.spec.ts
@@ -1,36 +1,36 @@
-import {Request, Response} from "express";
-import {experimentController} from "../api/experiment";
+import { Request, Response } from 'express';
+import { experimentController } from '../api/experiment';
 
 describe('Experiment controller', () => {
-  const req = { query: { name: '', lotteryNumber: ''}, cookies: {} } as Request;
-  const res = { sendStatus: jest.fn(), end: jest.fn()} as any;
+  const req = {
+    query: { name: '', lotteryNumber: '' },
+    cookies: {}
+  } as Request;
+  const res = { send: jest.fn(), status: jest.fn(), end: jest.fn() } as any;
 
-  it('should handle errors when provided with bad data', () => {
+  beforeEach(() => {
+    res.status.mockReturnValue(res);
+  });
+
+  it('should handle errors when provided with empty name and negative lottery number', () => {
     req.query.name = '';
     req.query.lotteryNumber = -1;
 
-    expect(() => {
-      experimentController(req, res);
-    }).toThrowError(
-      `bad experiment data provided, name[${req.query.name}], lotteryNumber[${req.query.lotteryNumber}]`
-    );
+    experimentController(req, res);
+    assertIs400Status(res, req);
+  });
 
+  it('should handle errors when provided with invalid number', () => {
     req.query.name = 'afjdjafia';
-    req.query.lotteryNumber = 8;
-
-    expect(() => {
-      experimentController(req, res);
-    }).toThrowError(
-      `bad experiment data provided, name[${req.query.name}], lotteryNumber[${req.query.lotteryNumber}]`
-    );
-
-    req.query.name = 'backgroundColor';
     req.query.lotteryNumber = -8;
 
-    expect(() => {
-      experimentController(req, res)
-    }).toThrowError(
-      `bad experiment data provided, name[${req.query.name}], lotteryNumber[${req.query.lotteryNumber}]`
-    );
+    experimentController(req, res);
+    assertIs400Status(res, req);
   });
-})
+});
+
+function assertIs400Status(res: Response, req: Request) {
+  expect(res.status).toHaveBeenCalledWith(400);
+  expect(res.send).toHaveBeenCalledWith(`bad experiment data provided,
+     name[${req.query.name}], lotteryNumber[${req.query.lotteryNumber}]`);
+}
--- server-src/api/experiment.ts
+++ server-src/api/experiment.ts
@@ -1,8 +1,13 @@
 import { Request, Response } from 'express';
 
-export const experimentController = function(req: Request, res: Response){
-  const { name, lotteryNumber } = req.query;
-  if(!name && !lotteryNumber || lotteryNumber > 100 || lotteryNumber < 0 ) {
-    res.sendStatus(400)
+function validateRequest(name: string, lotteryNumber: number, res: Response) {
+  if ((!name && !lotteryNumber) || lotteryNumber > 100 || lotteryNumber < 0) {
+    res.status(400).send(`bad experiment data provided,
+     name[${name}], lotteryNumber[${lotteryNumber}]`);
   }
 }
+
+export const experimentController = function(req: Request, res: Response) {
+  const { name, lotteryNumber } = req.query;
+  validateRequest(name, lotteryNumber, res);
+};
