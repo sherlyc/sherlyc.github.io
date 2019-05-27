bdb47bf612c92cbc06a0613da57a14c83f34d4d0
Viet Anh Tran <viet.tran@fairfaxmedia.co.nz>
2019-05-27 15:56:30 +1200

    EX-396: Set up experiment service with random number generator

--- src/app/services/experiment/experiment.service.spec.ts
+++ src/app/services/experiment/experiment.service.spec.ts
@@ -1,32 +1,48 @@
-import { ConfigService } from './../config/config.service';
+import { ConfigService } from '../config/config.service';
 import { TestBed } from '@angular/core/testing';
 
 import { ExperimentService } from './experiment.service';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { mockService, ServiceMock } from '../mocks/MockService';
+import { StoreService } from '../store/store.service';
+import * as random from 'math-random';
+import { HttpClient } from '@angular/common/http';
+import { of } from 'rxjs';
+
+jest.mock('math-random');
 
 describe('ExperimentService', () => {
   const experimentAPI = '/spade/api/experiment';
-  let httpMock: HttpTestingController;
   let configServiceMock: ServiceMock<ConfigService>;
+  let service: ExperimentService;
+  let storeService: ServiceMock<StoreService>;
+  let httpClient: ServiceMock<HttpClient>;
 
-  beforeEach(() =>  {
+  beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
+      imports: [],
       providers: [
         {
           provide: ConfigService,
           useClass: mockService(ConfigService)
+        },
+        {
+          provide: StoreService,
+          useClass: mockService(StoreService)
+        },
+        {
+          provide: HttpClient,
+          useClass: mockService(HttpClient)
         }
       ]
     });
-    httpMock = TestBed.get(HttpTestingController);
     configServiceMock = TestBed.get(ConfigService);
     configServiceMock.getConfig.mockReturnValue({ experimentAPI });
+    service = TestBed.get(ExperimentService);
+    storeService = TestBed.get(StoreService);
+    httpClient = TestBed.get(HttpClient);
   });
 
   it('should be created', () => {
-    const service: ExperimentService = TestBed.get(ExperimentService);
     expect(service).toBeTruthy();
   });
 
@@ -35,19 +51,59 @@ describe('ExperimentService', () => {
     const lotteryNumber = 50;
     const expectedVariant = 'redHighlight';
 
-    const service: ExperimentService = TestBed.get(ExperimentService);
+    httpClient.get.mockReturnValue(of(expectedVariant));
 
-    service
-      .getVariant(experiment, lotteryNumber)
-      .subscribe((response) => {
-        expect(response).toEqual(expectedVariant);
-        done();
-      });
-
-    const req = httpMock.expectOne(
-      `${experimentAPI}?name=${experiment}&lotteryNumber=${lotteryNumber}`
-    );
-    expect(req.request.method).toBe('GET');
-    req.flush(expectedVariant);
+    service.getVariant(experiment, lotteryNumber).subscribe((response) => {
+      expect(response).toEqual(expectedVariant);
+      done();
     });
+  });
+
+  it('should return a new random number if it does not exist in storage service', () => {
+    (random as jest.Mock).mockReturnValue(0.38);
+    storeService.get.mockReturnValue(undefined);
+
+    const experimentName = 'experimentName';
+    const response = service.getRandomNumber(experimentName);
+
+    expect(response).toEqual(38);
+    expect(storeService.set).toHaveBeenCalledWith(
+      `${experimentName}ExperimentLottery`,
+      response
+    );
+  });
+
+  it('should return existing random number if it exists in storage service', () => {
+    storeService.get.mockReturnValue(55);
+
+    const response = service.getRandomNumber('storageKey');
+
+    expect(response).toEqual(55);
+  });
+
+  it('should set up experiment information when not in control group', () => {
+    storeService.get.mockReturnValue(undefined);
+    const experimentName = 'FakeExperiment';
+    const variant = 'A';
+    (random as jest.Mock).mockReturnValue(0.38);
+    httpClient.get.mockReturnValueOnce(of(experimentName));
+    httpClient.get.mockReturnValueOnce(of(variant));
+
+    service.setUp();
+
+    expect(service.experiment.name).toEqual(experimentName);
+    expect(service.experiment.variant).toEqual(variant);
+  });
+
+  it('should set up experiment information when in control group', () => {
+    storeService.get.mockReturnValue(undefined);
+    const experimentName = 'control';
+    (random as jest.Mock).mockReturnValue(0.38);
+    httpClient.get.mockReturnValueOnce(of(experimentName));
+
+    service.setUp();
+
+    expect(service.experiment.name).toEqual(experimentName);
+    expect(service.experiment.variant).toEqual(undefined);
+  });
 });
--- src/app/services/experiment/experiment.service.ts
+++ src/app/services/experiment/experiment.service.ts
@@ -1,18 +1,65 @@
 import { Injectable } from '@angular/core';
-import { Observable, of, Subscriber } from 'rxjs';
+import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import * as random from 'math-random';
 import { ConfigService } from '../config/config.service';
+import { StoreService } from '../store/store.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExperimentService {
-  constructor(private http: HttpClient, private config: ConfigService) { }
+  experiment: {
+    name: string;
+    variant?: string;
+  } = { name: 'control' };
+  constructor(
+    private http: HttpClient,
+    private config: ConfigService,
+    private storeService: StoreService
+  ) {}
 
   getVariant(experiment: string, lotteryNumber: number): Observable<string> {
     return new Observable((subscriber) => {
-      this.http.get(`${this.config.getConfig().experimentAPI}?name=${experiment}&lotteryNumber=${lotteryNumber}`)
-      .subscribe((response))
+      this.http
+        .get<string>(
+          `${
+            this.config.getConfig().experimentAPI
+          }?name=${experiment}&lotteryNumber=${lotteryNumber}`
+        )
+        .subscribe((result) => {
+          subscriber.next(result);
+          subscriber.complete();
+        });
+    });
+  }
+
+  getRandomNumber(experimentName: string): number {
+    const experimentStorageKey = `${experimentName}ExperimentLottery`;
+    const existingLotteryNumber = this.storeService.get<number>(
+      experimentStorageKey
+    );
+    if (existingLotteryNumber) {
+      return existingLotteryNumber;
+    }
+    const newLotteryNumber = Math.floor(random() * 100);
+    this.storeService.set(experimentStorageKey, newLotteryNumber);
+    return newLotteryNumber;
+  }
+
+  setUp() {
+    const userLotteryNumber = this.getRandomNumber('Users');
+    this.getVariant('Users', userLotteryNumber).subscribe((experimentName) => {
+      this.experiment.name = experimentName;
+      if (experimentName === 'control') {
+        return;
+      }
+      const variantLotteryNumber = this.getRandomNumber(experimentName);
+      this.getVariant(experimentName, variantLotteryNumber).subscribe(
+        (variant) => {
+          this.experiment.variant = variant;
+        }
+      );
     });
   }
 }
