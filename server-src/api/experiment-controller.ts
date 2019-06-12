a10483e0a541a54e83566d37f6f30e0b9d583648
German Valdes <german.valdes@stuff.co.nz>
2019-06-13 11:02:59 +1200

    EX-510: Basic error logging in experiment and weather controllers

--- server-src/api/experiment-controller.ts
+++ server-src/api/experiment-controller.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express';
 import { getExperimentVariant } from '../services/experiment';
+import logger from '../services/utils/logger';
+import { IParams } from '../services/__types__/IParams';
 
 function validateRequest(name: string, lotteryNumber: number) {
   if (!name || !lotteryNumber || lotteryNumber > 100 || lotteryNumber < 0) {
@@ -8,7 +10,7 @@ function validateRequest(name: string, lotteryNumber: number) {
   }
 }
 
-export const experimentController = function(req: Request, res: Response) {
+export const experimentController = function(req: Request, res: Response, params: IParams) {
   const { name, lotteryNumber } = req.query;
 
   try {
@@ -16,6 +18,7 @@ export const experimentController = function(req: Request, res: Response) {
     const variant = getExperimentVariant(name, lotteryNumber);
     res.send(variant);
   } catch (e) {
+    logger.error(params.apiRequestId, e.message);
     res.status(400).send(e.message);
   }
 };
--- server-src/api/weather.ts
+++ server-src/api/weather.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from 'express';
 import extractParams from '../services/params-extractor';
 import { weatherService } from '../services/adapters/weather';
+import logger from '../services/utils/logger';
+import { IParams } from '../services/__types__/IParams';
 
-export const getWeather = async (req: Request, res: Response) => {
+export const getWeather = async (req: Request, res: Response, params: IParams ) => {
   const location = req.query.location;
   if (location) {
     try {
@@ -10,9 +12,11 @@ export const getWeather = async (req: Request, res: Response) => {
       res.json(weatherData);
       res.end();
     } catch (error) {
+      logger.error(params.apiRequestId, error.message);
       res.sendStatus(500);
     }
   } else {
+    logger.warn(params.apiRequestId, '400 error on backend Weather service');
     res.sendStatus(400);
   }
 };
