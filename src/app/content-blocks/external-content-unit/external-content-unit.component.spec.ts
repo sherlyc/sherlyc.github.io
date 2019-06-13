3e59e9006ef77c97dcc334026a1b578217ca27ba
Karen Xie <karen.xie@stuff.co.nz>
2019-06-13 15:58:30 +1200

    EX-503: Delete unused dependencies from external unit component

--- src/app/content-blocks/external-content-unit/external-content-unit.component.spec.ts
+++ src/app/content-blocks/external-content-unit/external-content-unit.component.spec.ts
@@ -2,15 +2,10 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ExternalContentUnitComponent } from './external-content-unit.component';
 import { DomSanitizer } from '@angular/platform-browser';
 import { ContentBlockType } from '../../../../common/__types__/ContentBlockType';
-import { ScriptInjectorService } from '../../services/script-injector/script-injector.service';
-import { mockService, ServiceMock } from '../../services/mocks/MockService';
-import { RuntimeService } from '../../services/runtime/runtime.service';
 
 describe('ExternalContentUnitComponent', () => {
   let component: ExternalContentUnitComponent;
   let fixture: ComponentFixture<ExternalContentUnitComponent>;
-  let scriptInjectorService: ServiceMock<ScriptInjectorService>;
-  let runtimeService: ServiceMock<RuntimeService>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -21,21 +16,11 @@ describe('ExternalContentUnitComponent', () => {
             sanitize: () => 'safe',
             bypassSecurityTrustResourceUrl: (url: string) => url
           }
-        },
-        {
-          provide: ScriptInjectorService,
-          useClass: mockService(ScriptInjectorService)
-        },
-        {
-          provide: RuntimeService,
-          useClass: mockService(RuntimeService)
         }
       ],
       declarations: [ExternalContentUnitComponent]
     }).compileComponents();
 
-    scriptInjectorService = TestBed.get(ScriptInjectorService);
-    runtimeService = TestBed.get(RuntimeService);
     fixture = TestBed.createComponent(ExternalContentUnitComponent);
     component = fixture.componentInstance;
   });
