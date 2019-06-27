95af561a89107e041d6bf0b385b1863c78a1c106
German Valdes <german.valdes@stuff.co.nz>
2019-06-27 12:12:58 +1200

    EX-405: removed cat funny gif

--- src/app/shared/components/authentication/signin-callback.component.ts
+++ src/app/shared/components/authentication/signin-callback.component.ts
@@ -3,12 +3,7 @@ import { AuthenticationService } from '../../../services/authentication/authenti
 
 @Component({
   selector: 'app-signin-callback',
-  template:
-    '<h3> Redirecting ... </h3> ' +
-    '<div style="width:100%;height:0;padding-bottom:70%;position:relative;">' +
-    '<iframe src="https://giphy.com/embed/w9yg6QsZJ3JC" width="100%" ' +
-    'height="20%" style="position:absolute" frameBorder="0" ' +
-    'class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/w9yg6QsZJ3JC">via GIPHY</a></p>'
+  template: '<h4> Redirecting ... </h4>'
 })
 export class SigninCallbackComponent implements OnInit {
   constructor(private authenticationService: AuthenticationService) {}
