a399d5b98d824d6c7cabd1557042e23d44c1ddbe
German Valdes <german.valdes@stuff.co.nz>
2019-06-29 13:36:57 +1200

    EX-405: Authentication service type safer

---
+++ src/app/services/authentication/__types__/IStuffLoginUser.ts
@@ -0,0 +1,20 @@
+export interface IStuffLoginUser {
+  'id_token': string;
+  'access_token': string;
+  'profile': {
+    'sub': string;
+    'auth_time': number;
+    'kid': string;
+    'jti': string;
+    'name': string;
+    'preferred_username': string;
+    'given_name': string;
+    'family_name': string;
+    'nickname': string;
+    'profile': string;
+    'picture': string;
+    'gender': string;
+    'locale': string;
+    'birthdate': string
+  };
+}
