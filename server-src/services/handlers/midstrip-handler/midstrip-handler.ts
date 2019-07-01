db24a8bfacf2781bf4fac13f6bb8f4c379dae763
Sherly Chan <sherly.chan@fairfaxmedia.co.nz>
2019-07-01 13:48:47 +1200

    EX-532: error handling for midstrip handler

--- server-src/services/handlers/midstrip-handler/midstrip-handler.ts
+++ server-src/services/handlers/midstrip-handler/midstrip-handler.ts
@@ -6,37 +6,46 @@ import { handlerRunnerFunction } from '../runner';
 import { IImageLinkUnit } from '../../../../common/__types__/IImageLinkUnit';
 import { IParams } from '../../__types__/IParams';
 import { ListAsset } from '../../listAsset';
+import logger from '../../utils/logger';
 
 export default async function(
   handlerRunner: handlerRunnerFunction,
   { totalArticles, strapName }: IMidStripHandlerInput,
   params: IParams
 ): Promise<IContentBlock[]> {
-  const rawArticles = (await getListAsset(
-    params,
-    ListAsset.MidStrip,
-    totalArticles
-  )).slice(0, totalArticles);
+  try {
+    const rawArticles = (await getListAsset(
+      params,
+      ListAsset.MidStrip,
+      totalArticles
+    )).slice(0, totalArticles);
 
-  return [
-    {
-      type: ContentBlockType.ColumnContainer,
-      items: rawArticles.reduce(
-        (final, article) => [
-          ...final,
-          {
-            type: ContentBlockType.ImageLinkUnit,
-            id: article.id,
-            strapName: strapName,
-            indexHeadline: article.indexHeadline,
-            imageSrc: article.imageSrc,
-            imageSrcSet: article.imageSrcSet,
-            linkUrl: article.linkUrl,
-            headlineFlags: article.headlineFlags
-          } as IImageLinkUnit
-        ],
-        [] as IContentBlock[]
-      )
-    }
-  ];
+    return [
+      {
+        type: ContentBlockType.ColumnContainer,
+        items: rawArticles.reduce(
+          (final, article) => [
+            ...final,
+            {
+              type: ContentBlockType.ImageLinkUnit,
+              id: article.id,
+              strapName: strapName,
+              indexHeadline: article.indexHeadline,
+              imageSrc: article.imageSrc,
+              imageSrcSet: article.imageSrcSet,
+              linkUrl: article.linkUrl,
+              headlineFlags: article.headlineFlags
+            } as IImageLinkUnit
+          ],
+          [] as IContentBlock[]
+        )
+      }
+    ];
+  } catch (e) {
+    logger.error(
+      params.apiRequestId,
+      `Midstrip handler error for ${strapName} - ${e}`
+    );
+    return [];
+  }
 }
