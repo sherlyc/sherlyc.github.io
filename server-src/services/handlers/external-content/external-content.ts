f069de62907bf99256f3453b0f3e568788b1875f
Sherly Chan <sherly.chan@fairfaxmedia.co.nz>
2019-07-01 13:57:45 +1200

    handle errors for external content handler

--- server-src/services/handlers/external-content/external-content.ts
+++ server-src/services/handlers/external-content/external-content.ts
@@ -3,18 +3,28 @@ import { ContentBlockType } from '../../../../common/__types__/ContentBlockType'
 import { handlerRunnerFunction } from '../runner';
 import { IParams } from '../../__types__/IParams';
 import { IExternalContentHandlerInput } from '../__types__/IExternalContentHandlerInput';
+import logger from '../../utils/logger';
 
 export default async function(
   handlerRunner: handlerRunnerFunction,
   { url, height, width }: IExternalContentHandlerInput,
   params: IParams
 ): Promise<IContentBlock[]> {
-  return [
-    {
-      type: ContentBlockType.ExternalContentUnit,
-      url,
-      width,
-      height
-    }
-  ];
+  try {
+    return [
+      {
+        type: ContentBlockType.ExternalContentUnit,
+        url,
+        width,
+        height
+      }
+    ];
+  } catch (e) {
+    logger.error(
+      params.apiRequestId,
+      `External content handler error for ${url} - ${e}`
+    );
+    return [];
+  }
+
 }
