7e06bf6d1accd7b9a296e67e670a2fbf276a10a7
Sherly Chan <sherly.chan@fairfaxmedia.co.nz>
2019-07-01 17:37:12 +1200

    EX-532: revert external content handler

--- server-src/services/handlers/external-content/external-content.ts
+++ server-src/services/handlers/external-content/external-content.ts
@@ -3,28 +3,18 @@ import { ContentBlockType } from '../../../../common/__types__/ContentBlockType'
 import { handlerRunnerFunction } from '../runner';
 import { IParams } from '../../__types__/IParams';
 import { IExternalContentHandlerInput } from '../__types__/IExternalContentHandlerInput';
-import logger from '../../utils/logger';
 
 export default async function(
   handlerRunner: handlerRunnerFunction,
   { url, height, width }: IExternalContentHandlerInput,
   params: IParams
 ): Promise<IContentBlock[]> {
-  try {
-    return [
-      {
-        type: ContentBlockType.ExternalContentUnit,
-        url,
-        width,
-        height
-      }
-    ];
-  } catch (e) {
-    logger.error(
-      params.apiRequestId,
-      `External content handler error for ${url} - ${e}`
-    );
-    return [];
-  }
-
+  return [
+    {
+      type: ContentBlockType.ExternalContentUnit,
+      url,
+      width,
+      height
+    }
+  ];
 }
