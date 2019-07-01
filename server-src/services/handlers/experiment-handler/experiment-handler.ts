754f9f66ca63bb8f05f49efa410e88344987d645
Sherly Chan <sherly.chan@fairfaxmedia.co.nz>
2019-07-01 13:57:30 +1200

    handle errors for experiment handler

--- server-src/services/handlers/experiment-handler/experiment-handler.ts
+++ server-src/services/handlers/experiment-handler/experiment-handler.ts
@@ -4,36 +4,44 @@ import { handlerRunnerFunction } from '../runner';
 import { IParams } from '../../__types__/IParams';
 import { ContentBlockType } from '../../../../common/__types__/ContentBlockType';
 import { IExperimentContainer } from '../../../../common/__types__/IExperimentContainer';
+import logger from '../../utils/logger';
 
-export default async function(
+export default async function (
   handlerRunner: handlerRunnerFunction,
   { name, variants }: IExperimentHandlerInput,
   params: IParams
 ): Promise<IContentBlock[]> {
-  const variantNames = Object.keys(variants);
+  try {
+    const variantNames = Object.keys(variants);
+    const contentBlockPromises: Promise<{
+      [key: string]: IContentBlock[];
+    }>[] = variantNames.map(async (variantName) => {
+      const contentBlocksForVariant = await handlerRunner(
+        variants[variantName],
+        params
+      );
+      return {
+        [variantName]: contentBlocksForVariant
+      };
+    });
 
-  const contentBlockPromises: Promise<{
-    [key: string]: IContentBlock[];
-  }>[] = variantNames.map(async (variantName) => {
-    const contentBlocksForVariant = await handlerRunner(
-      variants[variantName],
-      params
+    const variantBlocks = (await Promise.all(contentBlockPromises)).reduce(
+      (final, item) => ({ ...final, ...item }),
+      {}
     );
-    return {
-      [variantName]: contentBlocksForVariant
-    };
-  });
 
-  const variantBlocks = (await Promise.all(contentBlockPromises)).reduce(
-    (final, item) => ({ ...final, ...item }),
-    {}
-  );
-
-  return [
-    {
-      type: ContentBlockType.ExperimentContainer,
-      name,
-      variants: variantBlocks
-    } as IExperimentContainer
-  ];
+    return [
+      {
+        type: ContentBlockType.ExperimentContainer,
+        name,
+        variants: variantBlocks
+      } as IExperimentContainer
+    ];
+  } catch (e) {
+    logger.error(
+      params.apiRequestId,
+      `Experiment handler error for ${name} - ${e}`
+    );
+    return [];
+  }
 }
