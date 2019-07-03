b8393c02027143b20ad40f3b6dc6994be09b0114
Sherly Chan <sherly.chan@fairfaxmedia.co.nz>
2019-07-03 17:02:56 +1200

    EX-530: dont mock the handler runner

--- server-src/services/handlers/top-stories/top-stories.spec.ts
+++ server-src/services/handlers/top-stories/top-stories.spec.ts
@@ -1,5 +1,73 @@
+import { HandlerInputType } from '../__types__/HandlerInputType';
+import { IParams } from '../../../services/__types__/IParams';
+import topStoriesHandler from './top-stories';
+import { ITopStoriesHandlerInput } from '../__types__/ITopStoriesHandlerInput';
+import { getListAsset } from '../../adapters/jsonfeed';
+import handlerRunner from '../runner';
+
+jest.mock('../../adapters/jsonfeed');
+
 describe('TopStoriesHandler', () => {
-  it('should return ', () => {
-    expect(true).toBe(true);
+  const params: IParams = { apiRequestId: 'request-id-for-testing' };
+
+  const basicAdUnit = {
+    type: 'BasicAdUnit'
+  };
+
+  const article = {
+    id: '1',
+    indexHeadline: 'Headline 1',
+    introText: 'Intro 1',
+    linkUrl: '/link1',
+    imageSrc: '1.jpg',
+    imageSrcSet: '1.jpg 1w',
+    lastPublishedTime: 1,
+    headlineFlags: []
+  };
+
+  const articleAsBasicArticle = {
+    id: '1',
+    strapName: 'Latest',
+    headlineFlags: [],
+    imageSrc: '1.jpg',
+    imageSrcSet: '1.jpg 1w',
+    indexHeadline: 'Headline 1',
+    introText: 'Intro 1',
+    lastPublishedTime: 1,
+    linkUrl: '/link1',
+    type: 'BasicArticleUnit'
+  };
+
+  beforeEach(() => {
+    jest.resetModules();
+  });
+
+  it('should return default layout', async () => {
+    const handlerInput: ITopStoriesHandlerInput = {
+      type: HandlerInputType.TopStories,
+      strapName: 'Latest',
+      totalBasicArticlesUnit: 3
+    };
+    const rawArticles = [article, article, article];
+
+    (getListAsset as jest.Mock).mockResolvedValue(rawArticles);
+
+    const expectedContentBlocks = [
+      basicAdUnit,
+      articleAsBasicArticle,
+      basicAdUnit,
+      articleAsBasicArticle,
+      basicAdUnit,
+      articleAsBasicArticle,
+      basicAdUnit
+    ];
+
+    const contentBlocks = await topStoriesHandler(
+      handlerRunner,
+      handlerInput,
+      params
+    );
+
+    expect(contentBlocks).toEqual(expectedContentBlocks);
   });
 });
--- server-src/services/handlers/top-stories/top-stories.ts
+++ server-src/services/handlers/top-stories/top-stories.ts
@@ -1,5 +1,27 @@
 import { IContentBlock } from 'common/__types__/IContentBlock';
+import { ITopStoriesHandlerInput } from '../__types__/ITopStoriesHandlerInput';
+import { handlerRunnerFunction } from '../runner';
+import { IParams } from '../../../services/__types__/IParams';
+import { HandlerInputType } from '../__types__/HandlerInputType';
+import { ListAsset } from '../../../services/listAsset';
 
-export default async function(): Promise<IContentBlock[]> {
-  return {} as IContentBlock[];
+export default async function(
+  handlerRunner: handlerRunnerFunction,
+  {
+    strapName,
+    totalBasicArticlesUnit = 0,
+    totalBasicArticleTitleUnit = 0
+  }: ITopStoriesHandlerInput,
+  params: IParams
+): Promise<IContentBlock[]> {
+  return handlerRunner(
+    {
+      type: HandlerInputType.ArticleList,
+      sourceId: ListAsset.TopStories,
+      strapName,
+      totalBasicArticlesUnit,
+      totalBasicArticleTitleUnit
+    },
+    params
+  );
 }
