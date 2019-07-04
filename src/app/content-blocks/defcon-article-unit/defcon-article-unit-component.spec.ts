f0025ab10e4dbe59bb1c29bb5b94a4d753b5b1a0
Karen Xie <karen.xie@stuff.co.nz>
2019-07-05 09:16:00 +1200

    EX-530: Add time ago

--- src/app/content-blocks/defcon-article-unit/defcon-article-unit-component.spec.ts
+++ src/app/content-blocks/defcon-article-unit/defcon-article-unit-component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { DefconArticleUnitComponent } from './defcon-article-unit.component';
 import { SharedModule } from '../../shared/shared.module';
+import { MomentModule } from 'ngx-moment';
 
 describe('DefconArticleUnitComponent', () => {
   let component: DefconArticleUnitComponent;
@@ -8,7 +9,7 @@ describe('DefconArticleUnitComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [SharedModule],
+      imports: [MomentModule, SharedModule],
       declarations: [DefconArticleUnitComponent]
     }).compileComponents();
   }));
