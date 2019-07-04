59b5e8923286b2c509ea9a110e4183ea731535c5
Sherly Chan <sherly.chan@fairfaxmedia.co.nz>
2019-07-04 13:56:10 +1200

    EX-530: adding test defcon article list handler

--- server-src/services/handlers/defcon-article-list/defcon-article-list.spec.ts
+++ server-src/services/handlers/defcon-article-list/defcon-article-list.spec.ts
@@ -1,5 +1,64 @@
+import { IRawArticle } from "../../adapters/__types__/IRawArticle";
+
 describe('DefconArticleList', () => {
-  it('should be true', () => {
-    expect(true).toBe(true);
+  const basicAdUnit = {
+    type: 'BasicAdUnit'
+  };
+
+  const articleNumberOne = {
+    id: '1',
+    indexHeadline: 'Defcon Headline',
+    introText: 'Defcon Intro',
+    linkUrl: '/link1',
+    imageSrc: 'defcon.jpg',
+    imageSrcSet: 'defcon.jpg 1w',
+    lastPublishedTime: 1,
+    headlineFlags: []
+  };
+
+  const articleNumberTwo = {
+    id: '1',
+    indexHeadline: 'Headline 1',
+    introText: 'Intro 1',
+    linkUrl: '/link1',
+    imageSrc: '1.jpg',
+    imageSrcSet: '1.jpg 1w',
+    lastPublishedTime: 1,
+    headlineFlags: []
+  };
+
+  const articleNumberThree = {
+    id: '2',
+    indexHeadline: 'Headline 2',
+    introText: 'Intro 2',
+    linkUrl: '/link2',
+    imageSrc: '2.jpg',
+    imageSrcSet: '2.jpg 2w',
+    lastPublishedTime: 2,
+    headlineFlags: []
+  };
+
+  const rawDefconArticleList: IRawArticle[] = [articleNumberOne, articleNumberTwo, articleNumberThree];
+
+  const articleNumberOneAsDefconArticleUnit = {
+    ... articleNumberOne,
+    type: 'DefconArticleUnit'
+  };
+
+  const articleNumberTwoAsBasicArticle = {
+    ... articleNumberTwo,
+    type: 'BasicArticleUnit'
+  };
+
+  const articleNumberThreeAsBasicArticle = {
+    ... articleNumberThree,
+    type: 'BasicArticleUnit'
+  };
+
+  beforeEach(() => {
+    jest.resetModules();
+  });
+
+  it('should get a list of Defcon Article Unit and article units', async () => {
   });
 });
