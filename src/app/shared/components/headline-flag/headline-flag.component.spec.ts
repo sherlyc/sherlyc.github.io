75aa6dbaaacc4a32e911eb42fbec8106cb5c65d2
Viet Anh Tran <viet.tran@fairfaxmedia.co.nz>
2019-07-11 14:06:21 +1200

    EX-462: Add feature switch to headline flags

--- src/app/shared/components/headline-flag/headline-flag.component.spec.ts
+++ src/app/shared/components/headline-flag/headline-flag.component.spec.ts
@@ -2,17 +2,27 @@ import { HeadlineFlagComponent } from './headline-flag.component';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HeadlineFlags } from '../../../../../common/HeadlineFlags';
 import { By } from '@angular/platform-browser';
+import { mockService, ServiceMock } from '../../../services/mocks/MockService';
+import { FeatureSwitchService } from '../../../services/feature-switch/feature-switch.service';
 
 describe('Headline Flag Component', () => {
   let component: HeadlineFlagComponent;
   let fixture: ComponentFixture<HeadlineFlagComponent>;
+  let featureSwitchService: ServiceMock<FeatureSwitchService>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [],
+      providers: [
+        {
+          provide: FeatureSwitchService,
+          useClass: mockService(FeatureSwitchService)
+        }
+      ],
       declarations: [HeadlineFlagComponent]
     }).compileComponents();
 
+    featureSwitchService = TestBed.get(FeatureSwitchService);
     fixture = TestBed.createComponent(HeadlineFlagComponent);
     component = fixture.componentInstance;
   });
@@ -21,56 +31,49 @@ describe('Headline Flag Component', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should show video flag', () => {
-    component.flag = HeadlineFlags.VIDEO;
-    fixture.detectChanges();
+  it.each([
+    [HeadlineFlags.ADVERTISEMENT, '.advertisement-flag'],
+    [HeadlineFlags.SPONSORED, '.sponsored-flag'],
+    [HeadlineFlags.STUFF_NATION, '.stuff-nation-flag'],
+    [HeadlineFlags.PHOTO, '.photo-flag'],
+    [HeadlineFlags.VIDEO, '.video-flag']
+  ])(
+    'should show %s when feature switch is on',
+    async (headlineFlag, headlineFlagSelector) => {
+      featureSwitchService.getFeature.mockReturnValue(true);
+      component.flag = headlineFlag as HeadlineFlags;
 
-    const videoFlagElement = fixture.debugElement.query(By.css('.video-flag'))
-      .nativeElement;
+      await component.ngOnInit();
+      fixture.detectChanges();
 
-    expect(videoFlagElement).toBeTruthy();
-  });
+      const advertisementFlagElement = fixture.debugElement.query(
+        By.css(headlineFlagSelector)
+      );
 
-  it('should show photo flag', () => {
-    component.flag = HeadlineFlags.PHOTO;
-    fixture.detectChanges();
+      expect(advertisementFlagElement).toBeTruthy();
+    }
+  );
 
-    const photoFlagElement = fixture.debugElement.query(By.css('.photo-flag'))
-      .nativeElement;
+  it.each([
+    [HeadlineFlags.ADVERTISEMENT, '.advertisement-flag'],
+    [HeadlineFlags.SPONSORED, '.sponsored-flag'],
+    [HeadlineFlags.STUFF_NATION, '.stuff-nation-flag'],
+    [HeadlineFlags.PHOTO, '.photo-flag'],
+    [HeadlineFlags.VIDEO, '.video-flag']
+  ])(
+    'should not show %s when feature switch is off',
+    async (headlineFlag, headlineFlagSelector) => {
+      featureSwitchService.getFeature.mockReturnValue(false);
+      component.flag = headlineFlag as HeadlineFlags;
 
-    expect(photoFlagElement).toBeTruthy();
-  });
+      await component.ngOnInit();
+      fixture.detectChanges();
 
-  it('should show stuff nation flag', () => {
-    component.flag = HeadlineFlags.STUFF_NATION;
-    fixture.detectChanges();
+      const advertisementFlagElement = fixture.debugElement.query(
+        By.css(headlineFlagSelector)
+      );
 
-    const stuffNationFlagElement = fixture.debugElement.query(
-      By.css('.stuff-nation-flag')
-    ).nativeElement;
-
-    expect(stuffNationFlagElement).toBeTruthy();
-  });
-
-  it('should show sponsored flag', () => {
-    component.flag = HeadlineFlags.SPONSORED;
-    fixture.detectChanges();
-
-    const sponsoredFlagElement = fixture.debugElement.query(
-      By.css('.sponsored-flag')
-    ).nativeElement;
-
-    expect(sponsoredFlagElement).toBeTruthy();
-  });
-
-  it('should show advertisement flag', () => {
-    component.flag = HeadlineFlags.ADVERTISEMENT;
-    fixture.detectChanges();
-
-    const advertisementFlagElement = fixture.debugElement.query(
-      By.css('.advertisement-flag')
-    ).nativeElement;
-
-    expect(advertisementFlagElement).toBeTruthy();
-  });
+      expect(advertisementFlagElement).toBeFalsy();
+    }
+  );
 });
--- src/app/shared/components/headline-flag/headline-flag.component.ts
+++ src/app/shared/components/headline-flag/headline-flag.component.ts
@@ -1,11 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { HeadlineFlags } from '../../../../../common/HeadlineFlags';
+import { FeatureSwitchService } from '../../../services/feature-switch/feature-switch.service';
+import { Features } from '../../../../../common/Features';
 
 @Component({
   selector: 'app-headline-flag',
   templateUrl: './headline-flag.component.html',
   styleUrls: ['./headline-flag.component.scss']
 })
-export class HeadlineFlagComponent {
+export class HeadlineFlagComponent implements OnInit {
   @Input() flag?: HeadlineFlags;
+  isVisible = false;
+
+  constructor(private featureSwitchService: FeatureSwitchService) {}
+
+  async ngOnInit() {
+    this.isVisible = await this.featureSwitchService.getFeature(
+      Features.HeadlineFlagsFeature
+    );
+  }
 }
