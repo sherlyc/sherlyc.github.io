796dd9e8a58a0b535c35cdbcbba38ac29ec329ea
Viet Anh Tran <viet.tran@fairfaxmedia.co.nz>
2019-07-22 10:51:05 +1200

    Unit test for all weather icon

--- src/app/shared/components/weather-icon/weather-icon.component.spec.ts
+++ src/app/shared/components/weather-icon/weather-icon.component.spec.ts
@@ -1,5 +1,7 @@
 import { WeatherIconComponent } from './weather-icon.component';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Forecasts } from '../../../../../common/Forecasts';
+import { By } from '@angular/platform-browser';
 
 describe('Headline Flag Component', () => {
   let component: WeatherIconComponent;
@@ -17,5 +19,32 @@ describe('Headline Flag Component', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it.each([
+    [Forecasts.cloud, '.cloud'],
+    [Forecasts.drizzle, '.drizzle'],
+    [Forecasts.fewshowers, '.fewshowers'],
+    [Forecasts.fine, '.fine'],
+    [Forecasts.finewithshowers, '.finewithshowers'],
+    [Forecasts.fog, '.fog'],
+    [Forecasts.hail, '.hail'],
+    [Forecasts.partcloudy, '.partcloudy'],
+    [Forecasts.rain, '.rain'],
+    [Forecasts.showers, '.showers'],
+    [Forecasts.snow, '.snow'],
+    [Forecasts.thunder, '.thunder'],
+    [Forecasts.wind, '.wind'],
+    ['tick', '.tick'],
+    ['unknown', '.unknown'],
+    ['dropdown', '.dropdown'],
+    ['exit', '.exit']
+  ])('should show %s icon', (icon, iconSelector) => {
+    component.icon = icon;
+
+    fixture.detectChanges();
+
+    const weatherIcon = fixture.debugElement.query(By.css(iconSelector));
+    expect(weatherIcon).toBeTruthy();
   });
 });
