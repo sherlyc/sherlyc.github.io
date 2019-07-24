9e0964a2a051d8a3dc47d274f936c76312fa4ada
Sherly Chan <sherly.chan@fairfaxmedia.co.nz>
2019-07-24 15:24:33 +1200

    EX-577 apply strap config and fix config test

--- server-src/services/utils/config.spec.ts
+++ server-src/services/utils/config.spec.ts
@@ -1,24 +1,25 @@
 import * as configJson from '../../config.json';
+import * as strapConfig from '../../strapConfig.json';
 
 describe('Config Service', () => {
   it('should load config based on environment variable', () => {
     jest.resetModules();
     process.env.SPADE_ENV = 'staging';
     const config = require('./config').default;
-    expect(config).toEqual(configJson['staging']);
+    expect(config).toEqual({...configJson['staging'], strapConfig});
   });
 
   it('should fall back to production configuration when environment variable is not present', () => {
     jest.resetModules();
     delete process.env.SPADE_ENV;
     const config = require('./config').default;
-    expect(config).toEqual(configJson['production']);
+    expect(config).toEqual({...configJson['production'], strapConfig});
   });
 
   it('should fall back to production configuration when environment variable is not recognized', () => {
     jest.resetModules();
     process.env.SPADE_ENV = 'something';
     const config = require('./config').default;
-    expect(config).toEqual(configJson['production']);
+    expect(config).toEqual({...configJson['production'], strapConfig});
   });
 });
--- server-src/services/utils/config.ts
+++ server-src/services/utils/config.ts
@@ -1,5 +1,7 @@
 import * as config from '../../config.json';
+import * as strapConfig from '../../strapConfig.json';
 import { IEnvironmentDefinition } from './__types__/IEnvironmentDefinition';
+import {IStrapConfigDefinition} from "./__types__/IStrapConfigDefinition";
 
 const defaultEnvironmentName = 'production';
 const environmentName = process.env.SPADE_ENV || defaultEnvironmentName;
@@ -7,5 +9,7 @@ const environmentName = process.env.SPADE_ENV || defaultEnvironmentName;
 const environments = config as { [key: string]: IEnvironmentDefinition };
 const environment: IEnvironmentDefinition =
   environments[environmentName] || environments['production'];
+
+environment.strapConfig = strapConfig as IStrapConfigDefinition;
 
 export default environment;
