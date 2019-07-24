e13e88c89a46adfd6d755ad8dbac8f8c792fbcda
Sherly Chan <sherly.chan@fairfaxmedia.co.nz>
2019-07-24 16:50:13 +1200

    EX-577: fix lint errors

--- server-src/services/utils/config.ts
+++ server-src/services/utils/config.ts
@@ -1,7 +1,7 @@
 import * as config from '../../config.json';
 import * as strapConfig from '../../strapConfig.json';
 import { IEnvironmentDefinition } from './__types__/IEnvironmentDefinition';
-import {IStrapConfigDefinition} from "./__types__/IStrapConfigDefinition";
+import { IStrapConfigDefinition } from './__types__/IStrapConfigDefinition';
 
 const defaultEnvironmentName = 'production';
 const environmentName = process.env.SPADE_ENV || defaultEnvironmentName;
