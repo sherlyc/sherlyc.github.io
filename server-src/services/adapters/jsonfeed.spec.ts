8c4a87aca522a4149007b935bd7ad14260a7315d
German Valdes <german.valdes@stuff.co.nz>
2019-08-09 15:49:33 +1200

    EX-625: jsonfeed service allows to get section articles by section and get list articles by list id

--- server-src/services/adapters/jsonfeed.spec.ts
+++ server-src/services/adapters/jsonfeed.spec.ts
@@ -1,14 +1,9 @@
-import { getArticleList, getListAsset } from './jsonfeed';
+import { getSectionArticleList, getListAssetById } from './jsonfeed';
 import http from '../utils/http';
 import * as jsonfeed from './__fixtures__/jsonfeed/jsonfeed.json';
 import * as midStripData from './__fixtures__/mid-strip/mid-strip.json';
-import * as miniMidStripData from './__fixtures__/mini-mid-strip/mini-mid-strip.json';
-import * as editorsPickData from './__fixtures__/editors-pick/editors-pick.json';
-import * as dailyFixData from './__fixtures__/daily-fix/daily-fix.json';
-import * as topStories from './__fixtures__/top-stories.json';
 import { Section } from '../section';
 import { IParams } from '../__types__/IParams';
-import { ListAsset } from '../listAsset';
 import { IRawArticle } from './__types__/IRawArticle';
 
 jest.mock('../utils/http');
@@ -22,83 +17,20 @@ describe('json feed service', () => {
   });
   const params: IParams = { apiRequestId: 'request-id-for-testing' };
 
-  describe('raw article list service', () => {
-    it('should provide a raw article list', async () => {
-      (http(params).get as jest.Mock).mockResolvedValue({ data: jsonfeed });
+  it('should provide a raw article list given a json feed section', async () => {
+    (http(params).get as jest.Mock).mockResolvedValue({ data: jsonfeed });
 
-      const articles = await getArticleList(Section.Latest, 2, params);
+    const articles = await getSectionArticleList(Section.Latest, 2, params);
 
-      verifyArticles(articles);
-    });
+    verifyArticles(articles);
   });
 
-  describe('mid strip service', () => {
-    it('should provide mid strip data', async () => {
-      (http(params).get as jest.Mock).mockResolvedValue({ data: midStripData });
+  it('should provide list asset data given the json feed list id', async () => {
+    (http(params).get as jest.Mock).mockResolvedValue({ data: midStripData });
 
-      const midStripArticles = await getListAsset(
-        params,
-        ListAsset.MidStrip,
-        2
-      );
+    const midStripArticles = await getListAssetById(params, '8438437', 2);
 
-      verifyArticles(midStripArticles);
-    });
-  });
-
-  describe('mini mid strip service', () => {
-    it('should provide mini mid strip data', async () => {
-      (http(params).get as jest.Mock).mockResolvedValue({
-        data: miniMidStripData.threeListAssets
-      });
-
-      const miniMidStripArticles = await getListAsset(
-        params,
-        ListAsset.MiniMidStrip,
-        2
-      );
-
-      verifyArticles(miniMidStripArticles);
-    });
-  });
-
-  describe('editors pick service', () => {
-    it('should provide editors pick service data', async () => {
-      (http(params).get as jest.Mock).mockResolvedValue({
-        data: editorsPickData
-      });
-
-      const editorPicks = await getListAsset(params, ListAsset.EditorPicks);
-
-      verifyArticles(editorPicks);
-    });
-  });
-
-  describe('top stories service', () => {
-    it('should provide top stories service data', async () => {
-      (http(params).get as jest.Mock).mockResolvedValue({
-        data: topStories
-      });
-
-      const topStoriesArticles = await getListAsset(
-        params,
-        ListAsset.TopStories
-      );
-
-      verifyArticles(topStoriesArticles);
-    });
-  });
-
-  describe('daily fix service', () => {
-    it('should provide daily fix service data', async () => {
-      (http(params).get as jest.Mock).mockResolvedValue({
-        data: dailyFixData
-      });
-
-      const dailyFixArticles = await getListAsset(params, ListAsset.DailyFix);
-
-      verifyArticles(dailyFixArticles);
-    });
+    verifyArticles(midStripArticles);
   });
 });
 
