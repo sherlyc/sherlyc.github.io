53034063960a9d1a4960b69cb40b4068a447c734
Sherly Chan <sherly.chan@fairfaxmedia.co.nz>
2019-08-21 15:15:55 +1200

    EX-514: introducing big image article unit

---
+++ src/app/content-blocks/big-image-article-unit/big-image-article-unit.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { IBasicArticleUnit } from '../../../../common/__types__/IBasicArticleUnit';
+import { IContentBlockComponent } from '../__types__/IContentBlockComponent';
+import { AnalyticsService } from '../../services/analytics/analytics.service';
+import { AnalyticsEventsType } from '../../services/analytics/__types__/AnalyticsEventsType';
+
+@Component({
+  selector: 'app-big-image-article-unit',
+  templateUrl: './big-image-article-unit.component.html',
+  styleUrls: ['./big-image-article-unit.component.scss']
+})
+export class BigImageArticleUnitComponent implements IContentBlockComponent {
+  @Input() input!: IBasicArticleUnit;
+  index!: number;
+
+  constructor(private analyticsService: AnalyticsService) {}
+
+  sendAnalytics() {
+    const { strapName, indexHeadline, id } = this.input;
+
+    this.analyticsService.pushEvent({
+      type: AnalyticsEventsType.HOMEPAGE_STRAP_CLICKED,
+      strapName,
+      articleHeadline: indexHeadline,
+      articleId: id
+    });
+  }
+}
