27f906b17f73e369ac38c507c71d5467a9a61da2
Karen Xie <karen.xie@stuff.co.nz>
2019-08-28 15:57:11 +1200

    Clean up: Window types

--- src/app/services/window/__types__/IWindow.ts
+++ src/app/services/window/__types__/IWindow.ts
@@ -1,21 +1,13 @@
 import { IDigitalData } from '../../analytics/__types__/IDigitalData';
 import { IStuffLogin } from '../../authentication/__types__/IStuffLogin';
 import { ShieldedSite } from '../../../content-blocks/footer/__types__/ShieldedSite';
+import { NielsenAnalytics } from '../../analytics/__types__/NielsenAnalytics';
+import { IDtm } from '../../dtm/__types__/IDtm';
 
 export interface IWindow {
   digitalData: IDigitalData;
-  _satellite: {
-    pageBottom: () => void;
-  };
-  nol_t: (config: {
-    cid: string;
-    content: string;
-    server: string;
-  }) => {
-    record: () => {
-      post: () => void;
-    };
-  };
+  _satellite: IDtm;
+  nol_t: NielsenAnalytics;
   StuffLogin: IStuffLogin;
   ds07o6pcmkorn: ShieldedSite;
 }
