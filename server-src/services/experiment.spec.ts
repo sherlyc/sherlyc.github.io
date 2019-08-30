aff579a7dc170070eaabab3074ea4954073e9f1f
Karen Xie <karen.xie@fairfaxmedia.co.nz>
2019-08-30 03:16:33 +0000

    EX-516: Convert experiment config to JSON (pull request #443)
    Approved-by: Lucas Rainett <lucas.rainett@fairfaxmedia.co.nz>
    Approved-by: Viet Tran <viet.tran@fairfaxmedia.co.nz>

--- server-src/services/experiment.spec.ts
+++ server-src/services/experiment.spec.ts
@@ -20,31 +20,64 @@ describe('Experiment service', () => {
     ).toEqual(Experiments.TopStoriesVisualExperiment);
   });
 
-  it('should return groupOne when lottery number is 404 for TopStoriesVisualExperiment', () => {
-    const variant = 'groupOne';
-    const experimentName = Experiments.TopStoriesVisualExperiment;
-    const lotteryNumber = 404;
-    expect(
-      getExperimentVariant(experimentName, lotteryNumber, DeviceType.unknown)
-    ).toEqual(variant);
+  it('should return groupOne when lottery number is 404 and device is mobile for TopStoriesVisualExperiment', () => {
+    const experimentVariant = getExperimentVariant(
+      'TopStoriesVisualExperiment',
+      404,
+      DeviceType.mobile
+    );
+
+    expect(experimentVariant).toEqual('groupOne');
+  });
+
+  it('should return control when lottery number is 404 and device is not mobile for TopStoriesVisualExperiment', () => {
+    const experimentVariant = getExperimentVariant(
+      'TopStoriesVisualExperiment',
+      404,
+      DeviceType.unknown
+    );
+
+    expect(experimentVariant).toEqual('control');
+  });
+
+  it('should return groupTwo when lottery number is 505 and device is mobile for TopStoriesVisualExperiment', () => {
+    const experimentVariant = getExperimentVariant(
+      'TopStoriesVisualExperiment',
+      505,
+      DeviceType.mobile
+    );
+
+    expect(experimentVariant).toEqual('groupTwo');
+  });
+
+  it('should return control when lottery number is 505 and device is not mobile for TopStoriesVisualExperiment', () => {
+    const experimentVariant = getExperimentVariant(
+      'TopStoriesVisualExperiment',
+      505,
+      DeviceType.tablet
+    );
+
+    expect(experimentVariant).toEqual('control');
+  });
+
+  it('should return groupOne when lottery number is -1 and device is mobile for TopStoriesVisualExperiment', () => {
+    const experimentVariant = getExperimentVariant(
+      'TopStoriesVisualExperiment',
+      -1,
+      DeviceType.mobile
+    );
+
+    expect(experimentVariant).toEqual('groupOne');
   });
 
   it('should return control when lottery number is not 404 or 505 for TopStoriesVisualExperiment', () => {
-    const variant = 'control';
-    const experimentName = Experiments.TopStoriesVisualExperiment;
-    const lotteryNumber = 1;
-    expect(
-      getExperimentVariant(experimentName, lotteryNumber, DeviceType.unknown)
-    ).toEqual(variant);
-  });
+    const experimentVariant = getExperimentVariant(
+      'TopStoriesVisualExperiment',
+      1,
+      DeviceType.mobile
+    );
 
-  it('should return groupTwo when lottery number is 505 for TopStoriesVisualExperiment', () => {
-    const variant = 'groupTwo';
-    const experimentName = Experiments.TopStoriesVisualExperiment;
-    const lotteryNumber = 505;
-    expect(
-      getExperimentVariant(experimentName, lotteryNumber, DeviceType.unknown)
-    ).toEqual(variant);
+    expect(experimentVariant).toEqual('control');
   });
 
   it('should return control for other experiments', () => {
