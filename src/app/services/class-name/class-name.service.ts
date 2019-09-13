18cb3a4190ecf7862f1cc17243a0e25b8193d3ed
Sherly Chan <sherly.chan@fairfaxmedia.co.nz>
2019-09-13 14:07:06 +1200

    EX-681-syntax clean up

--- src/app/services/class-name/class-name.service.ts
+++ src/app/services/class-name/class-name.service.ts
@@ -1,17 +1,15 @@
 import { Injectable } from '@angular/core';
 import { memoize } from 'lodash';
 
-const removeSpecialChars = memoize((text: string) => {
-  return text.replace(/[^a-zA-Z0-9 ]/g, '');
-});
+const removeSpecialChars = memoize((text: string) =>
+  text.replace(/[^a-zA-Z0-9 ]/g, '')
+);
 
-const spaceToDash = memoize((text: string) => {
-  return text.replace(/[\s]+/g, '-');
-});
+const spaceToDash = memoize((text: string) => text.replace(/[\s]+/g, '-'));
 
-const camelCaseToDashCase = memoize((text: string) => {
-  return text.replace(/([a-z])([A-Z0-9])/g, '$1-$2').toLowerCase();
-});
+const camelCaseToDashCase = memoize((text: string) =>
+  text.replace(/([a-z])([A-Z0-9])/g, '$1-$2').toLowerCase()
+);
 
 @Injectable({
   providedIn: 'root'
