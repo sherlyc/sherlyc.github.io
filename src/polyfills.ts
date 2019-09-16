1fa48f1b39709d01f3209f42a3c62812d3e9debd
Lucas Rainett <lucas.rainett@stuff.co.nz>
2019-09-16 15:31:52 +1200

    add polyfills back

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -19,6 +19,8 @@
  * BROWSER POLYFILLS
  */
 
+import 'core-js/es/object';
+
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 import 'classlist.js'; // Run `npm install --save classlist.js`.
 
