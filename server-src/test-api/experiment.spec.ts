00320e67ac97920017924bccbf60638d49de0439
Viet Tran <viet.tran@fairfaxmedia.co.nz>
2019-10-07 19:36:26 +0000

    Merged in EX-708-apply-experiment-1-group-1 (pull request #513)
    EX-708 apply experiment 1 group 1
    
    * EX-708: Modify dev config to switch on group 1 on for all people
    
    * EX-708: Add api tests and update staging and prod configs
    
    Approved-by: Karen Xie
    Approved-by: Sherly Chan

--- server-src/test-api/experiment.spec.ts
+++ server-src/test-api/experiment.spec.ts
@@ -29,8 +29,8 @@ describe('Experiment API', () => {
     }
   );
 
-  test.each([[1], [34], [404]])(
-    'lottery number %i should return groupOne when it is between 1 and 34 or is 404 and device is mobile for TopStoriesVisualExperiment',
+  test.each([[1], [34], [65], [100], [404]])(
+    'should return groupOne variant for all numbers for TopStoriesVisualExperiment - lottery number %i',
     async (lotteryNumber: number) => {
       const response: supertest.Response = await supertest(app).get(
         `${experimentApi}/TopStoriesVisualExperiment/${lotteryNumber}/mobile`
@@ -43,34 +43,9 @@ describe('Experiment API', () => {
     }
   );
 
-  test.each([[35], [68], [505]])(
-    'lottery number %i should return groupTwo when it is between 35 and 68 or is 505 and device is mobile for TopStoriesVisualExperiment',
-    async (lotteryNumber: number) => {
-      const response: supertest.Response = await supertest(app).get(
-        `${experimentApi}/TopStoriesVisualExperiment/${lotteryNumber}/mobile`
-      );
-
-      const { text, status } = response;
-
-      expect(status).toBe(200);
-      expect(text).toBe('groupTwo');
-    }
-  );
-
   it('should return control when device is not mobile for TopStoriesVisualExperiment', async () => {
     const response: supertest.Response = await supertest(app).get(
       `${experimentApi}/TopStoriesVisualExperiment/404/tablet`
-    );
-
-    const { text, status } = response;
-
-    expect(status).toBe(200);
-    expect(text).toBe('control');
-  });
-
-  it('should return control when lottery number is between 69 to 100 and is not 404 or 505 for TopStoriesVisualExperiment', async () => {
-    const response: supertest.Response = await supertest(app).get(
-      `${experimentApi}/TopStoriesVisualExperiment/69/mobile`
     );
 
     const { text, status } = response;
