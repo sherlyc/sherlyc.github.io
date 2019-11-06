00fe85252b4e2637422c6da156a79b087c6f23ef
Karen Xie <karen.xie@stuff.co.nz>
2019-11-06 13:44:10 +1300

    Remove unused environment variables

--- smoke-test/test/environment-config.ts
+++ smoke-test/test/environment-config.ts
@@ -3,12 +3,8 @@ dotenv.config();
 
 interface IEnvironmentConfig {
   url: string;
-  username: string;
-  password: string;
 }
 
 export default {
-  url: process.env.URL || 'http://expfrontend:4000/',
-  username: process.env.PREPROD_USER_NAME || '',
-  password: process.env.PREPROD_PASSWORD || ''
+  url: process.env.URL || 'http://localhost:4000/'
 } as IEnvironmentConfig;
