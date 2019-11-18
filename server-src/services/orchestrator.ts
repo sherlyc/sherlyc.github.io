9efd9ab6f990233446d65fb3968f947b4bf3b632
Sherly Chan <sherly.chan@fairfaxmedia.co.nz>
2019-11-19 11:45:39 +1300

    revert url change for neighbourly widget

--- server-src/services/orchestrator.ts
+++ server-src/services/orchestrator.ts
@@ -195,7 +195,7 @@ export default async (params: IParams): Promise<IPage> => {
               height: '570px',
               width: '100%',
               margin: '0 0 20px 0',
-              url: 'https://cdn.neighbourly.co.nz/stuff/strap/empty'
+              url: 'https://www.neighbourly.co.nz/stuff/strap'
             },
             {
               type: HandlerInputType.ArticleSection,
