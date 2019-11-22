d38fe0354fe6871c445507ec60941438d09af9fb
Karen Xie <karen.xie@stuff.co.nz>
2019-11-22 16:04:56 +1300

    It.each and ctrl+g everything

--- src/app/services/device.service.spec.ts
+++ src/app/services/device.service.spec.ts
@@ -26,50 +26,18 @@ describe('DeviceService', () => {
     expect(deviceService).toBeTruthy();
   });
 
-  it('should get mobile device type', () => {
-    const mobileAgent =
-      'Mozilla/5.0 (Linux; Android 4.4.2; Nexus 4 Build/KOT49H)';
+  it.each([
+    [DeviceType.mobile, 'Mozilla/5.0 (Linux; Android 4.4.2; Nexus 4 Build/KOT49H)'],
+    [DeviceType.tablet, 'Mozilla/5.0 (iPad; CPU OS 11_0 like Mac OS X) Safari/604.1'],
+    [DeviceType.desktop, 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) Chrome/78.0.3904.97 Safari/537.36'],
+    [DeviceType.unknown, 'Invalid UserAgent']
+  ])('should get %s device from user agent %s', (device: string, userAgent: string) => {
     windowService.getWindow.mockReturnValue({
-      navigator: { userAgent: mobileAgent }
+      navigator: { userAgent }
     });
 
     const result = deviceService.getDevice();
 
-    expect(result).toEqual(DeviceType.mobile);
-  });
-
-  it('should get tablet device type', () => {
-    const tabletAgent =
-      'Mozilla/5.0 (iPad; CPU OS 11_0 like Mac OS X) Safari/604.1';
-    windowService.getWindow.mockReturnValue({
-      navigator: { userAgent: tabletAgent }
-    });
-
-    const result = deviceService.getDevice();
-
-    expect(result).toEqual(DeviceType.tablet);
-  });
-
-  it('should get desktop device type', () => {
-    const desktopAgent =
-      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) Chrome/78.0.3904.97 Safari/537.36';
-    windowService.getWindow.mockReturnValue({
-      navigator: { userAgent: desktopAgent }
-    });
-
-    const result = deviceService.getDevice();
-
-    expect(result).toEqual(DeviceType.desktop);
-  });
-
-  it('should get unknown device type', () => {
-    const randomAgent = 'what is this';
-    windowService.getWindow.mockReturnValue({
-      navigator: { userAgent: randomAgent }
-    });
-
-    const result = deviceService.getDevice();
-
-    expect(result).toEqual(DeviceType.unknown);
+    expect(result).toEqual(device);
   });
 });
