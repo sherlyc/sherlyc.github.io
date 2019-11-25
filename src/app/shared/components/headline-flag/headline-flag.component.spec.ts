5718f955a395b5757eed773697752cc465d9628b
Viet Anh Tran <viet.tran@fairfaxmedia.co.nz>
2019-11-25 15:23:39 +1300

    EX-763: Add play stuff headling flag

--- src/app/shared/components/headline-flag/headline-flag.component.spec.ts
+++ src/app/shared/components/headline-flag/headline-flag.component.spec.ts
@@ -1,7 +1,7 @@
-import { HeadlineFlagComponent } from './headline-flag.component';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { HeadlineFlags } from '../../../../../common/HeadlineFlags';
-import { By } from '@angular/platform-browser';
+import {HeadlineFlagComponent} from './headline-flag.component';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {HeadlineFlags} from '../../../../../common/HeadlineFlags';
+import {By} from '@angular/platform-browser';
 
 describe('Headline Flag Component', () => {
   let component: HeadlineFlagComponent;
@@ -26,7 +26,8 @@ describe('Headline Flag Component', () => {
     [HeadlineFlags.SPONSORED, '.sponsored-flag'],
     [HeadlineFlags.STUFF_NATION, '.stuff-nation-flag'],
     [HeadlineFlags.PHOTO, '.photo-flag'],
-    [HeadlineFlags.VIDEO, '.video-flag']
+    [HeadlineFlags.VIDEO, '.video-flag'],
+    [HeadlineFlags.PLAY_STUFF, '.play-stuff-flag']
   ])('should show %s', async (headlineFlag, headlineFlagSelector) => {
     component.flag = headlineFlag as HeadlineFlags;
     fixture.detectChanges();
