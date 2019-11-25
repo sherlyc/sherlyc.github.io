2f50c0dc72787c99a843f14f9bf4fdc41fa4aa70
Sherly Chan <sherly.chan@fairfaxmedia.co.nz>
2019-11-26 11:29:55 +1300

    EX-761: directive will add class names generated by global style service

--- src/app/shared/directives/global-style/global-style.directive.ts
+++ src/app/shared/directives/global-style/global-style.directive.ts
@@ -1,13 +1,17 @@
-import { Directive, Input, OnInit } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 import { GlobalStyleService } from '../../../services/global-style/global-style.service';
 
 @Directive({ selector: '[appGlobalStyle]' })
 export class GlobalStyleDirective implements OnInit {
   @Input('appGlobalStyle') inputStyle?: object;
 
-  constructor(private globalStyleService: GlobalStyleService) {}
+  constructor(
+    private el: ElementRef<HTMLElement>,
+    private globalStyleService: GlobalStyleService
+  ) {}
 
-  ngOnInit(): void {
-    this.globalStyleService.injectStyle(this.inputStyle);
+  ngOnInit() {
+    const className = this.globalStyleService.injectStyle(this.inputStyle);
+    this.el.nativeElement.classList.add(className);
   }
 }
