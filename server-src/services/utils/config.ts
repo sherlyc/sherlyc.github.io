5a6e5f452260bc83c8bd675c796f114ec1920fc3
Viet Anh Tran <viet.tran@fairfaxmedia.co.nz>
2019-11-27 15:26:55 +1300

    EX-758: Separate features switch config for each env

--- server-src/services/utils/config.ts
+++ server-src/services/utils/config.ts
@@ -1,8 +1,11 @@
 import * as config from '../../config.json';
 import * as strapConfiguration from '../../strapConfig.json';
+import * as featureProd from '../../featuresConfig/features-prod.json';
+import * as featureStaging from '../../featuresConfig/features-staging.json';
+import * as featureDev from '../../featuresConfig/features-dev.json';
 import { IEnvironmentDefinition } from './__types__/IEnvironmentDefinition';
 import { IStrapConfigDefinition } from './__types__/IStrapConfigDefinition';
-import { Omit } from 'utility-types';
+import { IFeaturesConfig } from '../__types__/IFeaturesConfig';
 
 const environmentName: string = process.env.SPADE_ENV || 'production';
 
@@ -17,13 +20,25 @@ const loadStrapConfig = (): IStrapConfigDefinition => {
   return strapConfig;
 };
 
+const featuresConfigs: { [key: string]: IFeaturesConfig } = {
+  production: featureProd,
+  staging: featureStaging,
+  development: featureDev,
+  localhost: featureDev
+};
+
+const loadFeatures = (): IFeaturesConfig => {
+  return featuresConfigs[environmentName] || featuresConfigs['production'];
+};
+
 const loadConfig = (): IEnvironmentDefinition => {
   const environments = config as {
-    [key: string]: Omit<IEnvironmentDefinition, 'strapConfig'>;
+    [key: string]: Partial<IEnvironmentDefinition>;
   };
   const environmentConfig =
     environments[environmentName] || environments['production'];
-  (environmentConfig as IEnvironmentDefinition).strapConfig = loadStrapConfig();
+  environmentConfig.strapConfig = loadStrapConfig();
+  environmentConfig.features = loadFeatures();
   return environmentConfig as IEnvironmentDefinition;
 };
 
