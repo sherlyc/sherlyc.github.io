f5809a495dc963ab61dd8a8a074deea70b1ade9d
Lucas Rainett <lucas.rainett@stuff.co.nz>
2019-12-02 17:51:29 +1300

    update feature names

--- common/FeatureName.ts
+++ common/FeatureName.ts
@@ -1,6 +1,5 @@
 export enum FeatureName {
   Recommendation = 'Recommendation',
   RecommendationDisplay = 'RecommendationDisplay',
-  LoginFlow = 'LoginFlow',
   AdsRelativePositioning = 'AdsRelativePositioning'
 }
