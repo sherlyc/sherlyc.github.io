f74d0ae32daa1fb4fe812abd6a79f7e1eb4932e1
Sherly Chan <sherly.chan@fairfaxmedia.co.nz>
2019-12-04 11:14:44 +1300

    EX-761: update grid gap helper test case

--- server-src/services/utils/grid-gap/grid-gap.spec.ts
+++ server-src/services/utils/grid-gap/grid-gap.spec.ts
@@ -2,99 +2,111 @@ import gridGap from './grid-gap';
 import { IGridConfig } from '../../../../common/__types__/IGridContainer';
 
 describe('grip gap helper', () => {
-  const gridConfig = {
-    gridTemplateColumns: '1fr 1fr 1fr 1fr 200px',
-    gridTemplateRows: 'auto auto auto',
-    gridGap: '20px',
-    gridBlocks: [
-      {
-        rowStart: 0,
-        rowSpan: 0,
-        columnStart: 0,
-        columnSpan: 0
-      }
-    ]
-  } as IGridConfig;
-
-  const expectedConfig = {
-    gridTemplateColumns: '1fr 20px 1fr 20px 1fr 20px 1fr 20px 200px',
-    gridTemplateRows: 'auto 20px auto 20px auto',
-    gridGap: '0',
-    gridBlocks: [
-      {
-        rowStart: 0,
-        rowSpan: 0,
-        columnStart: 0,
-        columnSpan: 0
-      }
-    ]
-  } as IGridConfig;
-
   it('should add grip gap to grid template rows and columns', () => {
-    expect(gridGap(gridConfig)).toEqual(expectedConfig);
-  });
-
-  it('should handle grid blocks', () => {
-    const config = {
-      ...gridConfig,
+    const gridConfig = {
+      gridTemplateColumns: '1fr 1fr 1fr 1fr 200px',
+      gridTemplateRows: 'auto auto auto',
+      gridGap: '20px',
       gridBlocks: [
         {
-          rowStart: 1,
-          rowSpan: 2,
-          columnStart: 1,
-          columnSpan: 2
+          rowStart: 0,
+          rowSpan: 0,
+          columnStart: 0,
+          columnSpan: 0
         }
       ]
     } as IGridConfig;
 
-    const expected = {
-      ...expectedConfig,
+    const expectedConfig = {
+      gridTemplateColumns: '1fr 20px 1fr 20px 1fr 20px 1fr 20px 200px',
+      gridTemplateRows: 'auto 20px auto 20px auto',
+      gridGap: '0',
+      gridBlocks: [
+        {
+          rowStart: 0,
+          rowSpan: 0,
+          columnStart: 0,
+          columnSpan: 0
+        }
+      ]
+    } as IGridConfig;
+
+    expect(gridGap(gridConfig)).toEqual(expectedConfig);
+  });
+
+  it('should handle multiple grid blocks', () => {
+    const config = {
+      gridTemplateColumns: '1fr 1fr 1fr 1fr',
+      gridTemplateRows: '1fr 1fr 1fr',
+      gridGap: '100px',
       gridBlocks: [
         {
           rowStart: 1,
-          rowSpan: 3,
+          rowSpan: 1,
           columnStart: 1,
+          columnSpan: 2
+        },
+        {
+          rowStart: 2,
+          rowSpan: 2,
+          columnStart: 1,
+          columnSpan: 1
+        },
+        {
+          rowStart: 2,
+          rowSpan: 1,
+          columnStart: 2,
+          columnSpan: 3
+        },
+        {
+          rowStart: 1,
+          rowSpan: 1,
+          columnStart: 3,
+          columnSpan: 2
+        },
+        {
+          rowStart: 3,
+          rowSpan: 1,
+          columnStart: 2,
           columnSpan: 3
         }
       ]
     };
 
-    expect(gridGap(config)).toEqual(expected);
-  });
-
-  it('should handle more than one grid blocks', () => {
-    const config = {
-      ...gridConfig,
+    const expected = {
+      gridTemplateColumns: '1fr 100px 1fr 100px 1fr 100px 1fr',
+      gridTemplateRows: '1fr 100px 1fr 100px 1fr',
+      gridGap: '0',
       gridBlocks: [
         {
           rowStart: 1,
-          rowSpan: 2,
+          rowSpan: 1,
           columnStart: 1,
-          columnSpan: 2
+          columnSpan: 3
         },
         {
           rowStart: 3,
           rowSpan: 3,
           columnStart: 1,
-          columnSpan: 2
-        }
-      ]
-    } as IGridConfig;
-
-    const expected = {
-      ...expectedConfig,
-      gridBlocks: [
+          columnSpan: 1
+        },
+        {
+          rowStart: 3,
+          rowSpan: 1,
+          columnStart: 3,
+          columnSpan: 5
+        },
         {
           rowStart: 1,
-          rowSpan: 3,
-          columnStart: 1,
+          rowSpan: 1,
+          columnStart: 5,
           columnSpan: 3
         },
         {
           rowStart: 5,
-          rowSpan: 5,
-          columnStart: 1,
-          columnSpan: 3
+          rowSpan: 1,
+          columnStart: 3,
+          columnSpan: 5
         }
       ]
     };
