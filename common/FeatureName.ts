e32e82fd3a83d80563c76de21376e8aad1d3c7b6
Lucas Rainett <lucas.rainett@stuff.co.nz>
2019-12-03 17:08:29 +1300

    add login flow switch

--- common/FeatureName.ts
+++ common/FeatureName.ts
@@ -2,5 +2,6 @@ export enum FeatureName {
   Recommendation = 'Recommendation',
   RecommendationDisplay = 'RecommendationDisplay',
   AdsRelativePositioning = 'AdsRelativePositioning',
+  LoginFlow = 'LoginFlow',
   AdobeLaunch = 'AdobeLaunch'
 }
