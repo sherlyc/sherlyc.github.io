a06ec6dd76b58625fe3b27fbc09403baa7db2903
Lucas Rainett <lucas.rainett@stuff.co.nz>
2019-12-06 13:43:46 +1300

    avoid angular reconciliation for components that don't change

--- src/app/content-blocks/big-image-article-unit/big-image-article-unit.component.ts
+++ src/app/content-blocks/big-image-article-unit/big-image-article-unit.component.ts
@@ -1,4 +1,9 @@
-import { Component, Input, OnInit } from "@angular/core";
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnInit
+} from "@angular/core";
 import { IBasicArticleUnit } from "../../../../common/__types__/IBasicArticleUnit";
 import { IContentBlockComponent } from "../__types__/IContentBlockComponent";
 import { AnalyticsService } from "../../services/analytics/analytics.service";
@@ -7,7 +12,8 @@ import { AnalyticsEventsType } from "../../services/analytics/__types__/Analytic
 @Component({
   selector: "app-big-image-article-unit",
   templateUrl: "./big-image-article-unit.component.html",
-  styleUrls: ["./big-image-article-unit.component.scss"]
+  styleUrls: ["./big-image-article-unit.component.scss"],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class BigImageArticleUnitComponent implements IContentBlockComponent {
   @Input() input!: IBasicArticleUnit;
