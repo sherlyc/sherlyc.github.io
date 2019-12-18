e2f695986a7e2adcafd6f54153c60b220a7b5048
Lucas Rainett <lucas.rainett@stuff.co.nz>
2019-12-18 16:49:20 +1300

    display module name and color

--- src/app/content-blocks/module-title/module-title.component.spec.ts
+++ src/app/content-blocks/module-title/module-title.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { ModuleTitleComponent } from "./module-title.component";
+import { IModuleTitle } from "../../../../common/__types__/IModuleTitle";
+import { By } from "@angular/platform-browser";
 
 describe("ErrorBlockComponent", () => {
   let component: ModuleTitleComponent;
@@ -19,5 +21,29 @@ describe("ErrorBlockComponent", () => {
 
   it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should display the module name", () => {
+    const moduleName = "FakeModuleName";
+
+    component.input = {
+      displayName: moduleName
+    } as IModuleTitle;
+    fixture.detectChanges();
+
+    const moduleTitle = fixture.debugElement.query(By.css(".title"));
+    expect(moduleTitle.nativeElement.textContent).toEqual(moduleName);
+  });
+
+  it("should display the color", () => {
+    const moduleColor = "FakeModuleColor";
+
+    component.input = {
+      displayNameColor: moduleColor
+    } as IModuleTitle;
+    fixture.detectChanges();
+
+    const moduleTitle = fixture.debugElement.query(By.css("." + moduleColor));
+    expect(moduleTitle).toBeTruthy();
   });
 });
