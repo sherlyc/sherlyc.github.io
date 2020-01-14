c98bcfd1e21d01365cb9e3917a6b563e505373a2
Viet Anh Tran <viet.tran@fairfaxmedia.co.nz>
2020-01-15 10:32:31 +1300

    Update npm test scripts and refactor script

--- pwa-cache-test/test-all-versions.sh
+++ pwa-cache-test/test-all-versions.sh
@@ -2,14 +2,14 @@
 
 cd smoke-test/ && npm i && cd ..
 export URL="http://localhost:8888/"
-GCR_URL="gcr.io/shared-218200/nz.stuff/experience/experience-frontend"
+GCR="gcr.io/shared-218200/nz.stuff/experience/experience-frontend"
 
 CURRENT_PROD_VERSION=498
 for (( i=439; i<=CURRENT_PROD_VERSION; i++ )); do
     export VERSION="1.${i}"
     echo "Running test on version ${VERSION}"
 
-    export DOCKER_IMAGE="${GCR_URL}:${VERSION}"
+    export DOCKER_IMAGE="${GCR}:${VERSION}"
 
     (docker-compose -f ./pwa-cache-test/docker-compose.yaml up) & (cd smoke-test/ && sleep 10 && npm run pwatest && cd ..)
     docker-compose -f ./pwa-cache-test/docker-compose.yaml down
