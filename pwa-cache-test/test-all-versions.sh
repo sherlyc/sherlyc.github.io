34f487ae6e6a277252f9503a783d7ce22d45e5fe
Viet Anh Tran <viet.tran@fairfaxmedia.co.nz>
2020-01-14 16:14:06 +1300

    Fix test script

--- pwa-cache-test/test-all-versions.sh
+++ pwa-cache-test/test-all-versions.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
+cd smoke-test/ && npm i && cd ..
+export URL="http://localhost:8888/"
 CURRENT_PROD_VERSION=498
-for (( i=439; i<=440; i++)); do
+for (( i=439; i<=CURRENT_PROD_VERSION; i++ )); do
     export VERSION="1.${i}"
     echo "Running test on version ${VERSION}"
-    echo ${VERSION}
-    docker-compose -f ./pwa-cache-test/docker-compose.yaml up -d
-    cd smoke-test/ || exit
-    npm i && npm run pwatest && cd ..
-    docker-compose -f ./pwa-cache-test/docker-compose.yaml rm -f
+    (docker-compose -f ./pwa-cache-test/docker-compose.yaml up) & (cd smoke-test/ && sleep 10 && npm run pwatest && cd ..)
+    docker-compose -f ./pwa-cache-test/docker-compose.yaml down
     echo "Successfully completed test for ${VERSION}"
+    printf "\n\n\n\n"
 done
 
