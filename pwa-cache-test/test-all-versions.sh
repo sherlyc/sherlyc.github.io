a55cdb2b35c37d25ac6c431d177f6eba429c9b3b
Viet Anh Tran <viet.tran@fairfaxmedia.co.nz>
2020-01-14 16:50:49 +1300

    Extract docker url

--- pwa-cache-test/test-all-versions.sh
+++ pwa-cache-test/test-all-versions.sh
@@ -2,12 +2,18 @@
 
 cd smoke-test/ && npm i && cd ..
 export URL="http://localhost:8888/"
+GCR_URL="gcr.io/shared-218200/nz.stuff/experience/experience-frontend"
+
 CURRENT_PROD_VERSION=498
 for (( i=439; i<=CURRENT_PROD_VERSION; i++ )); do
     export VERSION="1.${i}"
     echo "Running test on version ${VERSION}"
+
+    export DOCKER_IMAGE="${GCR_URL}:${VERSION}"
+
     (docker-compose -f ./pwa-cache-test/docker-compose.yaml up) & (cd smoke-test/ && sleep 10 && npm run pwatest && cd ..)
     docker-compose -f ./pwa-cache-test/docker-compose.yaml down
+
     echo "Successfully completed test for ${VERSION}"
     echo $(( CURRENT_PROD_VERSION - i )) "versions left to run"
     printf "\n\n\n\n"
