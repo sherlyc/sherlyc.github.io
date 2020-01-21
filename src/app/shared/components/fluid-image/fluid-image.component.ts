dab8627f31da2ef0497f0afcc28518a866d1bcae
James Shih <james.shih@stuff.co.nz>
2020-01-22 12:29:00 +1300

    EX-809 (WIP) use 16:9 image in new homepage modules

---
+++ src/app/shared/components/fluid-image/fluid-image.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input } from "@angular/core";
+
+@Component({
+  selector: "app-fluid-image",
+  templateUrl: "./fluid-image.component.html",
+  styleUrls: ["./fluid-image.component.scss"]
+})
+export class FluidImageComponent {
+  @Input() imageSrc!: string;
+  @Input() caption!: string;
+  @Input() aspectRatio = "16:9";
+  src!: string;
+  srcset!: string;
+  width = 100;
+
+  constructor() {}
+
+  onResize(entry: ResizeObserverEntry) {
+    const width = Math.ceil(entry.contentRect.width);
+    if (width >= this.width * 1.2) {
+      this.width = width;
+      const src = `${this.imageSrc}?format=pjpg&crop=${this.aspectRatio},smart&width=${width}`;
+      this.srcset = `${src}, ${src}&dpr=2 2x`;
+      this.src = src;
+    }
+  }
+}
