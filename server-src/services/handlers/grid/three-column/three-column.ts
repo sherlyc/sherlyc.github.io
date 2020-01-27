6292dbb2d7762da2e9ead48b391b0dbd05f52951
Karen Xie <karen.xie@stuff.co.nz>
2020-01-28 11:37:02 +1300

    EX-773: Get raw article, convert to title unit and return content blocks in one function

--- server-src/services/handlers/grid/three-column/three-column.ts
+++ server-src/services/handlers/grid/three-column/three-column.ts
@@ -9,84 +9,77 @@ import { Strap } from "../../../strap";
 import { basicArticleTitleUnit } from "../../../adapters/article-converter/basic-article-title.converter";
 import { ContentBlockType } from "../../../../../common/__types__/ContentBlockType";
 
+const getColumnContent = async (
+  handlerRunner: handlerRunnerFunction,
+  sourceId: Strap,
+  strapName: string,
+  totalArticles: number,
+  displayName: string,
+  displayNameColor: string,
+  params: IParams
+): Promise<IContentBlock[]> => {
+  const articles = await getRawArticles(sourceId, totalArticles, params);
+
+  const articleContentBlocks = articles.map((article) =>
+    basicArticleTitleUnit(article, strapName)
+  );
+
+  return [
+    {
+      type: ContentBlockType.ModuleTitle,
+      displayName,
+      displayNameColor
+    },
+    ...(await handlerRunner(
+      {
+        type: HandlerInputType.ListGrid,
+        content: articleContentBlocks
+      },
+      params
+    ))
+  ];
+};
+
 export default async function(
   handlerRunner: handlerRunnerFunction,
   {}: IThreeColumnHandlerInput,
   params: IParams
 ): Promise<IContentBlock[]> {
   const totalArticles = 8;
-  const articles = await getRawArticles(
-    Strap.EditorPicks,
-    totalArticles,
-    params
-  );
-  const articlesTwo = await getRawArticles(
-    Strap.Business,
-    totalArticles,
-    params
-  );
-  const articlesThree = await getRawArticles(
-    Strap.Opinion,
-    totalArticles,
-    params
-  );
 
-  const articleListOne = articles.map((article) =>
-    basicArticleTitleUnit(article, "Editors' Picks")
-  );
-
-  const articleListTwo = articlesTwo.map((article) =>
-    basicArticleTitleUnit(article, "Business")
-  );
-
-  const articleListThree = articlesThree.map((article) =>
-    basicArticleTitleUnit(article, "Opinion")
-  );
-
-  const columnOneContent: IContentBlock[] = [
-    {
-      type: ContentBlockType.ModuleTitle,
-      displayName: "Editors' Picks",
-      displayNameColor: "pizzaz"
-    },
-    ...(await handlerRunner(
-      {
-        type: HandlerInputType.ListGrid,
-        content: articleListOne
-      },
+  const [
+    columnOneContent,
+    columnTwoContent,
+    columnThreeContent
+  ] = await Promise.all([
+    getColumnContent(
+      handlerRunner,
+      Strap.EditorPicks,
+      "Editors' Picks",
+      totalArticles,
+      "Editors' Picks",
+      "pizzaz",
       params
-    ))
-  ];
-
-  const columnTwoContent: IContentBlock[] = [
-    {
-      type: ContentBlockType.ModuleTitle,
-      displayName: "Business",
-      displayNameColor: "pizzaz"
-    },
-    ...(await handlerRunner(
-      {
-        type: HandlerInputType.ListGrid,
-        content: articleListTwo
-      },
+    ),
+    getColumnContent(
+      handlerRunner,
+      Strap.Business,
+      "Business",
+      totalArticles,
+      "Business",
+      "pizzaz",
       params
-    ))
-  ];
-
-  const columnThreeContent: IContentBlock[] = [
-    {
-      type: ContentBlockType.ModuleTitle,
-      displayName: "Opinion",
-      displayNameColor: "pizzaz"
-    },
-    ...(await handlerRunner(
-      {
-        type: HandlerInputType.ListGrid,
-        content: articleListThree
-      },
+    ),
+    getColumnContent(
+      handlerRunner,
+      Strap.Opinion,
+      "Opinion",
+      totalArticles,
+      "Opinion",
+      "pizzaz",
       params
-    ))
-  ];
+    )
+  ]);
 
   const threeColumnGridHandlerInput: IColumnGridHandlerInput = {
     type: HandlerInputType.ColumnGrid,
