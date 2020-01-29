a1ca0b528e08478b63ddd6ef97f5a45eaeeed728
Karen Xie <karen.xie@stuff.co.nz>
2020-01-29 16:06:01 +1300

    EX-817: Remove fractional pixels

--- src/app/content-blocks/grid-container/__types__/MediaQuery.ts
+++ src/app/content-blocks/grid-container/__types__/MediaQuery.ts
@@ -1,6 +1,6 @@
 export enum MediaQuery {
-  Mobile = "@media only screen and (max-width: 767.999px)",
-  Tablet = "@media only screen and (min-width: 768px) and (max-width: 991.999px)",
+  Mobile = "@media only screen and (max-width: 767px)",
+  Tablet = "@media only screen and (min-width: 768px) and (max-width: 991px)",
   Desktop = "@media only screen and (min-width: 992px)",
   All = "@media all"
 }
