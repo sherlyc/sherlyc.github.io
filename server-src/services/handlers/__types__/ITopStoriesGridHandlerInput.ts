e514ab0087f0dd92cbf28ca5de071c9cce85da0c
Viet Anh Tran <viet.tran@fairfaxmedia.co.nz>
2020-01-31 15:37:54 +1300

    EX-813 Top Stories grid positions

---
+++ server-src/services/handlers/__types__/ITopStoriesGridHandlerInput.ts
@@ -0,0 +1,20 @@
+import { HandlerInputType } from "./HandlerInputType";
+import { IContentBlock } from "../../../../common/__types__/IContentBlock";
+
+export interface ITopStoriesGridHandlerInput {
+  type: HandlerInputType.TopStoriesGrid;
+  content: { [position in TopStoriesGridPositions]: IContentBlock[] };
+}
+
+export enum TopStoriesGridPositions {
+  BigTopLeft = "BigTopLeft",
+  Right = "Right",
+  FirstRow1 = "FirstRow1",
+  FirstRow2 = "FirstRow2",
+  FirstRow3 = "FirstRow3",
+  FirstRow4 = "FirstRow4",
+  SecondRow1 = "SecondRow1",
+  SecondRow2 = "SecondRow2",
+  SecondRow3 = "SecondRow3",
+  SecondRow4 = "SecondRow4"
+}
