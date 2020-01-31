58e7600e80d1d5dec7e1a4fe3649c37b5170384d
Viet Anh Tran <viet.tran@fairfaxmedia.co.nz>
2020-01-31 16:49:15 +1300

    EX-813 change from snapshot to plain object

--- server-src/services/handlers/grid/top-stories-grid/top-stories-grid.spec.ts
+++ server-src/services/handlers/grid/top-stories-grid/top-stories-grid.spec.ts
@@ -6,6 +6,8 @@ import {
 import { IParams } from "../../../__types__/IParams";
 import { HandlerInputType } from "../../__types__/HandlerInputType";
 import { IContentBlock } from "../../../../../common/__types__/IContentBlock";
+import { Border } from "../../../../../common/__types__/IGridContainer";
+import { ContentBlockType } from "../../../../../common/__types__/ContentBlockType";
 
 describe("Top Stories grid handler", () => {
   const handlerRunnerMock = jest.fn();
@@ -40,216 +42,178 @@ describe("Top Stories grid handler", () => {
       params
     );
 
-    expect(result).toMatchInlineSnapshot(`
-      Array [
-        Object {
-          "items": Object {
-            "BigTopLeft": Array [
-              Object {},
-            ],
-            "FirstRow1": Array [
-              Object {},
-            ],
-            "FirstRow2": Array [
-              Object {},
-            ],
-            "FirstRow3": Array [
-              Object {},
-            ],
-            "FirstRow4": Array [
-              Object {},
-            ],
-            "Right": Array [
-              Object {},
-            ],
-            "SecondRow1": Array [
-              Object {},
-            ],
-            "SecondRow2": Array [
-              Object {},
-            ],
-            "SecondRow3": Array [
-              Object {},
-            ],
-            "SecondRow4": Array [
-              Object {},
-            ],
-          },
-          "mobile": Object {
-            "gridBlocks": Object {
-              "BigTopLeft": Object {
-                "border": Array [],
-                "columnSpan": 1,
-                "columnStart": 1,
-                "rowSpan": 1,
-                "rowStart": 1,
-              },
-              "FirstRow1": Object {
-                "border": Array [],
-                "columnSpan": 1,
-                "columnStart": 1,
-                "rowSpan": 1,
-                "rowStart": 3,
-              },
-              "FirstRow2": Object {
-                "border": Array [
-                  "top",
-                ],
-                "columnSpan": 1,
-                "columnStart": 1,
-                "rowSpan": 1,
-                "rowStart": 4,
-              },
-              "FirstRow3": Object {
-                "border": Array [
-                  "top",
-                ],
-                "columnSpan": 1,
-                "columnStart": 1,
-                "rowSpan": 1,
-                "rowStart": 5,
-              },
-              "FirstRow4": Object {
-                "border": Array [
-                  "top",
-                ],
-                "columnSpan": 1,
-                "columnStart": 1,
-                "rowSpan": 1,
-                "rowStart": 6,
-              },
-              "Right": Object {
-                "border": Array [],
-                "columnSpan": 1,
-                "columnStart": 1,
-                "rowSpan": 1,
-                "rowStart": 2,
-              },
-              "SecondRow1": Object {
-                "border": Array [
-                  "top",
-                ],
-                "columnSpan": 1,
-                "columnStart": 1,
-                "rowSpan": 1,
-                "rowStart": 7,
-              },
-              "SecondRow2": Object {
-                "border": Array [
-                  "top",
-                ],
-                "columnSpan": 1,
-                "columnStart": 1,
-                "rowSpan": 1,
-                "rowStart": 8,
-              },
-              "SecondRow3": Object {
-                "border": Array [
-                  "top",
-                ],
-                "columnSpan": 1,
-                "columnStart": 1,
-                "rowSpan": 1,
-                "rowStart": 9,
-              },
-              "SecondRow4": Object {
-                "border": Array [
-                  "top",
-                ],
-                "columnSpan": 1,
-                "columnStart": 1,
-                "rowSpan": 1,
-                "rowStart": 10,
-              },
-            },
-            "gridColumnGap": "20px",
-            "gridRowGap": "20px",
-            "gridTemplateColumns": "1fr",
-            "gridTemplateRows": "auto auto auto auto auto auto auto auto auto auto",
-          },
-          "tablet": Object {
-            "gridBlocks": Object {
-              "BigTopLeft": Object {
-                "border": Array [
-                  "bottom",
-                ],
-                "columnSpan": 4,
-                "columnStart": 1,
-                "rowSpan": 1,
-                "rowStart": 1,
-              },
-              "FirstRow1": Object {
-                "border": Array [],
-                "columnSpan": 1,
-                "columnStart": 1,
-                "rowSpan": 1,
-                "rowStart": 2,
-              },
-              "FirstRow2": Object {
-                "border": Array [],
-                "columnSpan": 1,
-                "columnStart": 2,
-                "rowSpan": 1,
-                "rowStart": 2,
-              },
-              "FirstRow3": Object {
-                "border": Array [],
-                "columnSpan": 1,
-                "columnStart": 3,
-                "rowSpan": 1,
-                "rowStart": 2,
-              },
-              "FirstRow4": Object {
-                "border": Array [],
-                "columnSpan": 1,
-                "columnStart": 4,
-                "rowSpan": 1,
-                "rowStart": 2,
-              },
-              "Right": Object {
-                "border": Array [],
-                "columnSpan": 1,
-                "columnStart": 5,
-                "rowSpan": 3,
-                "rowStart": 1,
-              },
-              "SecondRow1": Object {
-                "border": Array [],
-                "columnSpan": 1,
-                "columnStart": 1,
-                "rowSpan": 1,
-                "rowStart": 3,
-              },
-              "SecondRow2": Object {
-                "border": Array [],
-                "columnSpan": 1,
-                "columnStart": 2,
-                "rowSpan": 1,
-                "rowStart": 3,
-              },
-              "SecondRow3": Object {
-                "border": Array [],
-                "columnSpan": 1,
-                "columnStart": 3,
-                "rowSpan": 1,
-                "rowStart": 3,
-              },
-              "SecondRow4": Object {
-                "border": Array [],
-                "columnSpan": 1,
-                "columnStart": 4,
-                "rowSpan": 1,
-                "rowStart": 3,
-              },
-            },
-            "gridColumnGap": "20px",
-            "gridRowGap": "20px",
-            "gridTemplateColumns": "1fr 1fr 1fr 1fr 300px",
-            "gridTemplateRows": "auto auto auto",
-          },
-          "type": "GridContainer",
+    expect(result).toEqual([
+      {
+        type: ContentBlockType.GridContainer,
+        items: {
+          [TopStoriesGridPositions.BigTopLeft]: [fakeContentBlock],
+          [TopStoriesGridPositions.Right]: [fakeContentBlock],
+          [TopStoriesGridPositions.FirstRow1]: [fakeContentBlock],
+          [TopStoriesGridPositions.FirstRow2]: [fakeContentBlock],
+          [TopStoriesGridPositions.FirstRow3]: [fakeContentBlock],
+          [TopStoriesGridPositions.FirstRow4]: [fakeContentBlock],
+          [TopStoriesGridPositions.SecondRow1]: [fakeContentBlock],
+          [TopStoriesGridPositions.SecondRow2]: [fakeContentBlock],
+          [TopStoriesGridPositions.SecondRow3]: [fakeContentBlock],
+          [TopStoriesGridPositions.SecondRow4]: [fakeContentBlock]
         },
-      ]
-    `);
+        mobile: {
+          gridTemplateColumns: "1fr",
+          gridTemplateRows: "auto auto auto auto auto auto auto auto auto auto",
+          gridColumnGap: "20px",
+          gridRowGap: "20px",
+          gridBlocks: {
+            [TopStoriesGridPositions.BigTopLeft]: {
+              border: [],
+              columnSpan: 1,
+              columnStart: 1,
+              rowSpan: 1,
+              rowStart: 1
+            },
+            [TopStoriesGridPositions.Right]: {
+              border: [],
+              columnSpan: 1,
+              columnStart: 1,
+              rowSpan: 1,
+              rowStart: 2
+            },
+            [TopStoriesGridPositions.FirstRow1]: {
+              border: [],
+              columnSpan: 1,
+              columnStart: 1,
+              rowSpan: 1,
+              rowStart: 3
+            },
+            [TopStoriesGridPositions.FirstRow2]: {
+              border: [Border.top],
+              columnSpan: 1,
+              columnStart: 1,
+              rowSpan: 1,
+              rowStart: 4
+            },
+            [TopStoriesGridPositions.FirstRow3]: {
+              border: [Border.top],
+              columnSpan: 1,
+              columnStart: 1,
+              rowSpan: 1,
+              rowStart: 5
+            },
+            [TopStoriesGridPositions.FirstRow4]: {
+              border: [Border.top],
+              columnSpan: 1,
+              columnStart: 1,
+              rowSpan: 1,
+              rowStart: 6
+            },
+            [TopStoriesGridPositions.SecondRow1]: {
+              border: [Border.top],
+              columnSpan: 1,
+              columnStart: 1,
+              rowSpan: 1,
+              rowStart: 7
+            },
+            [TopStoriesGridPositions.SecondRow2]: {
+              border: [Border.top],
+              columnSpan: 1,
+              columnStart: 1,
+              rowSpan: 1,
+              rowStart: 8
+            },
+            [TopStoriesGridPositions.SecondRow3]: {
+              border: [Border.top],
+              columnSpan: 1,
+              columnStart: 1,
+              rowSpan: 1,
+              rowStart: 9
+            },
+            [TopStoriesGridPositions.SecondRow4]: {
+              border: [Border.top],
+              columnSpan: 1,
+              columnStart: 1,
+              rowSpan: 1,
+              rowStart: 10
+            }
+          }
+        },
+        tablet: {
+          gridTemplateColumns: "1fr 1fr 1fr 1fr 300px",
+          gridTemplateRows: "auto auto auto",
+          gridColumnGap: "20px",
+          gridRowGap: "20px",
+          gridBlocks: {
+            [TopStoriesGridPositions.BigTopLeft]: {
+              border: [Border.bottom],
+              columnSpan: 4,
+              columnStart: 1,
+              rowSpan: 1,
+              rowStart: 1
+            },
+            [TopStoriesGridPositions.Right]: {
+              border: [],
+              columnSpan: 1,
+              columnStart: 5,
+              rowSpan: 3,
+              rowStart: 1
+            },
+            [TopStoriesGridPositions.FirstRow1]: {
+              border: [],
+              columnSpan: 1,
+              columnStart: 1,
+              rowSpan: 1,
+              rowStart: 2
+            },
+            [TopStoriesGridPositions.FirstRow2]: {
+              border: [],
+              columnSpan: 1,
+              columnStart: 2,
+              rowSpan: 1,
+              rowStart: 2
+            },
+            [TopStoriesGridPositions.FirstRow3]: {
+              border: [],
+              columnSpan: 1,
+              columnStart: 3,
+              rowSpan: 1,
+              rowStart: 2
+            },
+            [TopStoriesGridPositions.FirstRow4]: {
+              border: [],
+              columnSpan: 1,
+              columnStart: 4,
+              rowSpan: 1,
+              rowStart: 2
+            },
+            [TopStoriesGridPositions.SecondRow1]: {
+              border: [],
+              columnSpan: 1,
+              columnStart: 1,
+              rowSpan: 1,
+              rowStart: 3
+            },
+            [TopStoriesGridPositions.SecondRow2]: {
+              border: [],
+              columnSpan: 1,
+              columnStart: 2,
+              rowSpan: 1,
+              rowStart: 3
+            },
+            [TopStoriesGridPositions.SecondRow3]: {
+              border: [],
+              columnSpan: 1,
+              columnStart: 3,
+              rowSpan: 1,
+              rowStart: 3
+            },
+            [TopStoriesGridPositions.SecondRow4]: {
+              border: [],
+              columnSpan: 1,
+              columnStart: 4,
+              rowSpan: 1,
+              rowStart: 3
+            }
+          }
+        }
+      }
+    ]);
   });
 });
