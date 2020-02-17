db14450e4a66360cf8a1e6d3181c43cb9140ef93
Viet Anh Tran <viet.tran@fairfaxmedia.co.nz>
2020-02-18 09:36:08 +1300

    EX-799: Get most popular articles

--- server-src/services/adapters/most-popular/most-popular.service.spec.ts
+++ server-src/services/adapters/most-popular/most-popular.service.spec.ts
@@ -2,21 +2,47 @@ import { getMostPopular } from "./most-popular.service";
 import { IParams } from "../../__types__/IParams";
 import cacheHttp from "../../utils/cache-http";
 import config from "../../utils/config";
+import { getArticleById } from "../jsonfeed/jsonfeed";
 
 jest.mock("../../utils/cache-http");
+jest.mock("../jsonfeed/jsonfeed");
 
 describe("Most popular service", function() {
   const params: IParams = {
     apiRequestId: "1"
   };
+  const rawArticle = (id: string) => ({
+    id
+  });
 
   it("should get articles from most popular service", async () => {
     const limit = 10;
-    await getMostPopular(limit, params);
+    (cacheHttp as jest.Mock).mockResolvedValue({
+      data: {
+        stories: [
+          {
+            storyId: "1"
+          },
+          {
+            storyId: "2"
+          }
+        ]
+      }
+    });
+    (getArticleById as jest.Mock).mockResolvedValueOnce(rawArticle("1"));
+    (getArticleById as jest.Mock).mockResolvedValueOnce(rawArticle("2"));
+
+    const articles = await getMostPopular(limit, params);
 
     expect(cacheHttp).toHaveBeenCalledWith(
       params,
       `${config.mostPopularApi}?limit=${limit}`
     );
+    expect(getArticleById).toHaveBeenNthCalledWith(1, params, 1);
+    expect(getArticleById).toHaveBeenNthCalledWith(2, params, 2);
+    expect(articles).toEqual([
+      expect.objectContaining({ id: "1" }),
+      expect.objectContaining({ id: "2" })
+    ]);
   });
 });
--- server-src/services/adapters/most-popular/most-popular.service.ts
+++ server-src/services/adapters/most-popular/most-popular.service.ts
@@ -2,11 +2,26 @@ import { IRawArticle } from "../__types__/IRawArticle";
 import { IParams } from "../../__types__/IParams";
 import cacheHttp from "../../utils/cache-http";
 import config from "../../utils/config";
+import { getArticleById } from "../jsonfeed/jsonfeed";
+
+interface MostPopularResponse {
+  stories: Array<{
+    storyId: string;
+    [key: string]: any;
+  }>;
+}
 
 export const getMostPopular = async (
   limit: number,
   params: IParams
 ): Promise<IRawArticle[]> => {
-  await cacheHttp(params, `${config.mostPopularApi}?limit=${limit}`);
-  return [];
+  const response = await cacheHttp<MostPopularResponse>(
+    params,
+    `${config.mostPopularApi}?limit=${limit}`
+  );
+  return await Promise.all(
+    response.data.stories.map(({ storyId }) =>
+      getArticleById(params, parseInt(storyId, 10))
+    )
+  );
 };
