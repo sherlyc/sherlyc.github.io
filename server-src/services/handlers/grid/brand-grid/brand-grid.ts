86c79861eaddd07f908215495b0ce0e1d7c0e2c5
Karen Xie <karen.xie@stuff.co.nz>
2020-02-18 14:01:16 +1300

    Inline mobile config

--- server-src/services/handlers/grid/brand-grid/brand-grid.ts
+++ server-src/services/handlers/grid/brand-grid/brand-grid.ts
@@ -5,10 +5,7 @@ import {
   IBrandGridHandlerInput,
   BrandGridPositions
 } from "../../__types__/IBrandGridHandlerInput";
-import {
-  Border,
-  IGridConfig
-} from "../../../../../common/__types__/IGridContainer";
+import { Border } from "../../../../../common/__types__/IGridContainer";
 import { ContentBlockType } from "../../../../../common/__types__/ContentBlockType";
 import { gridBlock } from "../../../adapters/grid/grid-block";
 
@@ -17,23 +14,21 @@ export default async function(
   { content }: IBrandGridHandlerInput,
   params: IParams
 ): Promise<IContentBlock[]> {
-  const mobile: IGridConfig = {
-    gridTemplateColumns: "1fr",
-    gridTemplateRows: "auto auto auto",
-    gridColumnGap: "20px",
-    gridRowGap: "20px",
-    gridBlocks: {
-      [BrandGridPositions.ModuleTitle]: gridBlock(1, 1, 1, 1, []),
-      [BrandGridPositions.FirstRow]: gridBlock(2, 1, 1, 1, [Border.bottom]),
-      [BrandGridPositions.SecondRow]: gridBlock(3, 1, 1, 1, [])
-    }
-  };
-
   return [
     {
       type: ContentBlockType.GridContainer,
       items: content,
-      mobile
+      mobile: {
+        gridTemplateColumns: "1fr",
+        gridTemplateRows: "auto auto auto",
+        gridColumnGap: "20px",
+        gridRowGap: "20px",
+        gridBlocks: {
+          [BrandGridPositions.ModuleTitle]: gridBlock(1, 1, 1, 1, []),
+          [BrandGridPositions.FirstRow]: gridBlock(2, 1, 1, 1, [Border.bottom]),
+          [BrandGridPositions.SecondRow]: gridBlock(3, 1, 1, 1, [])
+        }
+      }
     }
   ];
 }
