06cda41ffe47f39918971591963ac8415b181e11
Karen Xie <karen.xie@stuff.co.nz>
2020-02-19 09:49:01 +1300

    EX-782: Cleanup

--- server-src/services/handlers/grid/brand/bullet-list.spec.ts
+++ server-src/services/handlers/grid/brand/bullet-list.spec.ts
@@ -5,7 +5,6 @@ import { Logo } from "../../../../../common/Logo";
 import { Strap } from "../../../strap";
 import { getRawArticles } from "../../../adapters/article-retriever/article-retriever";
 import { IRawArticle } from "../../../adapters/__types__/IRawArticle";
-import objectContaining = jasmine.objectContaining;
 import { ContentBlockType } from "../../../../../common/__types__/ContentBlockType";
 import { IBulletItem } from "../../../../../common/__types__/IBulletItem";
 
