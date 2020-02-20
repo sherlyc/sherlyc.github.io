0c74743f99c463b1227e7fa0efd5f11a7bd0085e
Lucas Rainett <lucas.rainett@stuff.co.nz>
2020-02-20 15:23:42 +1300

    add placeholders for images

--- src/app/shared/components/fluid-image/fluid-image.component.ts
+++ src/app/shared/components/fluid-image/fluid-image.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from "@angular/core";
+import { Component, HostBinding, Input } from "@angular/core";
 import { FluidImageWidth } from "../../../../../common/FluidImageWidth";
 
 @Component({
@@ -9,8 +9,11 @@ import { FluidImageWidth } from "../../../../../common/FluidImageWidth";
 export class FluidImageComponent {
   @Input() imageSrc!: string;
   @Input() caption!: string;
+
   @Input() aspectRatio = "16:9,smart";
-  src!: string;
+  @HostBinding("style.paddingBottom") height = `${(9 / 16) * 100}%`;
+
+  src: string | undefined;
   srcset!: string;
   width = 0;
 
@@ -31,6 +34,11 @@ export class FluidImageComponent {
     const src = `${this.imageSrc}?format=pjpg&crop=${this.aspectRatio}&width=${newWidth}`;
     this.srcset = `${src}, ${src}&dpr=2 2x, ${src}&dpr=3 3x`;
     this.src = src;
+  }
+
+  error() {
+    this.src = undefined;
+    this.height = "0";
   }
 
   onResize({ contentRect: { width } }: ResizeObserverEntry) {
