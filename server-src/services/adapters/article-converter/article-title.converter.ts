2fe5e091fcf4885c21db3fc22c20669faad6acd9
Viet Anh Tran <viet.tran@fairfaxmedia.co.nz>
2020-02-25 11:32:24 +1300

    EX-853: Show timestamp

---
+++ server-src/services/adapters/article-converter/article-title.converter.ts
@@ -0,0 +1,17 @@
+import { IRawArticle } from "../__types__/IRawArticle";
+import { ContentBlockType } from "../../../../common/__types__/ContentBlockType";
+import { IArticleTitle } from "../../../../common/__types__/IArticleTitle";
+
+export const articleTitle = (
+  article: IRawArticle,
+  strapName: string
+): IArticleTitle => ({
+  type: ContentBlockType.ArticleTitle,
+  id: article.id,
+  strapName: strapName,
+  indexHeadline: article.indexHeadline,
+  title: article.title,
+  linkUrl: article.linkUrl,
+  lastPublishedTime: article.lastPublishedTime,
+  headlineFlags: article.headlineFlags
+});
--- src/app/content-blocks/article-title/article-title.component.ts
+++ src/app/content-blocks/article-title/article-title.component.ts
@@ -1,8 +1,14 @@
-import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnInit
+} from "@angular/core";
 import { IContentBlockComponent } from "../__types__/IContentBlockComponent";
 import { AnalyticsService } from "../../services/analytics/analytics.service";
 import { AnalyticsEventsType } from "../../services/analytics/__types__/AnalyticsEventsType";
 import { IArticleTitle } from "../../../../common/__types__/IArticleTitle";
+import * as moment from "moment";
 
 @Component({
   selector: "app-article-title",
@@ -10,9 +16,16 @@ import { IArticleTitle } from "../../../../common/__types__/IArticleTitle";
   styleUrls: ["./article-title.scss"],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ArticleTitleComponent implements IContentBlockComponent {
+export class ArticleTitleComponent implements IContentBlockComponent, OnInit {
   @Input() input!: IArticleTitle;
   index!: number;
+  timeStamp!: string;
+
+  ngOnInit() {
+    this.timeStamp = moment
+      .unix(this.input.lastPublishedTime as number)
+      .format("LT");
+  }
 
   constructor(private analyticsService: AnalyticsService) {}
 
