7bb88424253095a14a312862d73e88f4c72698fd
Karen Xie <karen.xie@stuff.co.nz>
2020-03-05 14:11:15 +1300

    EX-883: Fix ImageLinkUnit interface and pass in correct color

--- common/__types__/IImageLinkUnit.ts
+++ common/__types__/IImageLinkUnit.ts
@@ -1,17 +1,8 @@
 import { ContentBlockType } from "./ContentBlockType";
-import { HeadlineFlags } from "../HeadlineFlags";
-import { ImageLayoutType } from "./ImageLayoutType";
 import { IArticle } from "./IArticle";
+import { ImageLayoutType } from "./ImageLayoutType";
 
 export interface IImageLinkUnit extends IArticle {
   type: ContentBlockType.ImageLinkUnit;
-  id: string;
-  strapName: string;
-  indexHeadline: string;
-  title: string;
-  linkUrl: string;
-  imageSrc: string | null;
-  imageSrcSet: string | null;
-  headlineFlags: HeadlineFlags[];
   layout: ImageLayoutType;
 }
