fe4e7301af765389d0e4e987a9bd33c60e027370
Sherly Chan <sherly.chan@fairfaxmedia.co.nz>
2020-03-05 14:24:28 +1300

    EX-860: apply analytics to module title component

--- src/app/content-blocks/module-title/module-title.component.ts
+++ src/app/content-blocks/module-title/module-title.component.ts
@@ -6,6 +6,8 @@ import {
 } from "@angular/core";
 import { IContentBlockComponent } from "../__types__/IContentBlockComponent";
 import { IModuleTitle } from "../../../../common/__types__/IModuleTitle";
+import { AnalyticsEventsType } from "../../services/analytics/__types__/AnalyticsEventsType";
+import { AnalyticsService } from "../../services/analytics/analytics.service";
 
 @Component({
   selector: "app-module-title-component",
@@ -17,7 +19,16 @@ export class ModuleTitleComponent implements IContentBlockComponent, OnInit {
   @Input()
   input!: IModuleTitle;
 
-  constructor() {}
+  constructor(private analyticsService: AnalyticsService) {}
 
   ngOnInit() {}
+
+  sendAnalytics() {
+    const { displayName } = this.input;
+
+    this.analyticsService.pushEvent({
+      type: AnalyticsEventsType.MODULE_TITLE_CLICKED,
+      title: displayName
+    });
+  }
 }
