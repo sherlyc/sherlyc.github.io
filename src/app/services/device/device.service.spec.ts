902a3784aba76d1e14b300653506b5c603289c9e
James Shih <james.shih@stuff.co.nz>
2020-03-17 17:20:24 +1300

    EX-892 CSS Grid feature detection

--- src/app/services/device/device.service.spec.ts
+++ src/app/services/device/device.service.spec.ts
@@ -1,16 +1,22 @@
 import { TestBed } from "@angular/core/testing";
+import { DeviceType } from "../../../../common/DeviceType";
+import { mockService, ServiceMock } from "../mocks/MockService";
+import { RuntimeService } from "../runtime/runtime.service";
+import { WindowService } from "../window/window.service";
 
 import { DeviceService } from "./device.service";
-import { WindowService } from "../window/window.service";
-import { mockService, ServiceMock } from "../mocks/MockService";
-import { DeviceType } from "../../../../common/DeviceType";
 
 describe("DeviceService", () => {
   let deviceService: DeviceService;
+  let runtimeService: ServiceMock<RuntimeService>;
   let windowService: ServiceMock<WindowService>;
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
+        {
+          provide: RuntimeService,
+          useClass: mockService(RuntimeService)
+        },
         {
           provide: WindowService,
           useClass: mockService(WindowService)
@@ -18,6 +24,7 @@ describe("DeviceService", () => {
       ]
     });
 
+    runtimeService = TestBed.get(RuntimeService);
     windowService = TestBed.get(WindowService);
     deviceService = TestBed.get(DeviceService);
   });
@@ -26,30 +33,76 @@ describe("DeviceService", () => {
     expect(deviceService).toBeTruthy();
   });
 
-  it.each([
-    [
-      DeviceType.mobile,
-      "Mozilla/5.0 (Linux; Android 4.4.2; Nexus 4 Build/KOT49H)"
-    ],
-    [
-      DeviceType.tablet,
-      "Mozilla/5.0 (iPad; CPU OS 11_0 like Mac OS X) Safari/604.1"
-    ],
-    [
-      DeviceType.desktop,
-      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) Chrome/78.0.3904.97 Safari/537.36"
-    ],
-    [DeviceType.unknown, "Invalid UserAgent"]
-  ])(
-    "should get %s device from user agent %s",
-    (device: string, userAgent: string) => {
-      windowService.getWindow.mockReturnValue({
-        navigator: { userAgent }
+  describe("getDevice", () => {
+    it.each([
+      [
+        DeviceType.mobile,
+        "Mozilla/5.0 (Linux; Android 4.4.2; Nexus 4 Build/KOT49H)"
+      ],
+      [
+        DeviceType.tablet,
+        "Mozilla/5.0 (iPad; CPU OS 11_0 like Mac OS X) Safari/604.1"
+      ],
+      [
+        DeviceType.desktop,
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) Chrome/78.0.3904.97 Safari/537.36"
+      ],
+      [DeviceType.unknown, "Invalid UserAgent"]
+    ])(
+      "should get %s device from user agent %s",
+      (device: string, userAgent: string) => {
+        windowService.getWindow.mockReturnValue({
+          navigator: { userAgent }
+        });
+
+        const result = deviceService.getDevice();
+
+        expect(result).toEqual(device);
+      }
+    );
+  });
+
+  describe("isGridSupported", () => {
+    describe("when in browser", () => {
+      beforeEach(() => {
+        jest.spyOn(runtimeService, "isBrowser").mockReturnValueOnce(true);
       });
 
-      const result = deviceService.getDevice();
+      it("returns true when CSS Grid is supported", () => {
+        jest.spyOn(document, "createElement").mockReturnValueOnce({
+          style: { gridTemplateColumns: "" }
+        } as HTMLElement);
+        expect(deviceService.isGridSupported()).toBe(true);
+      });
 
-      expect(result).toEqual(device);
-    }
-  );
+      it("returns true when CSS Grid (IE) is supported", () => {
+        jest.spyOn(document, "createElement").mockReturnValueOnce({
+          style: { msGridColumns: "" }
+        } as HTMLElement);
+        expect(deviceService.isGridSupported()).toBe(true);
+      });
+
+      it("returns false when CSS Grid is not supported", () => {
+        jest.spyOn(document, "createElement").mockReturnValueOnce({
+          style: {}
+        } as HTMLElement);
+        expect(deviceService.isGridSupported()).toBe(false);
+      });
+    });
+
+    describe("when in server", () => {
+      beforeEach(() => {
+        jest.spyOn(runtimeService, "isBrowser").mockReturnValueOnce(false);
+      });
+
+      it("returns true", () => {
+        jest.spyOn(document, "createElement").mockImplementationOnce(() => {
+          throw new Error(
+            "document.createElement is not supposed to be called"
+          );
+        });
+        expect(deviceService.isGridSupported()).toBe(true);
+      });
+    });
+  });
 });
--- src/app/services/device/device.service.ts
+++ src/app/services/device/device.service.ts
@@ -1,18 +1,33 @@
 import { Injectable } from "@angular/core";
-import { WindowService } from "../window/window.service";
 import { parse } from "bowser";
 import { DeviceType } from "../../../../common/DeviceType";
+import { RuntimeService } from "../runtime/runtime.service";
+import { WindowService } from "../window/window.service";
 
 @Injectable({
   providedIn: "root"
 })
 export class DeviceService {
-  constructor(private windowService: WindowService) {}
+  constructor(
+    private runtimeService: RuntimeService,
+    private windowService: WindowService
+  ) {}
 
   getDevice() {
     return (
       (parse(this.windowService.getWindow().navigator.userAgent).platform
         .type as DeviceType) || DeviceType.unknown
     );
+  }
+
+  isGridSupported() {
+    if (this.runtimeService.isBrowser()) {
+      const { style } = document.createElement("div");
+      return ["gridTemplateColumns", "msGridColumns"].some(
+        (key) => key in style
+      );
+    } else {
+      return true;
+    }
   }
 }
