509b50ccac2fef005aaa57bd31ef92fb0fc3b670
Viet Anh Tran <viet.tran@fairfaxmedia.co.nz>
2020-03-23 15:53:14 +1300

    WIP

--- server-src/spade-api.ts
+++ server-src/spade-api.ts
@@ -5,6 +5,14 @@ import { getWeather } from "./api/weather";
 import { versionGuard } from "./middlewares/version-guard";
 import { versionParityCheck } from "./middlewares/version-parity-check";
 import { getContent } from "./services/content";
+import * as httpProxy from "http-proxy";
+
+const adnosticProxy = httpProxy.createServer({
+  target: "https://provider.adsproduction.shift21.ffx.nz",
+  headers: {
+    host: "www.stuff.co.nz"
+  }
+});
 
 const versionedRouter = express.Router();
 versionedRouter.get("/content", getContent);
@@ -32,6 +40,10 @@ spadeRouter.get(
   "/feature/:featureName/:lotteryNumber/:deviceType",
   featureController
 );
+
+spadeRouter.get("/adnostic/*", (req, res) => {
+  adnosticProxy.web(req, res);
+});
 
 spadeRouter.use("/:version", versionGuard, versionParityCheck, versionedRouter);
 
