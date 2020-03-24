3aba33fc2487c7d3c5f896efc263358d33dbecfc
James Shih <james.shih@fairfaxmedia.co.nz>
2020-03-24 04:10:20 +0000

    Revert "Break cache for external content (pull request #848)"

--- server-src/services/handlers/responsive-external-content/responsive-external-content.ts
+++ server-src/services/handlers/responsive-external-content/responsive-external-content.ts
@@ -3,8 +3,8 @@ import { ContentBlockType } from "../../../../common/__types__/ContentBlockType"
 import { handlerRunnerFunction } from "../runner";
 import { IParams } from "../../__types__/IParams";
 import { IResponsiveExternalContentHandlerInput } from "../__types__/IResponsiveExternalContentHandlerInput";
+import cacheHttp from "../../utils/cache-http";
 import logger from "../../utils/logger";
-import http from "../../utils/http";
 
 export default async function(
   handlerRunner: handlerRunnerFunction,
@@ -19,9 +19,7 @@ export default async function(
   params: IParams
 ): Promise<IContentBlock[]> {
   try {
-    await http(params).get(url, {
-      params: { "cache-bust": `${Math.random()}` }
-    });
+    await cacheHttp(params, url);
   } catch (error) {
     logger.warn(
       params.apiRequestId,
