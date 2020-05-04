c657e17dabfa41822753b3602e1941f0a52f2e77
James Shih <james.shih@fairfaxmedia.co.nz>
2020-05-04 23:38:28 +0000

    EX-934: Add cache headers for CDN (pull request #889)

--- server-src/services/content.spec.ts
+++ server-src/services/content.spec.ts
@@ -1,11 +1,31 @@
-import { getContent } from "./content";
 import { Request } from "express";
+import { getContent } from "./content";
 import * as orchestrate from "./orchestrator";
+
+jest.mock("./utils/config", () => ({
+  default: {
+    strapConfig: {
+      homepageStraps: {
+        strapTopStories: {
+          ids: ["42", "43"]
+        }
+      }
+    },
+    loggerOptions: {
+      level: "debug",
+      format: "text"
+    }
+  }
+}));
 
 describe("Content Controller", () => {
   it("should send 500 status when orchestrator throws error", async () => {
     const req = { spadeParams: {} } as Request;
-    const res = { json: jest.fn(), sendStatus: jest.fn() } as any;
+    const res = {
+      set: jest.fn(),
+      json: jest.fn(),
+      sendStatus: jest.fn()
+    } as any;
     jest.spyOn(orchestrate, "default").mockRejectedValue(new Error());
 
     await getContent(req, res);
@@ -15,7 +35,7 @@ describe("Content Controller", () => {
 
   it("should return result and end request when orchestrator successfully returns", async () => {
     const req = { spadeParams: {} } as Request;
-    const res = { json: jest.fn(), end: jest.fn() } as any;
+    const res = { set: jest.fn(), json: jest.fn(), end: jest.fn() } as any;
 
     const result = {
       apiRequestId: "123",
@@ -28,6 +48,10 @@ describe("Content Controller", () => {
     await getContent(req, res);
 
     expect(res.json).toHaveBeenCalledWith(result);
+    expect(res.set).toHaveBeenCalledWith({
+      "Surrogate-Key": "42 43",
+      "Edge-Cache-Tag": "42, 43"
+    });
     expect(res.end).toHaveBeenCalled();
   });
 });
--- server-src/services/content.ts
+++ server-src/services/content.ts
@@ -1,9 +1,21 @@
-import orchestrate from "./orchestrator";
-import logger from "./utils/logger";
 import { Request, Response } from "express";
+import orchestrate from "./orchestrator";
+import { IStrapDefinition } from "./utils/__types__/IStrapDefinition";
+import config from "./utils/config";
+import logger from "./utils/logger";
+
+const listAssetIds = Object.values(config.strapConfig.homepageStraps).reduce(
+  (acc: string[], curr: IStrapDefinition) => acc.concat(curr.ids),
+  []
+);
+const cdnCacheHeaders = {
+  "Surrogate-Key": listAssetIds.join(" "),
+  "Edge-Cache-Tag": listAssetIds.join(", ")
+};
 
 export const getContent = async (req: Request, res: Response) => {
   try {
+    res.set(cdnCacheHeaders);
     res.json(await orchestrate(req.spadeParams));
     res.end();
   } catch (error) {
