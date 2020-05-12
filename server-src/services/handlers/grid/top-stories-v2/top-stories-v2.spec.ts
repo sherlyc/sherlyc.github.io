1e9896852b48f2c47cb8a21b1054b47d53fb5ec9
Karen Xie <karen.xie@stuff.co.nz>
2020-05-12 15:39:09 +1200

    EX-953: Retrieve 10 articles

--- server-src/services/handlers/grid/top-stories-v2/top-stories-v2.spec.ts
+++ server-src/services/handlers/grid/top-stories-v2/top-stories-v2.spec.ts
@@ -1,5 +1,27 @@
+import { getRawArticles } from "../../../adapters/article-retriever/article-retriever";
+import { IParams } from "../../../__types__/IParams";
+import { HandlerInputType } from "../../__types__/HandlerInputType";
+import { ITopStoriesV2HandlerInput } from "../../__types__/ITopStoriesV2HandlerInput";
+import { Strap } from "../../../strap";
+import topStoriesV2 from "./top-stories-v2";
+
+jest.mock("../../../adapters/article-retriever/article-retriever");
+
 describe("Top Stories V2", () => {
-  it("dummy test", () => {
-    expect(true).toBe(true);
+  const handlerRunnerMock = jest.fn();
+  const params: IParams = { apiRequestId: "123" };
+  const strapName = "Top Stories V2";
+  const color = "blue";
+
+  const handlerInput: ITopStoriesV2HandlerInput = {
+    type: HandlerInputType.TopStoriesV2,
+    strapName,
+    color
+  };
+
+  it("should retrieve articles", async () => {
+    await topStoriesV2(handlerRunnerMock, handlerInput, params);
+
+    expect(getRawArticles).toHaveBeenCalledWith(Strap.TopStories, 10, params);
   });
 });
--- server-src/services/handlers/grid/top-stories-v2/top-stories-v2.ts
+++ server-src/services/handlers/grid/top-stories-v2/top-stories-v2.ts
@@ -2,11 +2,14 @@ import { handlerRunnerFunction } from "../../runner";
 import { IParams } from "../../../__types__/IParams";
 import { IContentBlock } from "../../../../../common/__types__/IContentBlock";
 import { ITopStoriesV2HandlerInput } from "../../__types__/ITopStoriesV2HandlerInput";
+import { getRawArticles } from "../../../adapters/article-retriever/article-retriever";
+import { Strap } from "../../../strap";
 
 export default async function(
   handlerRunner: handlerRunnerFunction,
   { strapName, color }: ITopStoriesV2HandlerInput,
   params: IParams
 ): Promise<IContentBlock[]> {
+  const articles = getRawArticles(Strap.TopStories, 10, params);
   return [];
 }
