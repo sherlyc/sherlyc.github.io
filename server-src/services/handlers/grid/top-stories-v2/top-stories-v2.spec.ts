38071d656b010ebcd9f7d1a1bc1aa36ba311716c
Karen Xie <karen.xie@stuff.co.nz>
2020-05-12 16:23:03 +1200

    EX-953: Failing test for grid handler input

--- server-src/services/handlers/grid/top-stories-v2/top-stories-v2.spec.ts
+++ server-src/services/handlers/grid/top-stories-v2/top-stories-v2.spec.ts
@@ -4,6 +4,10 @@ import { HandlerInputType } from "../../__types__/HandlerInputType";
 import { ITopStoriesV2HandlerInput } from "../../__types__/ITopStoriesV2HandlerInput";
 import { Strap } from "../../../strap";
 import topStoriesV2 from "./top-stories-v2";
+import { ITopStoriesV2GridHandlerInput, TopStoriesV2GridPositions } from "../../__types__/ITopStoriesV2GridHandlerInput";
+import { ContentBlockType } from "../../../../../common/__types__/ContentBlockType";
+import { IRawArticle } from "../../../adapters/__types__/IRawArticle";
+import { IBasicAdUnit } from "../../../../../common/__types__/IBasicAdUnit";
 
 jest.mock("../../../adapters/article-retriever/article-retriever");
 
@@ -19,9 +23,77 @@ describe("Top Stories V2", () => {
     color
   };
 
+  const basicAdUnit: IBasicAdUnit = {
+    type: ContentBlockType.BasicAdUnit,
+    context: strapName
+  };
+
+  const fakeArticlesWithIds = (ids: number[]) =>
+    ids.map((id) => ({ id: `${id}` } as IRawArticle));
+
+  const expectBigImageArticle = (id: string) =>
+    expect.objectContaining({
+      type: ContentBlockType.BigImageArticleUnit,
+      id
+    });
+
+  const expectFeaturedArticle = (id: string) =>
+    expect.objectContaining({
+      type: ContentBlockType.FeaturedArticle,
+      id
+    });
+
+  const expectHalfWidthImage = (id: string) =>
+    expect.objectContaining({
+      type: ContentBlockType.HalfWidthImageArticleUnit,
+      id
+    });
+
+  const expectBasicArticleTitle = (id: string) =>
+    expect.objectContaining({
+      type: ContentBlockType.BasicArticleTitleUnit,
+      id
+    });
+
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
   it("should retrieve articles", async () => {
     await topStoriesV2(handlerRunnerMock, handlerInput, params);
 
     expect(getRawArticles).toHaveBeenCalledWith(Strap.TopStories, 9, params);
+  });
+
+  it("should call top stories v2 grid", async () => {
+    (getRawArticles as jest.Mock).mockResolvedValue(
+      fakeArticlesWithIds([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    );
+
+    await topStoriesV2(handlerRunnerMock, handlerInput, params);
+
+    const gridHandlerInput: ITopStoriesV2GridHandlerInput = {
+      type: HandlerInputType.TopStoriesV2Grid,
+      content: {
+        [TopStoriesV2GridPositions.LeftHighlight]: [
+          expect.objectContaining({
+            type: ContentBlockType.BigImageArticleUnit,
+            id: "1"
+          })
+        ],
+        [TopStoriesV2GridPositions.RightHighlight]: [expectFeaturedArticle("2")],
+        [TopStoriesV2GridPositions.BannerAd]: [{ type: ContentBlockType.StickyContainer, items: [basicAdUnit]}],
+        [TopStoriesV2GridPositions.LeftOne]: [expectBigImageArticle("3")],
+        [TopStoriesV2GridPositions.LeftTwo]: [expectBigImageArticle("4")],
+        [TopStoriesV2GridPositions.LeftThree]: [expectBigImageArticle("5")],
+        [TopStoriesV2GridPositions.LeftFour]: [basicAdUnit],
+        [TopStoriesV2GridPositions.RightOne]: [expectHalfWidthImage("6")],
+        [TopStoriesV2GridPositions.RightTwo]: [expectHalfWidthImage("7")],
+        [TopStoriesV2GridPositions.RightThree]: [expectBasicArticleTitle("8")],
+        [TopStoriesV2GridPositions.RightFour]: [expectBasicArticleTitle("9")]
+      }
+    };
+
+    expect(handlerRunnerMock).toHaveBeenCalledWith(gridHandlerInput, params);
   });
 });
--- server-src/services/handlers/grid/top-stories-v2/top-stories-v2.ts
+++ server-src/services/handlers/grid/top-stories-v2/top-stories-v2.ts
@@ -4,12 +4,168 @@ import { IContentBlock } from "../../../../../common/__types__/IContentBlock";
 import { ITopStoriesV2HandlerInput } from "../../__types__/ITopStoriesV2HandlerInput";
 import { getRawArticles } from "../../../adapters/article-retriever/article-retriever";
 import { Strap } from "../../../strap";
+import { ITopStoriesV2GridHandlerInput, TopStoriesV2GridPositions } from "../../__types__/ITopStoriesV2GridHandlerInput";
+import { HandlerInputType } from "../../__types__/HandlerInputType";
+import { bigImageArticleUnit } from "../../../adapters/article-converter/big-image-article.converter";
+import { contentErrorHandler } from "../content-error-handler";
+import { imageLinkUnit } from "../../../adapters/article-converter/image-link-unit.converter";
+import { IRawArticle } from "../../../adapters/__types__/IRawArticle";
+import { ImageLayoutType } from "../../../../../common/__types__/ImageLayoutType";
+import { ContentBlockType } from "../../../../../common/__types__/ContentBlockType";
+import { basicAdUnit } from "../../../adapters/article-converter/basic-ad-unit.converter";
+import { halfWidthImageArticleUnit } from "../../../adapters/article-converter/half-width-image-article-unit.converter";
+import { basicArticleTitleUnit } from "../../../adapters/article-converter/basic-article-title.converter";
 
 export default async function(
   handlerRunner: handlerRunnerFunction,
   { strapName, color }: ITopStoriesV2HandlerInput,
   params: IParams
 ): Promise<IContentBlock[]> {
-  const articles = getRawArticles(Strap.TopStories, 9, params);
-  return [];
+  const articles = await getRawArticles(Strap.TopStories, 9, params);
+
+  console.log(articles);
+  const gridInput: ITopStoriesV2GridHandlerInput = {
+    type: HandlerInputType.TopStoriesV2Grid,
+    content: {
+      [TopStoriesV2GridPositions.LeftHighlight]: [
+        contentErrorHandler(
+        () =>
+          bigImageArticleUnit(
+            articles.shift() as IRawArticle,
+            strapName,
+            ImageLayoutType.module
+          ),
+        HandlerInputType.TopStoriesV2,
+        Strap.TopStories,
+        params
+      )],
+      [TopStoriesV2GridPositions.RightHighlight]: [
+        contentErrorHandler(
+          () =>
+            bigImageArticleUnit(
+              articles.shift() as IRawArticle,
+              strapName,
+              ImageLayoutType.module
+            ),
+          HandlerInputType.TopStoriesV2,
+          Strap.TopStories,
+          params
+        )
+      ],
+      [TopStoriesV2GridPositions.BannerAd]: [
+        {
+          type: ContentBlockType.StickyContainer,
+          items: [basicAdUnit(strapName)]
+        }
+      ],
+      [TopStoriesV2GridPositions.LeftOne]: [
+        contentErrorHandler(
+          () =>
+            bigImageArticleUnit(
+              articles.shift() as IRawArticle,
+              strapName,
+              ImageLayoutType.module
+            ),
+          HandlerInputType.TopStoriesV2,
+          Strap.TopStories,
+          params
+        )
+      ],
+      [TopStoriesV2GridPositions.LeftTwo]: [
+        contentErrorHandler(
+          () =>
+            bigImageArticleUnit(
+              articles.shift() as IRawArticle,
+              strapName,
+              ImageLayoutType.module
+            ),
+          HandlerInputType.TopStoriesV2,
+          Strap.TopStories,
+          params
+        )
+      ],
+      [TopStoriesV2GridPositions.LeftThree]: [
+        contentErrorHandler(
+          () =>
+            bigImageArticleUnit(
+              articles.shift() as IRawArticle,
+              strapName,
+              ImageLayoutType.module
+            ),
+          HandlerInputType.TopStoriesV2,
+          Strap.TopStories,
+          params
+        )
+      ],
+      [TopStoriesV2GridPositions.LeftFour]: [
+        contentErrorHandler(
+          () =>
+            bigImageArticleUnit(
+              articles.shift() as IRawArticle,
+              strapName,
+              ImageLayoutType.module
+            ),
+          HandlerInputType.TopStoriesV2,
+          Strap.TopStories,
+          params
+        )
+      ],
+      [TopStoriesV2GridPositions.RightOne]: [
+        contentErrorHandler(
+          () =>
+            halfWidthImageArticleUnit(
+              articles.shift() as IRawArticle,
+              strapName,
+              ImageLayoutType.module
+            ),
+          HandlerInputType.TopStoriesV2,
+          Strap.TopStories,
+          params
+        )
+      ],
+      [TopStoriesV2GridPositions.RightTwo]: [
+        contentErrorHandler(
+          () =>
+            halfWidthImageArticleUnit(
+              articles.shift() as IRawArticle,
+              strapName,
+              ImageLayoutType.module
+            ),
+          HandlerInputType.TopStoriesV2,
+          Strap.TopStories,
+          params
+        )
+      ],
+      [TopStoriesV2GridPositions.RightThree]: [
+        contentErrorHandler(
+          () =>
+            basicArticleTitleUnit(
+              articles.shift() as IRawArticle,
+              strapName,
+              ImageLayoutType.module
+            ),
+          HandlerInputType.TopStoriesV2,
+          Strap.TopStories,
+          params
+        )
+      ],
+      [TopStoriesV2GridPositions.RightFour]: [
+        contentErrorHandler(
+          () =>
+            basicArticleTitleUnit(
+              articles.shift() as IRawArticle,
+              strapName,
+              ImageLayoutType.module
+            ),
+          HandlerInputType.TopStoriesV2,
+          Strap.TopStories,
+          params
+        )
+      ]
+    }
+  };
+
+  console.log(gridInput.content[TopStoriesV2GridPositions.LeftHighlight]);
+
+  return await handlerRunner(gridInput, params);
 }
