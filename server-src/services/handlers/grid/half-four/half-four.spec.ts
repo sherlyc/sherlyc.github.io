03e61e05aa60e73597d0ba6c121a0a6e2e3f44c7
Karen Xie <karen.xie@stuff.co.nz>
2020-05-14 14:14:55 +1200

    EX-788: Put content blocks in grid

--- server-src/services/handlers/grid/half-four/half-four.spec.ts
+++ server-src/services/handlers/grid/half-four/half-four.spec.ts
@@ -1,5 +1,67 @@
+import halfFour from "./half-four";
+import { IParams } from "../../../__types__/IParams";
+import { IHalfFourHandlerInput } from "../../__types__/IHalfFourHandlerInput";
+import { HandlerInputType } from "../../__types__/HandlerInputType";
+import { Strap } from "../../../strap";
+import { getRawArticles } from "../../../adapters/article-retriever/article-retriever";
+import { IRawArticle } from "../../../adapters/__types__/IRawArticle";
+import { HalfFourGridPositions, IHalfFourGridHandlerInput } from "../../__types__/IHalfFourGridHandlerInput";
+import { ContentBlockType } from "../../../../../common/__types__/ContentBlockType";
+
+jest.mock("../../../adapters/article-retriever/article-retriever");
+
 describe("Half four", () => {
-  it("should do stuff", () => {
-    expect(true).toBe(true);
+  const handlerRunnerMock = jest.fn();
+  const params: IParams = { apiRequestId: "123" };
+  const input: IHalfFourHandlerInput = {
+    type: HandlerInputType.HalfFour,
+    sourceId: Strap.NZFarmer,
+    displayName: "NZ Farmer",
+    color: "Orange",
+    linkUrl: "/nz-farmer",
+    strapName: "nz-farmer"
+  };
+
+  const articlesWithIds = (ids: number[]) =>
+    ids.map((id) => ({ id: `${id}` } as IRawArticle));
+
+  const expectContentBlock = (type: ContentBlockType, id: number) =>
+    expect.objectContaining({
+      type,
+      id: `${id}`
+    });
+
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it("should retrieve four articles", async () => {
+    await halfFour(handlerRunnerMock, input, params);
+
+    expect(getRawArticles).toHaveBeenCalledWith(input.sourceId, 4, params);
+  });
+
+  it("should call half four grid with correct content", async () => {
+    (getRawArticles as jest.Mock).mockResolvedValue(articlesWithIds([1, 2, 3, 4]));
+
+    await halfFour(handlerRunnerMock, input, params);
+
+    const expectedGrid: IHalfFourGridHandlerInput = {
+      type: HandlerInputType.HalfFourGrid,
+      content: {
+        [HalfFourGridPositions.ModuleTitle]: [{
+          type: ContentBlockType.ModuleTitle,
+          displayName: input.displayName,
+          displayNameColor: input.color,
+          linkUrl: input.linkUrl,
+        }],
+        [HalfFourGridPositions.Left]: [expectContentBlock(ContentBlockType.FeaturedArticle, 1)],
+        [HalfFourGridPositions.RightOne]: [expectContentBlock(ContentBlockType.BasicArticleTitleUnit, 2)],
+        [HalfFourGridPositions.RightTwo]: [expectContentBlock(ContentBlockType.BasicArticleTitleUnit, 3)],
+        [HalfFourGridPositions.RightThree]: [expectContentBlock(ContentBlockType.BasicArticleTitleUnit, 4)],
+      }
+    };
+
+    expect(handlerRunnerMock).toHaveBeenCalledWith(expectedGrid, params);
   });
 });
--- server-src/services/handlers/grid/half-four/half-four.ts
+++ server-src/services/handlers/grid/half-four/half-four.ts
@@ -2,6 +2,16 @@ import { handlerRunnerFunction } from "../../runner";
 import { IParams } from "../../../__types__/IParams";
 import { IContentBlock } from "../../../../../common/__types__/IContentBlock";
 import { IHalfFourHandlerInput } from "../../__types__/IHalfFourHandlerInput";
+import { getRawArticles } from "../../../adapters/article-retriever/article-retriever";
+import {HalfFourGridPositions, IHalfFourGridHandlerInput} from "../../__types__/IHalfFourGridHandlerInput";
+import {HandlerInputType} from "../../__types__/HandlerInputType";
+import {ContentBlockType} from "../../../../../common/__types__/ContentBlockType";
+import {featuredArticle} from "../../../adapters/article-converter/featured-article.converter";
+import {contentErrorHandler} from "../content-error-handler";
+import {responsiveBigImageArticleUnit} from "../../../adapters/article-converter/responsive-big-image-article.converter";
+import {IRawArticle} from "../../../adapters/__types__/IRawArticle";
+import {basicArticleUnit} from "../../../adapters/article-converter/basic-article-unit.converter";
+import {basicArticleTitleUnit} from "../../../adapters/article-converter/basic-article-title.converter";
 
 export default async function(
   handlerRunner: handlerRunnerFunction,
@@ -14,5 +24,72 @@ export default async function(
   }: IHalfFourHandlerInput,
   params: IParams
 ): Promise<IContentBlock[]> {
-  return [];
+  const articles = await getRawArticles(sourceId, 4, params);
+
+  const gridInput: IHalfFourGridHandlerInput = {
+    type: HandlerInputType.HalfFourGrid,
+    content: {
+      [HalfFourGridPositions.ModuleTitle]: [{
+        type: ContentBlockType.ModuleTitle,
+        displayName,
+        displayNameColor: color,
+        linkUrl,
+      }],
+      [HalfFourGridPositions.Left]: [
+        contentErrorHandler(
+          () =>
+            featuredArticle(
+              articles.shift() as IRawArticle,
+              strapName,
+              "black",
+              ""
+            ),
+          HandlerInputType.HalfFour,
+          sourceId,
+          params
+        )
+      ],
+      [HalfFourGridPositions.RightOne]: [
+        contentErrorHandler(
+          () =>
+            basicArticleTitleUnit(
+              articles.shift() as IRawArticle,
+              strapName,
+              color
+            ),
+          HandlerInputType.HalfFour,
+          sourceId,
+          params
+        )
+      ],
+      [HalfFourGridPositions.RightTwo]: [
+        contentErrorHandler(
+          () =>
+            basicArticleTitleUnit(
+              articles.shift() as IRawArticle,
+              strapName,
+              color
+            ),
+          HandlerInputType.HalfFour,
+          sourceId,
+          params
+        )
+      ],
+      [HalfFourGridPositions.RightThree]: [
+        contentErrorHandler(
+          () =>
+            basicArticleTitleUnit(
+              articles.shift() as IRawArticle,
+              strapName,
+              color
+            ),
+          HandlerInputType.HalfFour,
+          sourceId,
+          params
+        )
+      ],
+    }
+  };
+
+  return await handlerRunner(gridInput, params);
 }
