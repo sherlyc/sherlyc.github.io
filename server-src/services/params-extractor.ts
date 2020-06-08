85aa79b84ef8aba146beaf851ab39fc7ac80c373
Lucas Rainett <lucas@rainett.dev>
2020-06-08 23:56:27 +1200

    update everything

--- server-src/services/params-extractor.ts
+++ server-src/services/params-extractor.ts
@@ -1,10 +1,10 @@
 import { IParams } from "./__types__/IParams";
-import * as uuidv4 from "uuid/v4";
+import { v4 } from "uuid";
 import { Request } from "express";
 
 export default (req: Request): IParams => {
   return {
-    apiRequestId: uuidv4(),
-    authorization: req.header("Authorization")
+    apiRequestId: v4(),
+    authorization: req.header("Authorization"),
   };
 };
