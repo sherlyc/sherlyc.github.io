7bf6f3fc6a76b32383b1e899c801dbe26f500e88
Chao Yang <chao.yang@fairfaxmedia.co.nz>
2020-06-08 15:13:49 +1200

    EX-956: unit tests

--- server-src/services/adapters/brightcove/brightcove-retriever.spec.ts
+++ server-src/services/adapters/brightcove/brightcove-retriever.spec.ts
@@ -1,18 +1,53 @@
 import { retrieveBrightcovePlaylist } from "./brightcove-retriever";
 import { IParams } from "../../__types__/IParams";
-import { playStuffConfig } from "../../handlers/play-stuff/play-stuff-config";
+import http from "../../utils/http";
+import * as brightcovePlaylist from "./__fixture__/brightcove-playlist.json";
+
+jest.mock("../../utils/http");
 
 describe("Brightcove retriever", () => {
   const params: IParams = { apiRequestId: "request-id-for-testing" };
+  const httpGet: jest.Mock = jest.fn();
+
+  const playlist = "playlist-id";
+  const account = "brightcove-account";
+  const policyKey = "brightcove-policy-key";
+  const total = 9;
+
+  beforeAll(() => {
+    (http as jest.Mock).mockReturnValue({
+      get: httpGet
+    });
+  });
+
   it("should retrieve", async () => {
-    const { playlist, account, policyKey } = playStuffConfig;
+    httpGet.mockResolvedValue({ data: brightcovePlaylist });
+
     const list = await retrieveBrightcovePlaylist(
       account,
       playlist,
       policyKey,
-      9,
+      total,
       params
     );
-    console.log(list);
+    expect(list).toEqual(brightcovePlaylist);
+    expect(httpGet).toHaveBeenCalledWith(
+      `https://edge.api.brightcove.com/playback/v1/accounts/${account}/playlists/${playlist}?limit=${total}`,
+      {
+        headers: {
+          Authorization: `BCOV-Policy ${policyKey}`
+        }
+      }
+    );
+  });
+
+  it("should throw error when failed to retrieve", async () => {
+    const error = new Error("Http Error");
+    httpGet.mockRejectedValue(error);
+    try {
+      await retrieveBrightcovePlaylist(account, playlist, policyKey, 9, params);
+    } catch (e) {
+      expect(e.message).toEqual(error.message);
+    }
   });
 });
