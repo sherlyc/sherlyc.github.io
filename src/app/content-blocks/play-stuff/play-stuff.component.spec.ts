16c09a5d45b3bcf03aff16ce7f9518b973f3ba6e
Karen Xie <karen.xie@stuff.co.nz>
2020-06-09 15:10:37 +1200

    EX-956: Render 8 videos test

--- src/app/content-blocks/play-stuff/play-stuff.component.spec.ts
+++ src/app/content-blocks/play-stuff/play-stuff.component.spec.ts
@@ -1,24 +1,71 @@
-import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import {async, ComponentFixture, TestBed} from "@angular/core/testing";
 
-import { PlayStuffComponent } from "./play-stuff.component";
+import {PlayStuffComponent} from "./play-stuff.component";
+import {IPlayStuff} from "../../../../common/__types__/IPlayStuff";
+import {ContentBlockType} from "../../../../common/__types__/ContentBlockType";
+import {By} from "@angular/platform-browser";
+import {NO_ERRORS_SCHEMA} from "@angular/core";
 
 describe("PlayStuffComponent", () => {
   let component: PlayStuffComponent;
   let fixture: ComponentFixture<PlayStuffComponent>;
 
+  const fakeVideo = (id: string) => ({
+    id,
+    name: `name ${id}`,
+    description: `description ${id}`,
+    thumbnail: `thumbnail${id}.png`,
+    poster: `poster${id}.png`,
+  });
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [PlayStuffComponent]
+      declarations: [PlayStuffComponent],
+      schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(PlayStuffComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it("should create", () => {
+    component.input = {
+      type: ContentBlockType.PlayStuff,
+      videos: [
+        fakeVideo("1"),
+        fakeVideo("2"),
+        fakeVideo("3"),
+        fakeVideo("4"),
+        fakeVideo("5"),
+        fakeVideo("6"),
+        fakeVideo("7"),
+        fakeVideo("8")
+      ]
+    };
+    fixture.detectChanges();
     expect(component).toBeTruthy();
+  });
+
+  it("should render 8 videos", () => {
+    component.input = {
+      type: ContentBlockType.PlayStuff,
+      videos: [
+        fakeVideo("1"),
+        fakeVideo("2"),
+        fakeVideo("3"),
+        fakeVideo("4"),
+        fakeVideo("5"),
+        fakeVideo("6"),
+        fakeVideo("7"),
+        fakeVideo("8")
+      ]
+    };
+
+    fixture.detectChanges();
+    const videoElements = fixture.debugElement.queryAll(By.css("app-play-stuff-video"));
+
+    expect(videoElements).toHaveLength(8);
   });
 });
