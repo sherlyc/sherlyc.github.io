d49e37dd3649ab63b49c9dc7fe5e060b28c67f85
Karen Xie <karen.xie@stuff.co.nz>
2020-06-10 16:16:57 +1200

    EX-995: Test inputs

--- src/app/content-blocks/module-header/module-header.component.spec.ts
+++ src/app/content-blocks/module-header/module-header.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { ModuleHeaderComponent } from "./module-header.component";
+import { ContentBlockType } from "../../../../common/__types__/ContentBlockType";
+import { AccentColor } from "../../../../common/__types__/AccentColor";
+import { IModuleHeader, ModuleHeaderVariation } from "../../../../common/__types__/IModuleHeader";
+import { By } from "@angular/platform-browser";
 
 describe("ModuleHeaderComponent", () => {
   let component: ModuleHeaderComponent;
@@ -16,10 +20,35 @@ describe("ModuleHeaderComponent", () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ModuleHeaderComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it("should create", () => {
+    component.input = {
+      type: ContentBlockType.ModuleHeader,
+      color: AccentColor.TopStoriesBlue,
+    };
+    fixture.detectChanges();
     expect(component).toBeTruthy();
+  });
+
+  it("should set inputs", () => {
+    const input: IModuleHeader = {
+      type: ContentBlockType.ModuleHeader,
+      title: "Grumpy Cat",
+      url: "http://www.grumpy-cat.com",
+      color: AccentColor.TopStoriesBlue,
+      variation: ModuleHeaderVariation.NoLine
+    };
+    component.input = input;
+
+    fixture.detectChanges();
+
+    const wrapper = fixture.debugElement.query(By.css("div"));
+    expect(wrapper.styles.color).toBe(input.color);
+    expect(wrapper.nativeElement.className).toEqual(`variation-${input.variation}`.toLowerCase());
+
+    const link: HTMLAnchorElement = fixture.debugElement.query(By.css("a")).nativeElement;
+    expect(link.getAttribute("href")).toBe(input.url);
+    expect(link.textContent).toBe(input.title);
   });
 });
