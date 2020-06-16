341e6815a801593d9e63ead0f9c0cf7e2d25802b
Karen Xie <karen.xie@stuff.co.nz>
2020-06-16 15:15:52 +1200

    EX-992: Top stories parent retrieves 'defcon articles' and top stories articles

--- server-src/services/handlers/grid/top-stories-v2/top-stories-v2.spec.ts
+++ server-src/services/handlers/grid/top-stories-v2/top-stories-v2.spec.ts
@@ -1,5 +1,58 @@
+import { ITopStoriesV2HandlerInput } from "../../__types__/ITopStoriesV2HandlerInput";
+import { HandlerInputType } from "../../__types__/HandlerInputType";
+import { AccentColor } from "../../../../../common/__types__/AccentColor";
+import { Strap } from "../../../strap";
+import topStoriesV2 from "./top-stories-v2";
+import { IParams } from "../../../__types__/IParams";
+import { getRawArticles } from "../../../adapters/article-retriever/article-retriever";
+
+jest.mock("../../../adapters/article-retriever/article-retriever");
+
 describe("Top Stories V2", () => {
-  it("should do stuff", () => {
-    expect(true).toBe(true);
+  const handlerRunnerMock = jest.fn();
+  const params: IParams = { apiRequestId: "123" };
+  const input: ITopStoriesV2HandlerInput = {
+    type: HandlerInputType.TopStoriesV2,
+    color: AccentColor.CuriousBlue,
+    strapName: "topStoriesV2",
+    midInsertContent: {
+      type: HandlerInputType.ExternalContent,
+      url:
+        "https://interactives.stuff.co.nz/live/homepage/uber/corona/320-200.html",
+      width: "100%",
+      height: "43px",
+      margin: "0",
+    },
+    lowerRightContent: {
+      type: HandlerInputType.LatestHeadlines,
+      sourceId: Strap.LatestNews,
+      totalArticles: 7,
+      displayName: "latest headlines",
+      strapName: `homepageLatestHeadlines`,
+      color: "#ff433d",
+    },
+  };
+
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it("should retrieve defcon articles and top stories articles", async () => {
+    (getRawArticles as jest.Mock).mockResolvedValue([]);
+    await topStoriesV2(handlerRunnerMock, input, params);
+
+    expect(getRawArticles).toHaveBeenCalledTimes(2);
+    expect(getRawArticles).toHaveBeenNthCalledWith(
+      1,
+      Strap.LatestNews,
+      4,
+      params
+    ); // Replace with correct data source
+    expect(getRawArticles).toHaveBeenNthCalledWith(
+      2,
+      Strap.TopStories,
+      10,
+      params
+    );
   });
 });
--- server-src/services/handlers/grid/top-stories-v2/top-stories-v2.ts
+++ server-src/services/handlers/grid/top-stories-v2/top-stories-v2.ts
@@ -2,6 +2,8 @@ import { handlerRunnerFunction } from "../../runner";
 import { IParams } from "../../../__types__/IParams";
 import { IContentBlock } from "../../../../../common/__types__/IContentBlock";
 import { ITopStoriesV2HandlerInput } from "../../__types__/ITopStoriesV2HandlerInput";
+import { getRawArticles } from "../../../adapters/article-retriever/article-retriever";
+import { Strap } from "../../../strap";
 
 export default async function (
   handlerRunner: handlerRunnerFunction,
@@ -13,5 +15,7 @@ export default async function (
   }: ITopStoriesV2HandlerInput,
   params: IParams
 ): Promise<IContentBlock[]> {
+  const defconArticles = await getRawArticles(Strap.LatestNews, 4, params); // Replace with correct data source
+  const topStories = await getRawArticles(Strap.TopStories, 10, params);
   return [];
 }
