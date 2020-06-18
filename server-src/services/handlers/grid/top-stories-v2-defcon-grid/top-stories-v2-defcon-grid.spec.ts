faa58a6c654ec6733ae13c28376c3514363914d8
Karen Xie <karen.xie@stuff.co.nz>
2020-06-18 14:41:13 +1200

    Tweak desktop and tablet layout

--- server-src/services/handlers/grid/top-stories-v2-defcon-grid/top-stories-v2-defcon-grid.spec.ts
+++ server-src/services/handlers/grid/top-stories-v2-defcon-grid/top-stories-v2-defcon-grid.spec.ts
@@ -87,7 +87,7 @@ describe("Top Stories Defcon grid", () => {
     const [grid] = await topStoriesV2Defcon(handlerRunnerMock, input, params);
     expect((grid as IGridContainer).tablet).toEqual({
       gridTemplateColumns: "1fr 1fr 300px",
-      gridTemplateRows: "auto auto auto auto auto auto auto auto auto auto",
+      gridTemplateRows: repeat(" auto", 11).substring(1),
       gridColumnGap: "20px",
       gridRowGap: "20px",
       gridBlocks: {
@@ -98,33 +98,33 @@ describe("Top Stories Defcon grid", () => {
         [TopStoriesV2DefconGridPositions.TopTwo]: gridBlock(2, 2, 1, 1, [
           Border.bottom
         ]),
-        [TopStoriesV2DefconGridPositions.TopThree]: gridBlock(3, 1, 1, 1, [
+        [TopStoriesV2DefconGridPositions.TopThree]: gridBlock(3, 1, 1, 2, [
           Border.bottom
         ]),
-        [TopStoriesV2DefconGridPositions.TopFour]: gridBlock(3, 2, 1, 1, [
+        [TopStoriesV2DefconGridPositions.TopFour]: gridBlock(4, 1, 1, 2, [
           Border.bottom
         ]),
-        [TopStoriesV2DefconGridPositions.MidInsert]: gridBlock(4, 1, 1, 2, []),
         [TopStoriesV2DefconGridPositions.TopFive]: gridBlock(5, 1, 1, 2, [
           Border.bottom
         ]),
-        [TopStoriesV2DefconGridPositions.BottomOne]: gridBlock(6, 1, 1, 2, [
+        [TopStoriesV2DefconGridPositions.MidInsert]: gridBlock(6, 1, 1, 2, [Border.bottom]),
+        [TopStoriesV2DefconGridPositions.BottomOne]: gridBlock(7, 1, 2, 1, [
           Border.bottom
         ]),
-        [TopStoriesV2DefconGridPositions.BottomTwo]: gridBlock(7, 1, 1, 2, [
+        [TopStoriesV2DefconGridPositions.BottomTwo]: gridBlock(7, 2, 2, 1, [
           Border.bottom
         ]),
-        [TopStoriesV2DefconGridPositions.BottomThree]: gridBlock(8, 1, 1, 2, [
+        [TopStoriesV2DefconGridPositions.BottomThree]: gridBlock(9, 1, 1, 2, [
           Border.bottom
         ]),
-        [TopStoriesV2DefconGridPositions.BottomFour]: gridBlock(9, 1, 1, 2, [
+        [TopStoriesV2DefconGridPositions.BottomFour]: gridBlock(10, 1, 1, 2, [
           Border.bottom
         ]),
-        [TopStoriesV2DefconGridPositions.BottomFive]: gridBlock(10, 1, 1, 2, [
+        [TopStoriesV2DefconGridPositions.BottomFive]: gridBlock(11, 1, 1, 2, [
           Border.bottom
         ]),
-        [TopStoriesV2DefconGridPositions.BannerAd]: gridBlock(2, 3, 5, 1, []),
-        [TopStoriesV2DefconGridPositions.LowerRight]: gridBlock(7, 3, 4, 1, [])
+        [TopStoriesV2DefconGridPositions.BannerAd]: gridBlock(2, 3, 6, 1, []),
+        [TopStoriesV2DefconGridPositions.LowerRight]: gridBlock(8, 3, 4, 1, [])
       }
     });
   });
