53b3e0d1ad9164e0b561373acbd6b8fb65910f4b
Lucas Rainett <lucas@rainett.dev>
2020-06-20 01:45:45 +1200

    homepage article pixel perfect

--- server-src/services/handlers/grid/top-stories-v2-defcon-grid/top-stories-v2-defcon-grid.spec.ts
+++ server-src/services/handlers/grid/top-stories-v2-defcon-grid/top-stories-v2-defcon-grid.spec.ts
@@ -47,37 +47,35 @@ describe("Top Stories Defcon grid", () => {
       gridBlocks: {
         [TopStoriesV2DefconGridPositions.Defcon]: gridBlock(1, 1, 1, 2, []),
         [TopStoriesV2DefconGridPositions.BannerAd]: gridBlock(2, 1, 1, 2, []),
-        [TopStoriesV2DefconGridPositions.TopOne]: gridBlock(3, 1, 1, 1, [
-          Border.bottom
-        ]),
-        [TopStoriesV2DefconGridPositions.TopTwo]: gridBlock(3, 2, 1, 1, [
-          Border.bottom
-        ]),
-        [TopStoriesV2DefconGridPositions.TopThree]: gridBlock(4, 1, 1, 2, [
-          Border.bottom
-        ]),
-        [TopStoriesV2DefconGridPositions.TopFour]: gridBlock(5, 1, 1, 2, [
-          Border.bottom
-        ]),
-        [TopStoriesV2DefconGridPositions.TopFive]: gridBlock(6, 1, 1, 2, [
-          Border.bottom
-        ]),
+        [TopStoriesV2DefconGridPositions.TopOne]: gridBlock(3, 1, 1, 1, []),
+        [TopStoriesV2DefconGridPositions.TopTwo]: gridBlock(3, 2, 1, 1, []),
+        [TopStoriesV2DefconGridPositions.TopThree]: gridBlock(4, 1, 1, 2, []),
+        [TopStoriesV2DefconGridPositions.TopFour]: gridBlock(5, 1, 1, 2, []),
+        [TopStoriesV2DefconGridPositions.TopFive]: gridBlock(6, 1, 1, 2, []),
         [TopStoriesV2DefconGridPositions.MidInsert]: gridBlock(7, 1, 1, 2, []),
-        [TopStoriesV2DefconGridPositions.BottomOne]: gridBlock(8, 1, 1, 1, [
-          Border.bottom
-        ]),
-        [TopStoriesV2DefconGridPositions.BottomTwo]: gridBlock(8, 2, 1, 1, [
-          Border.bottom
-        ]),
-        [TopStoriesV2DefconGridPositions.BottomThree]: gridBlock(9, 1, 1, 2, [
-          Border.bottom
-        ]),
-        [TopStoriesV2DefconGridPositions.BottomFour]: gridBlock(10, 1, 1, 2, [
-          Border.bottom
-        ]),
-        [TopStoriesV2DefconGridPositions.BottomFive]: gridBlock(11, 1, 1, 2, [
-          Border.bottom
-        ]),
+        [TopStoriesV2DefconGridPositions.BottomOne]: gridBlock(8, 1, 1, 1, []),
+        [TopStoriesV2DefconGridPositions.BottomTwo]: gridBlock(8, 2, 1, 1, []),
+        [TopStoriesV2DefconGridPositions.BottomThree]: gridBlock(
+          9,
+          1,
+          1,
+          2,
+          []
+        ),
+        [TopStoriesV2DefconGridPositions.BottomFour]: gridBlock(
+          10,
+          1,
+          1,
+          2,
+          []
+        ),
+        [TopStoriesV2DefconGridPositions.BottomFive]: gridBlock(
+          11,
+          1,
+          1,
+          2,
+          []
+        ),
         [TopStoriesV2DefconGridPositions.LowerRight]: gridBlock(12, 1, 1, 2, [])
       }
     });
@@ -92,39 +90,35 @@ describe("Top Stories Defcon grid", () => {
       gridRowGap: "20px",
       gridBlocks: {
         [TopStoriesV2DefconGridPositions.Defcon]: gridBlock(1, 1, 1, 3, []),
-        [TopStoriesV2DefconGridPositions.TopOne]: gridBlock(2, 1, 1, 1, [
-          Border.bottom
-        ]),
-        [TopStoriesV2DefconGridPositions.TopTwo]: gridBlock(2, 2, 1, 1, [
-          Border.bottom
-        ]),
-        [TopStoriesV2DefconGridPositions.TopThree]: gridBlock(3, 1, 1, 2, [
-          Border.bottom
-        ]),
-        [TopStoriesV2DefconGridPositions.TopFour]: gridBlock(4, 1, 1, 2, [
-          Border.bottom
-        ]),
-        [TopStoriesV2DefconGridPositions.TopFive]: gridBlock(5, 1, 1, 2, [
-          Border.bottom
-        ]),
-        [TopStoriesV2DefconGridPositions.MidInsert]: gridBlock(6, 1, 1, 2, [
-          Border.bottom
-        ]),
-        [TopStoriesV2DefconGridPositions.BottomOne]: gridBlock(7, 1, 2, 1, [
-          Border.bottom
-        ]),
-        [TopStoriesV2DefconGridPositions.BottomTwo]: gridBlock(7, 2, 2, 1, [
-          Border.bottom
-        ]),
-        [TopStoriesV2DefconGridPositions.BottomThree]: gridBlock(9, 1, 1, 2, [
-          Border.bottom
-        ]),
-        [TopStoriesV2DefconGridPositions.BottomFour]: gridBlock(10, 1, 1, 2, [
-          Border.bottom
-        ]),
-        [TopStoriesV2DefconGridPositions.BottomFive]: gridBlock(11, 1, 1, 2, [
-          Border.bottom
-        ]),
+        [TopStoriesV2DefconGridPositions.TopOne]: gridBlock(2, 1, 1, 1, []),
+        [TopStoriesV2DefconGridPositions.TopTwo]: gridBlock(2, 2, 1, 1, []),
+        [TopStoriesV2DefconGridPositions.TopThree]: gridBlock(3, 1, 1, 2, []),
+        [TopStoriesV2DefconGridPositions.TopFour]: gridBlock(4, 1, 1, 2, []),
+        [TopStoriesV2DefconGridPositions.TopFive]: gridBlock(5, 1, 1, 2, []),
+        [TopStoriesV2DefconGridPositions.MidInsert]: gridBlock(6, 1, 1, 2, []),
+        [TopStoriesV2DefconGridPositions.BottomOne]: gridBlock(7, 1, 2, 1, []),
+        [TopStoriesV2DefconGridPositions.BottomTwo]: gridBlock(7, 2, 2, 1, []),
+        [TopStoriesV2DefconGridPositions.BottomThree]: gridBlock(
+          9,
+          1,
+          1,
+          2,
+          []
+        ),
+        [TopStoriesV2DefconGridPositions.BottomFour]: gridBlock(
+          10,
+          1,
+          1,
+          2,
+          []
+        ),
+        [TopStoriesV2DefconGridPositions.BottomFive]: gridBlock(
+          11,
+          1,
+          1,
+          2,
+          []
+        ),
         [TopStoriesV2DefconGridPositions.BannerAd]: gridBlock(2, 3, 6, 1, []),
         [TopStoriesV2DefconGridPositions.LowerRight]: gridBlock(8, 3, 4, 1, [])
       }
@@ -140,40 +134,24 @@ describe("Top Stories Defcon grid", () => {
       gridRowGap: "40px",
       gridBlocks: {
         [TopStoriesV2DefconGridPositions.Defcon]: gridBlock(1, 1, 1, 3, []),
-        [TopStoriesV2DefconGridPositions.TopOne]: gridBlock(2, 1, 3, 1, [
-          Border.bottom
-        ]),
-        [TopStoriesV2DefconGridPositions.TopTwo]: gridBlock(2, 2, 3, 1, [
-          Border.bottom
-        ]),
-        [TopStoriesV2DefconGridPositions.TopThree]: gridBlock(2, 3, 1, 1, [
-          Border.bottom
-        ]),
-        [TopStoriesV2DefconGridPositions.TopFour]: gridBlock(3, 3, 1, 1, [
-          Border.bottom
-        ]),
-        [TopStoriesV2DefconGridPositions.TopFive]: gridBlock(4, 3, 1, 1, [
-          Border.bottom
-        ]),
-        [TopStoriesV2DefconGridPositions.BottomOne]: gridBlock(6, 1, 3, 1, [
-          Border.bottom
-        ]),
-        [TopStoriesV2DefconGridPositions.BottomTwo]: gridBlock(6, 2, 3, 1, [
-          Border.bottom
-        ]),
-        [TopStoriesV2DefconGridPositions.BottomThree]: gridBlock(6, 3, 1, 1, [
-          Border.bottom
-        ]),
-        [TopStoriesV2DefconGridPositions.BottomFour]: gridBlock(7, 3, 1, 1, [
-          Border.bottom
-        ]),
-        [TopStoriesV2DefconGridPositions.BottomFive]: gridBlock(8, 3, 1, 1, [
-          Border.bottom
-        ]),
+        [TopStoriesV2DefconGridPositions.TopOne]: gridBlock(2, 1, 3, 1, []),
+        [TopStoriesV2DefconGridPositions.TopTwo]: gridBlock(2, 2, 3, 1, []),
+        [TopStoriesV2DefconGridPositions.TopThree]: gridBlock(2, 3, 1, 1, []),
+        [TopStoriesV2DefconGridPositions.TopFour]: gridBlock(3, 3, 1, 1, []),
+        [TopStoriesV2DefconGridPositions.TopFive]: gridBlock(4, 3, 1, 1, []),
+        [TopStoriesV2DefconGridPositions.BottomOne]: gridBlock(6, 1, 3, 1, []),
+        [TopStoriesV2DefconGridPositions.BottomTwo]: gridBlock(6, 2, 3, 1, []),
+        [TopStoriesV2DefconGridPositions.BottomThree]: gridBlock(
+          6,
+          3,
+          1,
+          1,
+          []
+        ),
+        [TopStoriesV2DefconGridPositions.BottomFour]: gridBlock(7, 3, 1, 1, []),
+        [TopStoriesV2DefconGridPositions.BottomFive]: gridBlock(8, 3, 1, 1, []),
         [TopStoriesV2DefconGridPositions.BannerAd]: gridBlock(1, 4, 3, 1, []),
-        [TopStoriesV2DefconGridPositions.MidInsert]: gridBlock(5, 1, 1, 3, [
-          Border.bottom
-        ]),
+        [TopStoriesV2DefconGridPositions.MidInsert]: gridBlock(5, 1, 1, 3, []),
         [TopStoriesV2DefconGridPositions.LowerRight]: gridBlock(4, 4, 5, 1, [])
       }
     });
