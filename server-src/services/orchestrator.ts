64a186e64c0ac875831b57bf4d2e13a3758a5675
Chao Yang <chao.yang@fairfaxmedia.co.nz>
2020-06-23 16:16:29 +1200

    EX-1013: make homepage as the fallback when feature flag is disabled

--- server-src/services/orchestrator.ts
+++ server-src/services/orchestrator.ts
@@ -13,6 +13,7 @@ import config from "./utils/config";
 import logger from "./utils/logger";
 import { formatVersion, parseVersion } from "./utils/version";
 import { IParams } from "./__types__/IParams";
+import { flatMap } from "lodash-es";
 
 const homepageStrapsConfig = config.strapConfig!.homepageStraps;
 
@@ -61,8 +62,13 @@ const billboard = (prefix: string): IContentBlockHandlerInput => ({
   ]
 });
 
+const interpolateBillboards = (
+  handlers: HandlerInput[],
+  billboard: HandlerInput
+) => [...flatMap(handlers, (handler) => [billboard, handler]), billboard];
+
 export const newPage = (): HandlerInput[] => {
-  const page: HandlerInput[] = [
+  const homepageV2: HandlerInput[] = [
     {
       type: HandlerInputType.ContentBlockHandler,
       contentBlocks: [
@@ -86,163 +92,175 @@ export const newPage = (): HandlerInput[] => {
       type: HandlerInputType.Weather
     },
     {
-      type: HandlerInputType.Feature,
-      name: FeatureName.HomepageV2,
-      content: ([
+      type: HandlerInputType.TopStoriesV2,
+      strapName: `${homepageV2AdPrefix}TopStoriesDefaultOne`,
+      color: AccentColor.CuriousBlue,
+      midInsertContent: {
+        type: HandlerInputType.ExternalContent,
+        url: "",
+        width: "100%",
+        height: "43px",
+        margin: "0"
+      },
+      lowerRightContent: {
+        type: HandlerInputType.LatestHeadlines,
+        sourceId: Strap.LatestNews,
+        totalArticles: 7,
+        displayName: "latest headlines",
+        strapName: `${homepageV2AdPrefix}LatestHeadlines`,
+        color: AccentColor.Coral
+      }
+    },
+    {
+      type: HandlerInputType.EditorsPicks,
+      displayName: "editors' picks",
+      color: AccentColor.Charcoal,
+      strapName: `${homepageV2AdPrefix}EditorsPicks`,
+      sourceId: Strap.EditorPicks
+    },
+    {
+      type: HandlerInputType.NewsSixV2,
+      displayName: "coronavirus",
+      color: AccentColor.DeepBlue,
+      linkUrl: "/" + Section.Coronavirus,
+      strapName: `${homepageV2AdPrefix}Coronavirus`,
+      sourceId: Strap.Coronavirus
+    },
+    {
+      type: HandlerInputType.NewsSixV2,
+      displayName: "national",
+      color: AccentColor.DeepBlue,
+      linkUrl: "/" + Section.National,
+      strapName: `${homepageV2AdPrefix}National`,
+      sourceId: Strap.National
+    },
+    {
+      type: HandlerInputType.MostRead,
+      displayName: "most read",
+      strapName: `${homepageV2AdPrefix}MostRead`
+    },
+    {
+      type: HandlerInputType.PlayStuff,
+      total: 8
+    },
+    {
+      type: HandlerInputType.NewsSixV2,
+      displayName: "world",
+      color: AccentColor.Denim,
+      linkUrl: "/" + Section.World,
+      strapName: `${homepageV2AdPrefix}World`,
+      sourceId: Strap.World
+    },
+    {
+      type: HandlerInputType.NewsSixV2,
+      displayName: "business",
+      color: AccentColor.NavyBlue,
+      linkUrl: "/" + Section.Business,
+      strapName: `${homepageV2AdPrefix}Business`,
+      sourceId: Strap.Business
+    },
+    {
+      type: HandlerInputType.LargeLeadSixV2,
+      displayName: "prosper",
+      color: AccentColor.NavyBlue,
+      linkUrl: "/" + Section.Prosper,
+      strapName: `${homepageV2AdPrefix}Prosper`,
+      sourceId: Strap.Prosper
+    },
+    {
+      type: HandlerInputType.LargeLeadSixV2,
+      displayName: "climate change",
+      color: AccentColor.DeepBlue,
+      linkUrl: "/" + Section.ClimateChange,
+      strapName: `${homepageV2AdPrefix}ClimateChange`,
+      sourceId: Strap.ClimateChange
+    },
+    {
+      type: HandlerInputType.BiggieSmallsV2,
+      displayName: "sport",
+      color: AccentColor.CardinalRed,
+      linkUrl: "/" + Section.Sport,
+      strapName: `${homepageV2AdPrefix}Sport`,
+      sourceId: Strap.Sport
+    },
+    {
+      type: HandlerInputType.BiggieSmallsV2,
+      displayName: "travel",
+      color: AccentColor.Aqua,
+      linkUrl: "/" + Section.Travel,
+      strapName: `${homepageV2AdPrefix}Travel`,
+      sourceId: Strap.Travel
+    },
+    {
+      type: HandlerInputType.BiggieSmallsV2,
+      displayName: "homed",
+      color: AccentColor.Cyan,
+      linkUrl: "/" + Section.Homed,
+      strapName: `${homepageV2AdPrefix}Homed`,
+      sourceId: Strap.Homed
+    },
+    {
+      type: HandlerInputType.EditorsPicks,
+      displayName: "life & style",
+      color: AccentColor.Crimson,
+      strapName: `${homepageV2AdPrefix}LifeStyle`,
+      sourceId: Strap.LifeStyle
+    },
+    {
+      type: HandlerInputType.BiggieSmallsV2,
+      displayName: "entertainment",
+      color: AccentColor.Violet,
+      linkUrl: "/" + Section.Entertainment,
+      strapName: `${homepageV2AdPrefix}Entertainment`,
+      sourceId: Strap.Entertainment
+    },
+    {
+      type: HandlerInputType.LargeLeadSixV2,
+      displayName: "motoring",
+      color: AccentColor.DustyGray,
+      linkUrl: "/" + Section.Motoring,
+      strapName: `${homepageV2AdPrefix}Motoring`,
+      sourceId: Strap.Motoring
+    },
+    {
+      type: HandlerInputType.StripsV2,
+      displayName: "spotlight",
+      color: AccentColor.DarkGray,
+      linkUrl: "/" + Section.Premium,
+      sourceId: Strap.Premium,
+      strapName: Strap.Premium,
+      articleCount: 4
+    },
+    {
+      type: HandlerInputType.Partner
+    },
+    {
+      type: HandlerInputType.Brand,
+      module: BrandModule.Network
+    }
+  ];
+  const homepage: HandlerInput[] = [
+    {
+      type: HandlerInputType.ContentBlockHandler,
+      contentBlocks: [
         {
-          type: HandlerInputType.TopStoriesV2,
-          strapName: `${homepageV2AdPrefix}TopStoriesDefaultOne`,
-          color: AccentColor.CuriousBlue,
-          midInsertContent: {
-            type: HandlerInputType.ExternalContent,
-            url: "",
-            width: "100%",
-            height: "43px",
-            margin: "0"
-          },
-          lowerRightContent: {
-            type: HandlerInputType.LatestHeadlines,
-            sourceId: Strap.LatestNews,
-            totalArticles: 7,
-            displayName: "latest headlines",
-            strapName: `${homepageV2AdPrefix}LatestHeadlines`,
-            color: AccentColor.Coral
+          type: ContentBlockType.Oli,
+          config: {
+            adUnitPath: "/6674/mob.stuff.homepage",
+            size: [320, 460],
+            targetingParams: {
+              spade: "true",
+              pos: "interstitial-portrait"
+            }
           }
-        },
-        {
-          type: HandlerInputType.EditorsPicks,
-          displayName: "editors' picks",
-          color: AccentColor.Charcoal,
-          strapName: `${homepageV2AdPrefix}EditorsPicks`,
-          sourceId: Strap.EditorPicks
-        },
-        {
-          type: HandlerInputType.NewsSixV2,
-          displayName: "coronavirus",
-          color: AccentColor.DeepBlue,
-          linkUrl: "/" + Section.Coronavirus,
-          strapName: `${homepageV2AdPrefix}Coronavirus`,
-          sourceId: Strap.Coronavirus
-        },
-        {
-          type: HandlerInputType.NewsSixV2,
-          displayName: "national",
-          color: AccentColor.DeepBlue,
-          linkUrl: "/" + Section.National,
-          strapName: `${homepageV2AdPrefix}National`,
-          sourceId: Strap.National
-        },
-        {
-          type: HandlerInputType.MostRead,
-          displayName: "most read",
-          strapName: `${homepageV2AdPrefix}MostRead`
-        },
-        {
-          type: HandlerInputType.PlayStuff,
-          total: 8
-        },
-        {
-          type: HandlerInputType.NewsSixV2,
-          displayName: "world",
-          color: AccentColor.Denim,
-          linkUrl: "/" + Section.World,
-          strapName: `${homepageV2AdPrefix}World`,
-          sourceId: Strap.World
-        },
-        {
-          type: HandlerInputType.NewsSixV2,
-          displayName: "business",
-          color: AccentColor.NavyBlue,
-          linkUrl: "/" + Section.Business,
-          strapName: `${homepageV2AdPrefix}Business`,
-          sourceId: Strap.Business
-        },
-        {
-          type: HandlerInputType.LargeLeadSixV2,
-          displayName: "prosper",
-          color: AccentColor.NavyBlue,
-          linkUrl: "/" + Section.Prosper,
-          strapName: `${homepageV2AdPrefix}Prosper`,
-          sourceId: Strap.Prosper
-        },
-        {
-          type: HandlerInputType.LargeLeadSixV2,
-          displayName: "climate change",
-          color: AccentColor.DeepBlue,
-          linkUrl: "/" + Section.ClimateChange,
-          strapName: `${homepageV2AdPrefix}ClimateChange`,
-          sourceId: Strap.ClimateChange
-        },
-        {
-          type: HandlerInputType.BiggieSmallsV2,
-          displayName: "sport",
-          color: AccentColor.CardinalRed,
-          linkUrl: "/" + Section.Sport,
-          strapName: `${homepageV2AdPrefix}Sport`,
-          sourceId: Strap.Sport
-        },
-        {
-          type: HandlerInputType.BiggieSmallsV2,
-          displayName: "travel",
-          color: AccentColor.Aqua,
-          linkUrl: "/" + Section.Travel,
-          strapName: `${homepageV2AdPrefix}Travel`,
-          sourceId: Strap.Travel
-        },
-        {
-          type: HandlerInputType.BiggieSmallsV2,
-          displayName: "homed",
-          color: AccentColor.Cyan,
-          linkUrl: "/" + Section.Homed,
-          strapName: `${homepageV2AdPrefix}Homed`,
-          sourceId: Strap.Homed
-        },
-        {
-          type: HandlerInputType.EditorsPicks,
-          displayName: "life & style",
-          color: AccentColor.Crimson,
-          strapName: `${homepageV2AdPrefix}LifeStyle`,
-          sourceId: Strap.LifeStyle
-        },
-        {
-          type: HandlerInputType.BiggieSmallsV2,
-          displayName: "entertainment",
-          color: AccentColor.Violet,
-          linkUrl: "/" + Section.Entertainment,
-          strapName: `${homepageV2AdPrefix}Entertainment`,
-          sourceId: Strap.Entertainment
-        },
-        {
-          type: HandlerInputType.LargeLeadSixV2,
-          displayName: "motoring",
-          color: AccentColor.DustyGray,
-          linkUrl: "/" + Section.Motoring,
-          strapName: `${homepageV2AdPrefix}Motoring`,
-          sourceId: Strap.Motoring
-        },
-        {
-          type: HandlerInputType.StripsV2,
-          displayName: "spotlight",
-          color: AccentColor.DarkGray,
-          linkUrl: "/" + Section.Premium,
-          sourceId: Strap.Premium,
-          strapName: Strap.Premium,
-          articleCount: 4
-        },
-        {
-          type: HandlerInputType.Partner
-        },
-        {
-          type: HandlerInputType.Brand,
-          module: BrandModule.Network
         }
-      ] as HandlerInput[]).reduce(
-        (acc: HandlerInput[], currentHandler: HandlerInput) => {
-          return [...acc, currentHandler, billboard(homepageV2AdPrefix)];
-        },
-        [billboard(homepageV2AdPrefix)]
-      ),
-      fallback: []
+      ]
+    },
+    {
+      type: HandlerInputType.BreakingNews
+    },
+    {
+      type: HandlerInputType.Weather
     },
     {
       type: HandlerInputType.TopStories,
@@ -567,13 +585,14 @@ export const newPage = (): HandlerInput[] => {
       module: BrandModule.Network
     }
   ];
-
-  return page.reduce(
-    (acc: HandlerInput[], currentHandler: HandlerInput) => {
-      return [...acc, currentHandler, billboard(homepageAdPrefix)];
-    },
-    [billboard(homepageAdPrefix)]
-  );
+  return [
+    {
+      type: HandlerInputType.Feature,
+      name: FeatureName.HomepageV2,
+      content: interpolateBillboards(homepageV2, billboard(homepageV2AdPrefix)),
+      fallback: interpolateBillboards(homepage, billboard(homepageAdPrefix))
+    }
+  ];
 };
 
 export const oldPage = (): HandlerInput[] => [
