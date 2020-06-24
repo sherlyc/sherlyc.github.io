94a31b7df3e46fb999d77b3cf66c5aece8f51a26
Sherly Chan <sherly.chan@fairfaxmedia.co.nz>
2020-06-24 14:24:51 +1200

    EX-991: scroller buttons will listen to scroll position.

--- src/app/content-blocks/skybox/skybox.component.ts
+++ src/app/content-blocks/skybox/skybox.component.ts
@@ -1,29 +1,68 @@
-import { Component, ElementRef, Input, OnInit, ViewChild } from "@angular/core";
+import { Component, ElementRef, Input, ViewChild } from "@angular/core";
+import { debounce } from "lodash-es";
+import { IHomepageArticleContent } from "../../../../common/__types__/IHomepageArticleContent";
 import { ISkybox } from "../../../../common/__types__/ISkybox";
+import { AnalyticsService } from "../../services/analytics/analytics.service";
+import { AnalyticsEventsType } from "../../services/analytics/__types__/AnalyticsEventsType";
 
 @Component({
   selector: "app-skybox",
   templateUrl: "./skybox.component.html",
-  styleUrls: ["./skybox.component.scss"]
+  styleUrls: ["./skybox.component.scss"],
 })
-export class SkyboxComponent implements OnInit {
+export class SkyboxComponent {
+
+  constructor(private analyticsService: AnalyticsService) {}
   @Input()
   input!: ISkybox;
   @ViewChild("scroller") scroller!: ElementRef;
+  @ViewChild("leftButton") leftButton!: ElementRef;
+  @ViewChild("rightButton") rightButton!: ElementRef;
 
-  constructor() {
-  }
+  buttonState: "start" | "middle" | "end" = "start";
 
-  ngOnInit(): void {
-  }
+  scroll = debounce(() => {
+    const articlesWidth = this.scroller.nativeElement.scrollWidth;
+    const scrollPosition = this.scroller.nativeElement.scrollLeft;
+    const articleWidth = this.scroller.nativeElement.getBoundingClientRect().width;
+
+    if (scrollPosition === 0) {
+        this.buttonState = "start";
+      } else if (scrollPosition >= articlesWidth - articleWidth) {
+        this.buttonState = "end";
+      } else {
+        this.buttonState = "middle";
+      }
+  });
 
   scrollLeft(): void {
-    console.log(this.scroller.nativeElement);
-    this.scroller.nativeElement.scrollTo({ left: (this.scroller.nativeElement.scrollLeft - 150), behavior: "smooth" });
+    const scrollPosition = this.scroller.nativeElement.scrollLeft;
+    const articleWidth = this.scroller.nativeElement.getBoundingClientRect().width;
+
+    this.scroller.nativeElement.scrollTo({
+      left: scrollPosition - articleWidth,
+      behavior: "smooth"
+    });
   }
 
   scrollRight(): void {
-    console.log(this.scroller.nativeElement);
-    this.scroller.nativeElement.scrollTo({ left: (this.scroller.nativeElement.scrollLeft + 150), behavior: "smooth" });
+    const scrollPosition = this.scroller.nativeElement.scrollLeft;
+    const articleWidth = this.scroller.nativeElement.getBoundingClientRect().width;
+
+    this.scroller.nativeElement.scrollTo({
+      left: scrollPosition + articleWidth,
+      behavior: "smooth"
+    });
+  }
+
+  sendAnalytics(article: IHomepageArticleContent) {
+    const { title, id } = article;
+
+    this.analyticsService.pushEvent({
+      type: AnalyticsEventsType.HOMEPAGE_STRAP_CLICKED,
+      strapName: this.input.strapName,
+      articleHeadline: title,
+      articleId: id
+    });
   }
 }
