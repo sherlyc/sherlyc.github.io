cec11557964bc18a68b700d59ab6188f389650e1
Lucas Rainett <lucas.rainett@stuff.co.nz>
2020-06-25 16:02:10 +1200

    link inside a link

--- src/app/shared/components/tag-link/tag-link.component.ts
+++ src/app/shared/components/tag-link/tag-link.component.ts
@@ -1,16 +1,16 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
   selector: "app-tag-link",
   templateUrl: "./tag-link.component.html",
   styleUrls: ["./tag-link.component.scss"]
 })
-export class TagLinkComponent implements OnInit {
+export class TagLinkComponent {
   @Input() name!: string;
   @Input() url!: string;
   @Input() color?: string;
 
-  constructor() {}
-
-  ngOnInit(): void {}
+  onClick(event: Event){
+    event.stopPropagation();
+  }
 }
