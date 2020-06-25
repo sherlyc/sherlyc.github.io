7e96dfb89177facd2969ee74a4c5749cd1db61cf
Viet Anh Tran <tranvietanh1996@gmail.com>
2020-06-25 15:29:37 +1200

    Revert "Merged in EX-1020-ads-not-showing (pull request #1008)"
    This reverts commit 82e6e0366e14fc226440821debaa73f8f0371736, reversing
    changes made to 56e5f9da30baf95aec29cef75da2bd4d8301e68f.

--- src/app/shared/components/page/page.component.ts
+++ src/app/shared/components/page/page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Title } from "@angular/platform-browser";
 import { NavigationStart } from "@angular/router";
-import { from, Subject, zip } from "rxjs";
+import { Subject } from "rxjs";
 import { IContentBlock } from "../../../../../common/__types__/IContentBlock";
 import { IPage } from "../../../../../common/__types__/IPage";
 import { AdService } from "../../../services/ad/ad.service";
@@ -9,8 +9,6 @@ import { AnalyticsService } from "../../../services/analytics/analytics.service"
 import { ContentRetrieverService } from "../../../services/content-retriever/content-retriever.service";
 import { CorrelationService } from "../../../services/correlation/correlation.service";
 import { EventsService } from "../../../services/events/events.service";
-import { FeatureSwitchService } from "../../../services/feature-switch/feature-switch.service";
-import { RuntimeService } from "../../../services/runtime/runtime.service";
 import { SeoService } from "../../../services/seo/seo.service";
 
 @Component({
@@ -27,9 +25,7 @@ export class PageComponent implements OnInit {
     private correlationService: CorrelationService,
     private eventsService: EventsService,
     private analyticsService: AnalyticsService,
-    private seoService: SeoService,
-    private featureSwitchService: FeatureSwitchService,
-    private runtime: RuntimeService
+    private seoService: SeoService
   ) {
     this.navigationStartSubject = this.eventsService.getEventSubject().NavigationStart;
   }
@@ -46,23 +42,12 @@ export class PageComponent implements OnInit {
   getData() {
     this.correlationService.generatePageScopedId();
     this.seoService.reset();
-    if (this.runtime.isBrowser()) {
-      zip(
-        this.contentRetriever.getContent(),
-        from(this.featureSwitchService.getFeatures())
-      ).subscribe(([page, features]) => this.afterDataRetrieval(page));
-    } else {
-      this.contentRetriever
-        .getContent()
-        .subscribe((page) => this.afterDataRetrieval(page));
-    }
-  }
-
-  private afterDataRetrieval(page: IPage) {
-    this.correlationService.setApiRequestId(page.apiRequestId);
-    this.title.setTitle(page.title);
-    this.contentBlocks = page.content;
-    this.adService.notify();
-    this.analyticsService.trackPageByNielsen();
+    this.contentRetriever.getContent().subscribe(async (page: IPage) => {
+      this.correlationService.setApiRequestId(page.apiRequestId);
+      this.title.setTitle(page.title);
+      this.contentBlocks = page.content;
+      this.adService.notify();
+      this.analyticsService.trackPageByNielsen();
+    });
   }
 }
