0f91590232596f0a3e1acb7e2142ed9f2d5b964c
Lucas Rainett <lucas.rainett@stuff.co.nz>
2020-06-26 17:40:40 +1200

    update playlist id

--- server-src/services/handlers/play-stuff/play-stuff-config.ts
+++ server-src/services/handlers/play-stuff/play-stuff-config.ts
@@ -2,7 +2,7 @@ import { IPlayStuffConfig } from "./IPlayStuffConfig";
 
 export const playStuffConfig: IPlayStuffConfig = {
   account: "6005208634001",
-  playlist: "6041675177001",
+  playlist: "1670507911386572773",
   policyKey:
     "BCpkADawqM3XbgNT2rxILaX_0Gmok75p2ZBntZDaN-bQXO7Qatbxuf0ECXO0Xjf-Z4muB0XoFYvXKDHe6zeoTpxsPxi6PrDo9vdWqYpUPArEXZ4YqCW8NI4-UdcAChh-2Hr9nhYI9vf-9X0a",
   thumbnailSize: "320x180",
