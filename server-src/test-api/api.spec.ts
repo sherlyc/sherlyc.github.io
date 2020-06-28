10870d4fc3a477bec2663601e6d60634a68c4a05
Viet Anh Tran <viet.tran@fairfaxmedia.co.nz>
2020-06-28 18:32:12 +1200

    Remove stupid test

--- server-src/test-api/api.spec.ts
+++ server-src/test-api/api.spec.ts
@@ -73,36 +73,6 @@ describe("api test", () => {
     );
   });
 
-  it("should return feature switch container for page v2 and page v1 for new front end version", async () => {
-    const app = require("../app").default;
-    const response: supertest.Response = await supertest(app)
-      .get("/spade/api/1.900/content")
-      .set("Accept", "application/json");
-
-    expect(response.ok).toBeTruthy();
-    expect(response.header["content-type"]).toMatch(/application\/json/);
-
-    const page: IPage = response.body;
-    expect(page.title).toBe(
-      "Latest breaking news NZ | Stuff.co.nz | New Zealand"
-    );
-
-    expect(page.content).toEqual(
-      expect.arrayContaining([
-        expect.objectContaining({ type: ContentBlockType.Header }),
-        expect.objectContaining({
-          type: ContentBlockType.Container,
-          items: expect.arrayContaining([
-            expect.objectContaining({
-              type: ContentBlockType.FeatureContainer
-            })
-          ])
-        }),
-        expect.objectContaining({ type: ContentBlockType.Footer })
-      ])
-    );
-  });
-
   it("should accept requests with no version", async () => {
     const app = require("../app").default;
     const response: supertest.Response = await supertest(app)
