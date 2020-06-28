c6670c3c680c475e9dc5276b36de38b2e717f8d7
Viet Anh Tran <viet.tran@fairfaxmedia.co.nz>
2020-06-28 16:54:36 +1200

    Api test for newer front end version

--- server-src/test-api/api.spec.ts
+++ server-src/test-api/api.spec.ts
@@ -6,7 +6,7 @@ jest.setTimeout(10000);
 
 describe("api test", () => {
   it.each([["1.648"], ["SNAPSHOT"], [""]])(
-    "should return old layout for front end version %s ",
+    "should return page v0 for front end version %s ",
     async (frontendVersion: string) => {
       const app = require("../app").default;
       const response: supertest.Response = await supertest(app)
@@ -37,10 +37,46 @@ describe("api test", () => {
     }
   );
 
-  it("should return new layout for new front end version", async () => {
+  it("should return page v1 for front end version above 1.649", async () => {
     const app = require("../app").default;
     const response: supertest.Response = await supertest(app)
       .get("/spade/api/1.649/content")
+      .set("Accept", "application/json");
+
+    expect(response.ok).toBeTruthy();
+    expect(response.header["content-type"]).toMatch(/application\/json/);
+
+    const page: IPage = response.body;
+    expect(page.title).toBe(
+      "Latest breaking news NZ | Stuff.co.nz | New Zealand"
+    );
+    expect(page.content).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ type: ContentBlockType.Header }),
+        expect.objectContaining({
+          type: ContentBlockType.Container,
+          items: expect.arrayContaining([
+            expect.objectContaining({
+              type: ContentBlockType.GridContainer,
+              items: expect.objectContaining({
+                ModuleTitle: expect.arrayContaining([
+                  expect.objectContaining({
+                    type: ContentBlockType.ModuleTitle
+                  })
+                ])
+              })
+            })
+          ])
+        }),
+        expect.objectContaining({ type: ContentBlockType.Footer })
+      ])
+    );
+  });
+
+  it("should return feature switch container for page v2 and page v1 for new front end version", async () => {
+    const app = require("../app").default;
+    const response: supertest.Response = await supertest(app)
+      .get("/spade/api/1.900/content")
       .set("Accept", "application/json");
 
     expect(response.ok).toBeTruthy();
