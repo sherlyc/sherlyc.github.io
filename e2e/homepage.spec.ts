81347c1b9d3a726f06b234df77b5bac011fd1b5c
Viet Anh Tran <tranvietanh1996@gmail.com>
2020-07-06 17:56:43 +1200

    Increase selenium driver timeout

--- e2e/homepage.spec.ts
+++ e2e/homepage.spec.ts
@@ -17,7 +17,7 @@ describe("Homepage", () => {
 
   beforeAll(async () => {
     driver = await getDriver();
-    await driver.manage().setTimeouts({ pageLoad: 90000 });
+    await driver.manage().setTimeouts({ pageLoad: 120000 });
     await driver.get(config.url);
     await pageToBeReady(driver);
   });
