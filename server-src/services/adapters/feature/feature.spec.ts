717fbeb06d2ee1a78ce52c9aa8899632620fe5e0
Viet Anh Tran <tranvietanh1996@gmail.com>
2020-07-08 08:55:49 +1200

    Release skybox

--- server-src/services/adapters/feature/feature.spec.ts
+++ server-src/services/adapters/feature/feature.spec.ts
@@ -6,7 +6,7 @@ import { isFeatureEnabled } from "./feature";
 describe("Feature service", () => {
   const testCases: { [key in FeatureName]: [number, number] } = {
     [FeatureName.Placeholder]: [-1, -1],
-    [FeatureName.Skybox]: [-1, -1]
+    [FeatureName.Skybox]: [0, 100]
   };
 
   Object.entries(testCases).forEach(([featureName, [min, max]]) => {
