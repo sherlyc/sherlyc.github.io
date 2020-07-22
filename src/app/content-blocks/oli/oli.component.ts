aa3b125c9042d7698e989db33fea9935a2d836c5
Karen Xie <karen.xie@stuff.co.nz>
2020-07-22 14:45:21 +1200

    EX-1082: Fix oli loading stuck by forcing change detection

--- src/app/content-blocks/oli/oli.component.ts
+++ src/app/content-blocks/oli/oli.component.ts
@@ -1,14 +1,23 @@
-import { Component, Input, OnDestroy, OnInit } from "@angular/core";
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  Input,
+  OnDestroy,
+  OnInit
+} from "@angular/core";
 import { Subscription } from "rxjs";
 import { IOli } from "../../../../common/__types__/IOli";
 import { OliService } from "../../services/oli/oli.service";
 import { RuntimeService } from "../../services/runtime/runtime.service";
 import { IContentBlockComponent } from "../__types__/IContentBlockComponent";
+import { finalize } from "rxjs/operators";
 
 @Component({
   selector: "app-oli",
   templateUrl: "./oli.component.html",
-  styleUrls: ["./oli.component.scss"]
+  styleUrls: ["./oli.component.scss"],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class OliComponent implements IContentBlockComponent, OnInit, OnDestroy {
   @Input() input!: IOli;
@@ -19,7 +28,8 @@ export class OliComponent implements IContentBlockComponent, OnInit, OnDestroy {
 
   constructor(
     private oliService: OliService,
-    private runtimeService: RuntimeService
+    private runtimeService: RuntimeService,
+    private changeDetectorRef: ChangeDetectorRef
   ) {
     this.oliAdId = `spade-oli-slot-${Math.random()}`;
   }
@@ -31,6 +41,7 @@ export class OliComponent implements IContentBlockComponent, OnInit, OnDestroy {
     }
     this.subscription = this.oliService
       .load({ ...this.input.config, elementId: this.oliAdId })
+      .pipe(finalize(() => this.changeDetectorRef.detectChanges()))
       .subscribe({
         next: () => (this.loading = false),
         error: () => (this.show = false)
