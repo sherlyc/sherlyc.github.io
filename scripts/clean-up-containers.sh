7060915bb275d1d6b4a7340d9c9cb0975c0ebdb2
Viet Anh Tran <viet.tran@fairfaxmedia.co.nz>
2020-07-28 09:58:43 +1200

    Remove redundant files

--- scripts/clean-up-containers.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-echo "Clean up containers if exists"
-
-OLD_CONTAINER_IDS="$(docker ps --all --quiet --filter "name=yokohama_yokohama-autocannon-*" --filter "name=experience_experience-frontend-*")"
-if [[ -n "$OLD_CONTAINER_IDS" ]]; then
-  docker stop ${OLD_CONTAINER_IDS} && docker rm ${OLD_CONTAINER_IDS}
-fi
