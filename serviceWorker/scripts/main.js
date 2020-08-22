72312d5544cc63426531a4e5d43dcc28b19f8b46
Sherly Chan <sherly.chan@fairfaxmedia.co.nz>
2020-08-23 00:32:29 +1200

    initial setup for push

--- serviceWorker/scripts/main.js
+++ serviceWorker/scripts/main.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator && 'PushManager' in window) {
   console.log('Service Worker and Push is supported');
 
-  navigator.serviceWorker.register('sw.js')
+  navigator.serviceWorker.register('service-worker.js')
     .then(function(swReg) {
       console.log('Service Worker is registered', swReg);
       swRegistration = swReg;
