ecf181062395e012c231532144722235dc6304b8
Sherly Chan <sherly.chan@fairfaxmedia.co.nz>
2020-08-30 22:41:47 +1200

    move permission to main script

--- serviceWorker/scripts/main.js
+++ serviceWorker/scripts/main.js
@@ -1,15 +1,32 @@
-if ('serviceWorker' in navigator && 'PushManager' in window) {
-  console.log('Service Worker and Push is supported');
+if ("serviceWorker" in navigator && "PushManager" in window) {
+  console.log("Service Worker and Push is supported");
 
-  navigator.serviceWorker.register('./scripts/service-worker.js')
-    .then(function(registration) {
-      console.log('Service Worker is registered', registration);
-      return registration
+  navigator.serviceWorker
+    .register("./scripts/service-worker.js")
+    .then(function (registration) {
+      console.log("Service Worker is registered", registration);
+      return registration;
     })
-    .catch(function(error) {
-      console.error('Service Worker Error', error);
+    .then(function () {
+      return new Promise(function (resolve, reject) {
+        const permissionResult = Notification.requestPermission(function (
+          result
+        ) {
+          resolve(result);
+        });
+        if (permissionResult) {
+          permissionResult.then(resolve, reject);
+        }
+      }).then(function (permissionResult) {
+        if (permissionResult !== "granted") {
+          throw new Error("We weren't granted permission.");
+        }
+      });
+    })
+    .catch(function (error) {
+      console.error("Service Worker Error", error);
     });
 } else {
-  console.warn('Push messaging is not supported');
+  console.warn("Push messaging is not supported");
   console.error("Unable to register service worker.", err);
 }
--- serviceWorker/scripts/service-worker.js
+++ serviceWorker/scripts/service-worker.js
@@ -1,18 +1,1 @@
 console.log("service worker script is loaded");
-
-self.addEventListener('activated', () => {
-  console.log("activated, going to request Permission");
-  return new Promise(function(resolve, reject) {
-    const permissionResult = Notification.requestPermission(function(result) {
-      resolve(result);
-    })
-    if (permissionResult) {
-      permissionResult.then(resolve, reject);
-    }
-  })
-    .then(function(permissionResult) {
-      if (permissionResult !== "granted") {
-        throw new Error("We weren't granted permission.");
-      }
-    });
-})
