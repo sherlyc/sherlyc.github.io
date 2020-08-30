75ff2b2bf96870350d06859a081ee53e03740178
Sherly Chan <sherly.chan@fairfaxmedia.co.nz>
2020-08-30 22:18:52 +1200

    ask permission

--- serviceWorker/scripts/service-worker.js
+++ serviceWorker/scripts/service-worker.js
@@ -1,1 +1,17 @@
 console.log("service worker script is loaded");
+
+(function askPermission() {
+  return new Promise(function(resolve, reject) {
+    const permissionResult = Notification.requestPermission(function(result) {
+      resolve(result);
+    })
+    if (permissionResult) {
+      permissionResult.then(resolve, reject);
+    }
+  })
+    .then(function(permissionResult) {
+      if (permissionResult !== "granted") {
+        throw new Error("We weren't granted permission.");
+      }
+    });
+}());
